310591d2a278d7d8c8746ccda4e7284f14768d4b
bryanstearns <stearns@eliot.com>
2009-01-07 23:26:30 +0000

    Add snapshot of our plugins; see vendor/plugins/*/ORIGIN_INFO for details

---
+++ vendor/plugins/workling/lib/workling/remote/runners/spawn_runner.rb
@@ -0,0 +1,36 @@
+require 'workling/remote/runners/base'
+
+#
+#  Run the job over the spawn plugin. Refer to the README for instructions on 
+#  installing Spawn. 
+#
+#  Spawn forks the entire process once for each job. This means that the job starts 
+#  with a very low latency, but takes up more memory for each job. 
+# 
+#  It's also possible to configure Spawn to start a Thread for each job. Do this
+#  by setting
+#
+#      Workling::Remote::Runners::SpawnRunner.options = { :method => :thread }
+#
+#  Have a look at the Spawn README to find out more about the characteristics of this. 
+#
+module Workling
+  module Remote
+    module Runners
+      class SpawnRunner < Workling::Remote::Runners::Base
+        cattr_accessor :options
+        @@options = { :method => :fork }
+        include Spawn if Workling.spawn_installed?
+        
+        # dispatches to Spawn, using the :fork option. 
+        def run(clazz, method, options = {})
+          spawn(SpawnRunner.options) do # exceptions are trapped in here. 
+            dispatch!(clazz, method, options)
+          end
+          
+          return nil # that means nothing!
+        end
+      end
+    end
+  end
+end
