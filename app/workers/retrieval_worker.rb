4dc4b150a7f0d2960c01f8be47da123448a14c7b
bryanstearns <stearns@eliot.com>
2009-02-23 19:53:06 +0000

    Fix retriever call - API had been updated, but call wasn't

--- app/workers/retrieval_worker.rb
+++ app/workers/retrieval_worker.rb
@@ -3,7 +3,8 @@ class RetrievalWorker < Workling::Base
   def retrieval(options)
     puts "RetrievalWorker: #{options.inspect}"
     article = Article.find(options[:article_id])
-    Retriever::update(article, false, options[:source], true)
+    Retriever.new(:only_source => options[:source],
+                  :verbose => options[:verbose]).update(article)
     puts "RetrievalWorker done."
   end
 end
