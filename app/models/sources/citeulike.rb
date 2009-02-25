ec729c469637d76ae3229fb6cdfbdd5365120a6c
articlemetrics <alm_dev@plos.org>
2009-02-25 21:17:35 +0000

    store all the info from citulike response in citation details - tags are comma separated string

--- app/models/sources/citeulike.rb
+++ app/models/sources/citeulike.rb
@@ -6,10 +6,21 @@ class Citeulike < Source
     url = "http://www.citeulike.org/api/posts/for/doi/#{CGI.escape(article.doi)}"
     get_xml(url) do |document|
       citations = []
-      document.find("//posts/post/link").each do |cite|
-        linkURL = cite.attributes['url']
+      document.find("//posts/post").each do |cite|
+	link = cite.find_first("link")
+	post_time = cite.find_first("post_time")
+	tags = []
+	cite.find("tag").each do |tag|
+	  tags << tag.content
+	end
+
         citation = {}
-        citation[:uri] = linkURL
+	citation[:username] = cite.attributes['username']
+	citation[:articleid] = cite.attributes['article_id']
+	citation[:post_time] = post_time.content
+	citation[:tags] = tags.join ', ' 
+        citation[:uri] = link.attributes['url']
+	
         citations << citation
       end
       citations
