d85503f7da6c0cb6df18e437111d708b25986ae4
josowski <josowski@plos.org>
2010-01-19 19:50:36 +0000

    Correcting Citeulike link.  Note JO: 15796

--- app/models/sources/citeulike.rb
+++ app/models/sources/citeulike.rb
@@ -31,7 +31,7 @@ class Citeulike < Source
   end
 
   def public_url(retrieval)
-    retrieval.local_id && ("http://www.citeulike.org/article/" \
+    retrieval.local_id && ("http://www.citeulike.org/article-posts/" \
                            + retrieval.local_id)
   end
   
