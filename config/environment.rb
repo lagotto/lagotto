bcee8f28ecaff911b5719095ab4b6d570db23fb4
mbaehr <mbaehr@plos.org>
2011-01-26 22:15:15 +0000

    Merge r6078 from HEAD: 'Fixes for Scopus WSDL driver'

--- config/environment.rb
+++ config/environment.rb
@@ -35,7 +35,7 @@ Rails::Initializer.run do |config|
 
   # Add additional load paths for your own custom dirs
   # config.load_paths += %W( #{RAILS_ROOT}/extras )
-  config.load_paths += %W[#{RAILS_ROOT}/app/models/sources/]
+  config.load_paths += %W[#{RAILS_ROOT}/app/models/sources/ #{RAILS_ROOT}/app/models/sources/scopus]
 
   # Force all environments to use the same logger level
   # (by default production uses :info, the others :debug)
