d308ce4dc6fbf4e5b36b82f3757b7a2848d4d93c
josowski <josowski@plos.org>
2011-02-10 00:48:54 +0000

    Removing code

--- db/migrate/20101223214158_add_not_null_to_source_disable_delay.rb
+++
@@ -1,9 +0,0 @@
-class AddNotNullToSourceDisableDelay < ActiveRecord::Migration
-  def self.up
-    change_column :sources, :disable_delay, :integer, :null => false
-  end
-
-  def self.down
-    change_column :sources, :disable_delay, :integer, :null => true
-  end
-end
