6f8f4f562f60145b8233930ebf8d9973f07e339d
jusigler <jusigler@gmail.com>
2012-03-28 17:35:29 +0000

    added configuration

---
+++ config/initializers/custom.rb
@@ -0,0 +1,7 @@
+load "#{Rails.root}/lib/source_job.rb"
+
+Delayed::Worker.sleep_delay = 5
+Delayed::Worker.max_attempts = 0
+
+
+APP_CONFIG = YAML.load_file("#{Rails.root}/config/settings.yml")[Rails.env]
