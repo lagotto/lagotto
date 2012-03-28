61e040cad70b47939574bbe5f0ad32fb6181ff52
jsong-plos <jsong@plos.org>
2012-03-28 23:43:11 +0000

    updated twitter source

--- app/models/sources/twitter.rb
+++ app/models/sources/twitter.rb
@@ -1,11 +1,10 @@
-require 'cgi'
 
 class Twitter < Source
 
-  SOURCE_URL = "http://tws-mia.plos.org:5984/plos-tweetstream/_design/tweets/_view/by_doi?key="
+  def get_data(article)
+    # TODO check for required fields
 
-  def get_data(doi)
-    query_url = "#{SOURCE_URL}#{CGI.escape("\"#{doi}\"")}"
+    query_url = "#{configuration.url}#{CGI.escape("\"#{article.doi}\"")}"
 
     puts "#{query_url}"
 
@@ -37,118 +36,34 @@ class Twitter < Source
       end
     end
 
-    [events, events.length]
+    {:events => events, :event_count => events.length}
   end
 
-  def queue_jobs
+
+  def queue_new_articles
 
     # get the source specific configurations
-    source_config = get_source_config(self.name)
+    source_config = YAML.load_file("#{Rails.root}/config/source_configs.yml")[Rails.env]
+    source_config = source_config[name]
 
     # get job specific configuration
-    if !source_config.has_key?('queue_jobs')
+    if !source_config.has_key?('new_articles')
       # TODO let someone know that the source isn't configured correctly
-      puts "queue_jobs is missing"
+      puts "new_articles configuration is missing"
       return
     end
 
-    source_config = source_config['queue_jobs']
-    if !source_config.has_key?('batch_time_interval') || !source_config.has_key?('staleness')
-      # TODO let someone know that the source isn't configured correctly
-      puts "batch_time_interval is missing or staleness is missing"
-      return
-    end
-    # batch_time_interval should be in seconds
-    # staleness should be in seconds
-
-    # determine if the source is active
-    if self.active
-      puts "source is active"
-      queue_job = true
-
-      # determine if the source is disabled or not
-      unless self.disable_until.nil?
-        queue_job = false
-
-        if self.disable_until < Time.now.utc
-          self.disable_until = nil
-          self.save
-          queue_job = true
-        elsif self.disable_until < (Time.now.utc + source_config['batch_time_interval'])
-          # the source will become not disabled before the next round (of job queueing)
-          # just sleep til the source will become not disabled and queue the jobs
-          source_config['batch_time_interval'] = Time.now.utc - self.disable_until
-        end
-      end
-
-      if queue_job
-        queue_jobs_helper(source_config)
-      end
-    end
-
-    return source_config['batch_time_interval']
-  end
-
-  def queue_jobs_helper(options)
-
-    if !options.has_key?('staleness')
-      Rails.logger.info "Staleness value was not passed in.  Existing"
-      return
-    end
-
-    # find articles that need to be updated
-
-    # not queued currently
-    # stale from updated_at
-    retrieval_statuses = RetrievalStatus.joins(:article, :source).
-        where('sources.id = ?
-               and articles.published_on < ?
-               and queued_at is NULL
-               and retrieved_at < TIMESTAMPADD(SECOND, -?, UTC_TIMESTAMP())',
-              id, Time.zone.today, options['staleness']).
-        readonly(false).limit(10)
-
-    puts "#{name} total article queued #{retrieval_statuses.length}"
-    Rails.logger.debug "#{name} total article queued #{retrieval_statuses.length}"
-
-    retrieval_statuses.each do | retrieval_status |
-      puts "#{name} article id #{retrieval_status.article_id}"
-      Rails.logger.debug  "#{name} article id #{retrieval_status.article_id}"
-
-      article = Article.find(retrieval_status.article_id)
-
-      retrieval_history = RetrievalHistory.new
-      retrieval_history.article_id = article.id
-      retrieval_history.source_id = id
-      retrieval_history.save
-
-      Delayed::Job.enqueue SourceJob.new(article.doi, self, retrieval_status, retrieval_history), :queue => name
-    end
-  end
-
-
-  def queue_new_article_jobs
-
-    # get the source specific configurations
-    source_config = get_source_config(name)
-
-    # get job specific configuration
-    if !source_config.has_key?('queue_new_article_jobs')
-      # TODO let someone know that the source isn't configured correctly
-      puts "queue_new_article_jobs is missing"
-      return
-    end
-
-    source_config = source_config['queue_new_article_jobs']
+    source_config = source_config['new_articles']
     if !source_config.has_key?('batch_time_interval') || !source_config.has_key?('days_since_published')
       # TODO let someone know that the source isn't configured correctly
       puts "batch_time_interval is missing or days_since_published is missing"
       return
     end
 
+    source_config['batch_time_interval'] = parse_time_config(source_config['batch_time_interval'])
+
     # determine if the source is active
     if self.active
-      puts "source is active"
       queue_job = true
 
       # determine if the source is disabled or not
@@ -157,7 +72,7 @@ class Twitter < Source
 
         if self.disable_until < Time.now.utc
           self.disable_until = nil
-          self.save
+          save
           queue_job = true
         elsif self.disable_until < (Time.now.utc + source_config['batch_time_interval'])
           # the source will become not disabled before the next round (of job queueing)
@@ -167,19 +82,14 @@ class Twitter < Source
       end
 
       if queue_job
-        queue_new_article_jobs_helper(source_config)
+        queue_new_article_jobs(source_config)
       end
     end
 
     return source_config['batch_time_interval']
   end
 
-  def queue_new_article_jobs_helper(options)
-
-    if !options.has_key?('days_since_published')
-      Rails.logger.info "days_since_published value was not passed in.  Existing"
-      return
-    end
+  def queue_new_article_jobs(source_config)
 
     # find articles that need to be updated
     retrieval_statuses = RetrievalStatus.joins(:article, :source).
@@ -187,25 +97,20 @@ class Twitter < Source
                and articles.published_on < ?
                and articles.published_on > ?
                and queued_at is NULL',
-              id, Time.zone.today, Time.zone.today - options['days_since_published']).
-        readonly(false).limit(1)
+              id, Time.zone.today, Time.zone.today - source_config['days_since_published']).
+        readonly(false)
 
     puts "#{name} total article queued #{retrieval_statuses.length}"
     Rails.logger.debug "#{name} total article queued #{retrieval_statuses.length}"
 
     retrieval_statuses.each do | retrieval_status |
 
-      puts "#{name} article id #{retrieval_status.article_id}"
-      Rails.logger.debug  "#{name} article id #{retrieval_status.article_id}"
-
-      article = Article.find(retrieval_status.article_id)
-
       retrieval_history = RetrievalHistory.new
-      retrieval_history.article_id = article.id
+      retrieval_history.article_id = retrieval_status.article_id
       retrieval_history.source_id = id
       retrieval_history.save
 
-      Delayed::Job.enqueue SourceJob.new(article.doi, self, retrieval_status, retrieval_history), :queue => name
+      Delayed::Job.enqueue SourceJob.new(retrieval_status.article_id, self, retrieval_status, retrieval_history), :queue => name
     end
   end
 
