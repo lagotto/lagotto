e1a341b042a0a36ae0ae09e82daccccad46a99d7
jusigler <jusigler@gmail.com>
2012-04-10 18:17:34 +0000

    PDEV-434 setting up plos layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  APP_CONFIG = YAML.load_file("#{Rails.root}/config/settings.yml")[Rails.env]
+  layout APP_CONFIG['layout']
 end
