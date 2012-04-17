8edab42ffda54c57d4a43a8890bfba49bba0821f
jusigler <jusigler@gmail.com>
2012-04-17 23:27:12 +0000

    PDEV-435 display retrieval data in html

--- app/models/retrieval_status.rb
+++ app/models/retrieval_status.rb
@@ -1,5 +1,20 @@
+require 'source_helper'
+
 class RetrievalStatus < ActiveRecord::Base
+  include SourceHelper
+
   belongs_to :article
   belongs_to :source
   has_many :retrieval_histories, :dependent => :destroy
+
+  def get_retrieval_data
+    source = Source.find(source_id)
+    article = Article.find(article_id)
+    begin
+      data = get_alm_data("#{source.name}:#{CGI.escape(article.doi)}")
+    rescue => e
+      Rails.logger.error "Failed to get data for #{source.name}:#{article.doi}.  #{e.message}"
+    end
+  end
+
 end
