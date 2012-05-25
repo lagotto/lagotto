597607118fa6a2e5d161fcb5d812f4ead484da55
jusigler <jusigler@gmail.com>
2012-05-25 23:16:18 +0000

    added logic to prevent a worker from getting stuck on a job

--- config/initializers/custom.rb
+++ config/initializers/custom.rb
@@ -5,5 +5,6 @@ require "#{Rails.root}/lib/source_job.rb"
 Delayed::Worker.sleep_delay = 5
 Delayed::Worker.max_attempts = 0
 Delayed::Worker.default_priority = 1
+Delayed::Worker.max_run_time = 30.minutes
 
 APP_CONFIG = YAML.load_file("#{Rails.root}/config/settings.yml")[Rails.env]
