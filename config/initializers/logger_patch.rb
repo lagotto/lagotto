9405b2da9b0ead6f60323ebfef8d7802a4cd84c6
jsong-plos <jsong@plos.org>
2012-05-29 18:32:13 +0000

    PDEV-823 upgrading to rails 3.2

--- config/initializers/logger_patch.rb
+++ config/initializers/logger_patch.rb
@@ -1,22 +1,19 @@
 #
 # ActiveSupport patch
-# This will not work in rails 3.2
 #
-module ActiveSupport
 
-  # Format the buffered logger with timestamp
-  class BufferedLogger
-
-    def add(severity, message = nil, progname = nil, &block)
-      return if @level > severity
-      message = (message || (block && block.call) || progname).to_s
-      # If a newline is necessary then create a new message ending with a newline.
-      # Ensures that the original message is not mutated.
-      message = "#{Time.now.strftime('%F %T %z')} #{message}\n" unless message[-1] == ?\n
-      buffer << message
-      auto_flush
-      message
-    end
-
+class ActiveSupport::BufferedLogger
+  def formatter=(formatter)
+    @log.formatter = formatter
   end
 end
+
+class Formatter
+
+  def call(severity, time, progname, msg)
+    "#{time.strftime('%F %T %z')} #{severity} #{msg} \n"
+  end
+
+end
+
+Rails.logger.formatter = Formatter.new
