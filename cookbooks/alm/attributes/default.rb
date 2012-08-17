020ccc9f02d09a66fd392dacb402b6b400d19643
Martin Fenner <mf@martinfenner.org>
2012-08-17 11:09:39 +0200

    PDEV-965 cookbooks moved to vendor directory

--- cookbooks/alm/attributes/default.rb
+++
@@ -1,5 +0,0 @@
-default[:app][:name] = "alm"
-default[:app][:useragent] = "Article Level Metrics"
-default[:app][:layout] = "greenrobo"
-default[:app][:seed_additional_sources] = true
-default[:app][:seed_sample_articles] = true
