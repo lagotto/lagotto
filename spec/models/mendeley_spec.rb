ed9bfddd6e3aad7ce917f1aff76754d86780f0c3
Martin Fenner <mf@martinfenner.org>
2012-11-29 17:55:48 +0100

    ALM-133 reverted back to previous version

--- spec/models/mendeley_spec.rb
+++ spec/models/mendeley_spec.rb
@@ -12,9 +12,11 @@ describe Mendeley do
     it "should report if there are no events and event_count returned by the Mendeley API" do
       article_without_events = FactoryGirl.build(:article, :doi => "10.1371/journal.pone.0044294", :mendeley => "")
       stub = stub_request(:get, mendeley.get_query_url(CGI.escape(CGI.escape(article_without_events.doi)), "doi")).to_return(:body => File.read(fixture_path + 'mendeley_nil.json'), :status => 200)
+      stub_pubmed = stub_request(:get, mendeley.get_query_url(article_without_events.pub_med, "pmid")).to_return(:body => File.read(fixture_path + 'mendeley_nil.json'), :status => 200)
       stub_related = stub_request(:get, mendeley.related_url("46cb51a0-6d08-11df-afb8-0026b95d30b2")).to_return(:body => File.read(fixture_path + 'mendeley_related.json'), :status => 200)
       mendeley.get_data(article_without_events).should eq({ :events => [], :event_count => 0 })
       stub.should have_been_requested
+      stub_pubmed.should have_been_requested
     end
     
     it "should report if there are events and event_count returned by the Mendeley API" do
