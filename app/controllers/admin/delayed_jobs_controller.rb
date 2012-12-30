9b6fe6ebfd1c398b6b09398926dd240b85c70887
Martin Fenner <mf@martinfenner.org>
2012-12-30 12:17:39 +0100

    ALM-125 link queues to sources

--- app/controllers/admin/delayed_jobs_controller.rb
+++ app/controllers/admin/delayed_jobs_controller.rb
@@ -1,14 +1,8 @@
 class Admin::DelayedJobsController < Admin::ApplicationController
   
   def index
-    @jobs = DelayedJob.select("queue, COUNT(*) as total, COUNT(locked_at) as active, COUNT(failed_at) as failed").group(:queue)
-    respond_with @jobs
-  end
-  
-  def destroy
-    @delayed_job = DelayedJob.find(params[:id])
-    @delayed_job.delete
-    redirect_to admin_root_path
+    @sources = Source.joins("LEFT OUTER JOIN delayed_jobs ON sources.name = delayed_jobs.queue").select("sources.id as id, sources.display_name as display_name, COUNT(run_at) as total, COUNT(locked_at) as locked, COUNT(failed_at) as failed").group(:queue)
+    respond_with @sources
   end
   
 end
