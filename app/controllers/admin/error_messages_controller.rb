2788ad486ab25bab942e67b7772959311ba41e1a
Martin Fenner <mf@martinfenner.org>
2013-01-07 17:44:03 +0100

    ALM-152 option to delete error messages by class

--- app/controllers/admin/error_messages_controller.rb
+++ app/controllers/admin/error_messages_controller.rb
@@ -10,8 +10,16 @@ class Admin::ErrorMessagesController < Admin::ApplicationController
   
   def destroy
     @error_message = ErrorMessage.find(params[:id])
-    ErrorMessage.destroy_all(:message => @error_message.message)
-    @error_messages = ErrorMessage.order("updated_at DESC").paginate(:page => params[:page])
+    if params[:by_class]
+      ErrorMessage.destroy_all(:class_name => @error_message.class_name)
+    else
+      ErrorMessage.destroy_all(:message => @error_message.message)
+    end
+    
+    collection = ErrorMessage
+    collection = collection.query(params[:query]) if params[:query]
+    
+    @error_messages = collection.paginate(:page => params[:page])
     respond_with(@error_messages) do |format|  
       format.js { render :index }
     end
