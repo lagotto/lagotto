f3e2d515db1ffa6834b19fb47317daa6cb3cf41e
Martin Fenner <mf@martinfenner.org>
2013-01-11 00:31:02 +0100

    ALM-167 optimize SQL for responses and events

--- app/controllers/admin/events_controller.rb
+++ app/controllers/admin/events_controller.rb
@@ -2,6 +2,7 @@ class Admin::EventsController < Admin::ApplicationController
   
   def index
     @sources = Source.order("name")
+    @events_count = RetrievalStatus.group(:source_id).sum(:event_count)
     respond_with @sources
   end
   
