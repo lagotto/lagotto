23a910b2db9cb207e6a5ac829267743131664246
Martin Fenner <mf@martinfenner.org>
2013-01-13 04:00:14 +0100

    ALM-184 added d3.js support, added basic bar charts to events page

--- app/controllers/admin/events_controller.rb
+++ app/controllers/admin/events_controller.rb
@@ -2,7 +2,9 @@ class Admin::EventsController < Admin::ApplicationController
   
   def index
     @sources = Source.order("name")
-    @events_count = RetrievalStatus.group(:source_id).sum(:event_count)
+    gon.articles = Source.order("name").map { |source| source.retrieval_statuses.count(:conditions => "event_count > 0") }
+    gon.events = RetrievalStatus.group(:source_id).sum(:event_count).values
+    gon.labels = Source.order("id").pluck(:display_name)
     respond_with @sources
   end
   
