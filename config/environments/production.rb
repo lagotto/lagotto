44e2af7b6a9990d51fe44b0771bc436c68809c4d
Martin Fenner <mf@martinfenner.org>
2013-01-15 16:06:28 +0100

    ALM-157 precompile plos.css

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,7 +15,7 @@ Alm::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = true
+  config.assets.compile = false
 
   # Generate digests for assets URLs
   config.assets.digest = true
@@ -43,7 +43,7 @@ Alm::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  config.assets.precompile += %w( greenrobo.css )
+  config.assets.precompile += %w( plos.css )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
