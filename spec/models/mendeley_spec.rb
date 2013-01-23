696bad13939d64c03893d75019717f1a941778d6
Martin Fenner <mf@martinfenner.org>
2013-01-24 00:42:04 +0100

    ALM-159 search for uuid by title if we can't find it via pmid or doi

--- spec/models/mendeley_spec.rb
+++ spec/models/mendeley_spec.rb
@@ -12,26 +12,41 @@ describe Mendeley do
     let(:article) { FactoryGirl.build(:article, :doi => "10.1371/journal.pone.0008776", :mendeley => "") }
     
     it "should return the Mendeley uuid by the Mendeley API" do
-      stub = stub_request(:get, mendeley.get_query_url(CGI.escape(CGI.escape(article.doi)), "doi")).to_return(:body => File.read(fixture_path + 'mendeley.json'), :status => 200)
+      stub = stub_request(:get, mendeley.get_query_url(article.pub_med, "pmid")).to_return(:body => File.read(fixture_path + 'mendeley.json'), :status => 200)
       mendeley.get_mendeley_uuid(article).should eq("46cb51a0-6d08-11df-afb8-0026b95d30b2")
       stub.should have_been_requested
     end
     
+    it "should return the Mendeley uuid by searching the Mendeley API" do
+      article = FactoryGirl.build(:article, :doi => "10.1371/journal.pone.0000001", :mendeley => "") 
+      stub = stub_request(:get, mendeley.get_query_url(article.pub_med, "pmid")).to_return(:body => File.read(fixture_path + 'mendeley_nil.json'), :status => 200)
+      stub_doi = stub_request(:get, mendeley.get_query_url(CGI.escape(CGI.escape(article.doi)), "doi")).to_return(:body => File.read(fixture_path + 'mendeley_nil.json'), :status => 200)
+      stub_title = stub_request(:get, mendeley.get_query_url(CGI.escape(article.title), "title")).to_return(:body => File.read(fixture_path + 'mendeley_search.json'), :status => 200)
+      mendeley.get_mendeley_uuid(article).should eq("1779af10-6d0c-11df-a2b2-0026b95e3eb7")
+      stub.should have_been_requested
+      stub_doi.should have_been_requested
+      stub_title.should have_been_requested
+    end
+    
     it "should return nil for the Mendeley uuid if the Mendeley API returns malformed response" do
-      stub = stub_request(:get, mendeley.get_query_url(CGI.escape(CGI.escape(article.doi)), "doi")).to_return(:body => File.read(fixture_path + 'mendeley_nil.json'), :status => 200)
-      stub_pubmed = stub_request(:get, mendeley.get_query_url(article.pub_med, "pmid")).to_return(:body => File.read(fixture_path + 'mendeley_nil.json'), :status => 200)
+      stub = stub_request(:get, mendeley.get_query_url(article.pub_med, "pmid")).to_return(:body => File.read(fixture_path + 'mendeley_nil.json'), :status => 200)
+      stub_doi = stub_request(:get, mendeley.get_query_url(CGI.escape(CGI.escape(article.doi)), "doi")).to_return(:body => File.read(fixture_path + 'mendeley_nil.json'), :status => 200)
+      stub_title = stub_request(:get, mendeley.get_query_url(CGI.escape(article.title), "title")).to_return(:body => File.read(fixture_path + 'mendeley_search.json'), :status => 200)
       mendeley.get_mendeley_uuid(article).should be_nil
       stub.should have_been_requested
-      stub_pubmed.should have_been_requested
+      stub_doi.should have_been_requested
+      stub_title.should have_been_requested
       ErrorMessage.count.should == 0
     end
     
     it "should return nil for the Mendeley uuid if the Mendeley API returns incomplete response" do
-      stub = stub_request(:get, mendeley.get_query_url(CGI.escape(CGI.escape(article.doi)), "doi")).to_return(:body => File.read(fixture_path + 'mendeley_incomplete.json'), :status => 200)
-      stub_pubmed = stub_request(:get, mendeley.get_query_url(article.pub_med, "pmid")).to_return(:body => File.read(fixture_path + 'mendeley_incomplete.json'), :status => 200)
+      stub = stub_request(:get, mendeley.get_query_url(article.pub_med, "pmid")).to_return(:body => File.read(fixture_path + 'mendeley_incomplete.json'), :status => 200)
+      stub_doi = stub_request(:get, mendeley.get_query_url(CGI.escape(CGI.escape(article.doi)), "doi")).to_return(:body => File.read(fixture_path + 'mendeley_incomplete.json'), :status => 200)
+      stub_title = stub_request(:get, mendeley.get_query_url(CGI.escape(article.title), "title")).to_return(:body => File.read(fixture_path + 'mendeley_search.json'), :status => 200)
       mendeley.get_mendeley_uuid(article).should be_nil
       stub.should have_been_requested
-      stub_pubmed.should have_been_requested
+      stub_doi.should have_been_requested
+      stub_title.should have_been_requested
       ErrorMessage.count.should == 1
       error_message = ErrorMessage.first
       error_message.class_name.should eq("Net::HTTPConflict")
