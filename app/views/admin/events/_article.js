ad31137079737566c9e65946cacce3820a7b7733
Martin Fenner <mf@martinfenner.org>
2013-01-26 11:10:06 +0100

    ALM-187 fixed misaligned bars for 0 events, also added links to sources and color by group

--- app/views/admin/events/_article.js
+++ app/views/admin/events/_article.js
@@ -1,5 +1,5 @@
-var data = <%= @articles %>;
-var labels = <%= raw @labels %>;
+var data = <%= raw @data %>;
+var colors = ["#304345","#789aa1","#ad9a27","#a17f78","#a0d5d6"]
   
 var l = 200; // left margin
 var r = 120; // right margin
@@ -14,31 +14,36 @@ var chart = d3.select("div#article").append("svg")
   .append("g")
   .attr("transform", "translate(230,20)");
 var x = d3.scale.linear()
-  .domain([0, d3.max(data)])
-  .range([1, w]);
+  .domain([0, d3.max(data, function(d) { return d.article_count; })])
+  .range([0, w]);
 var y = d3.scale.ordinal()
-  .domain(labels)
-  .rangeBands([0, (h + 2 * s) * labels.length]);
+  .domain(data.map(function(d) { return d.name; }))
+  .rangeBands([0, (h + 2 * s) * data.length]);
+var z = d3.scale.ordinal()
+  .domain(data.map(function(d) { return d.group; }))
+  .range(colors);
 chart.selectAll("text.labels")
-  .data(labels)
-  .enter().append("text")
+  .data(data)
+  .enter().append("a").attr("xlink:href", function(d) { return d.url; }).append("text")
   .attr("x", 0)
-  .attr("y", function(d) { return y(d) + y.rangeBand() / 2; })
+  .attr("y", function(d) { return y(d.name) + y.rangeBand() / 2; })
   .attr("dx", -230) // padding-right
   .attr("dy", ".35em") // vertical-align: middle
-  .text(String)
+  .text(function(d) { return d.name; })
 chart.selectAll("rect")
   .data(data)
   .enter().append("rect")
-  .attr("fill", "#789aa1")
-  .attr("y", y)
-  .attr("width", x)
-  .attr("height", h);
+  .attr("fill", function(d) { return z(d.group); })
+  .attr("y", function(d,i) { return y(d.name); })  
+  .attr("height", h)
+  .transition().duration(500).delay(200)
+  .attr("width", function(d) { return x(d.article_count); });
 chart.selectAll("text.values")
   .data(data)
   .enter().append("text")
-  .attr("x", x)
-  .attr("y", function(d) { return y(d) + y.rangeBand() / 2; })
+  .attr("x", function(d) { return x(d.article_count); })
+  .attr("y", function(d) { return y(d.name) + y.rangeBand() / 2; })
   .attr("dx", 5) // padding-right
   .attr("dy", ".35em") // vertical-align: middle
-  .text(function(d) { return d.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","); })
+  .transition().delay(700)
+  .text(function(d) { return d.article_count.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","); })
