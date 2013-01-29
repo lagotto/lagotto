11d799f5d50bcd66d866e1cd9c963cea46d74335
Martin Fenner <mf@martinfenner.org>
2013-01-29 12:34:34 +0100

    ALM-193 increased timeout for jobs

--- app/controllers/admin/responses_controller.rb
+++ app/controllers/admin/responses_controller.rb
@@ -3,7 +3,7 @@ class Admin::ResponsesController < Admin::ApplicationController
   def index
     @sources = Source.active
     @responses_count = RetrievalStatus.joins(:source).where("retrieved_at > NOW() - INTERVAL 24 HOUR").group(:source_id).count
-    @errors_count = ErrorMessage.unscoped.joins(:source).where("error_messages.created_at > NOW() - INTERVAL 24 HOUR").order("group_id, display_name").group(:source_id).count
+    @errors_count = ErrorMessage.unscoped.joins(:source).where("error_messages.created_at > NOW() - INTERVAL 24 HOUR AND sources.active = 1").order("group_id, display_name").group(:source_id).count
     respond_with @sources
   end
 
--- config/initializers/delayed_job_config.rb
+++ config/initializers/delayed_job_config.rb
@@ -20,6 +20,6 @@ Delayed::Worker.destroy_failed_jobs = true
 Delayed::Worker.sleep_delay = 5
 Delayed::Worker.max_attempts = 1
 Delayed::Worker.default_priority = 1
-Delayed::Worker.max_run_time = 30.minutes
+Delayed::Worker.max_run_time = 50.minutes
 Delayed::Worker.read_ahead = 10
 Delayed::Worker.delay_jobs = !Rails.env.test?
