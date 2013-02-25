19a58227485d3c10fff9be2dba2e03239b93e432
Martin Fenner <mf@martinfenner.org>
2013-02-25 10:18:19 +0100

    ALM-196 enable caching on production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -6,7 +6,7 @@ Alm::Application.configure do
 
   # Full error reports are disabled and caching is turned on
   config.consider_all_requests_local       = false
-  config.action_controller.perform_caching = false
+  config.action_controller.perform_caching = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
   config.serve_static_assets = false
