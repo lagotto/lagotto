6b043ed933b040e5282458d41ffa2e95fb146793
Martin Fenner <mf@martinfenner.org>
2013-05-05 13:32:35 +0200

    ALM-278 require an API key for d3.js calls

--- app/views/articles/_year.js
+++ app/views/articles/_year.js
@@ -1,6 +1,6 @@
 var doi = d3.select("dd#doi").attr('data-doi');
 
-d3.json("/api/v3/articles/info:doi/" + doi + "?info=history", function(error, data) {
+d3.json("/api/v3/articles/info:doi/" + doi + "?info=history&api_key=" + api_key, function(error, data) {
   
   var l = 20; // left margin
   var r = 50; // right margin
