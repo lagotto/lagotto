94e15ba7f4011e5ee354767175725fe13eea1992
Martin Fenner <mf@martinfenner.org>
2013-07-10 23:37:07 +0200

    ALM-379 simplified MySQL query

--- app/controllers/admin/responses_controller.rb
+++ app/controllers/admin/responses_controller.rb
@@ -5,7 +5,7 @@ class Admin::ResponsesController < Admin::ApplicationController
   def index
     if request.xhr?
       responses = RetrievalStatus.includes(:source).where("sources.active = 1 AND retrieved_at > NOW() - INTERVAL 24 HOUR").order("group_id, display_name").group(:source_id).count
-      errors = ErrorMessage.unscoped.includes(:source).where("sources.active = 1 AND error_messages.created_at > NOW() - INTERVAL 24 HOUR AND sources.active = 1").order("group_id, display_name").group(:source_id).count
+      errors = ErrorMessage.unscoped.includes(:source).where("sources.active = 1 AND error_messages.created_at > NOW() - INTERVAL 24 HOUR").order("group_id, display_name").group(:source_id).count
       @sources = Source.active.zip(responses, errors).map { |source| { "id" => source.first.id,
                                                                        "name" => source.first.display_name, 
                                                                        "status" => source.first.status,
