5afbf1a78e233f8058406315e7d181f607598563
Martin Fenner <mf@martinfenner.org>
2013-07-11 09:36:42 +0200

    ALM-379 correctly match responses and errors to sources

--- app/controllers/admin/responses_controller.rb
+++ app/controllers/admin/responses_controller.rb
@@ -6,13 +6,13 @@ class Admin::ResponsesController < Admin::ApplicationController
     if request.xhr?
       responses = RetrievalStatus.includes(:source).where("sources.active = 1 AND retrieved_at > NOW() - INTERVAL 24 HOUR").order("group_id, display_name").group(:source_id).count
       errors = ErrorMessage.unscoped.includes(:source).where("sources.active = 1 AND error_messages.created_at > NOW() - INTERVAL 24 HOUR").order("group_id, display_name").group(:source_id).count
-      @sources = Source.active.zip(responses, errors).map { |source| { "id" => source.first.id,
-                                                                       "name" => source.first.display_name, 
-                                                                       "status" => source.first.status,
-                                                                       "url" => admin_source_path(source.first),
-                                                                       "group" => source.first.group_id, 
-                                                                       "response_count" => source[1],
-                                                                       "error_count" => source[2] } }
+      @sources = Source.active.map { |source| { "id" => source.id,
+                                                "name" => source.display_name, 
+                                                "status" => source.status,
+                                                "url" => admin_source_path(source),
+                                                "group" => source.group_id, 
+                                                "response_count" => responses[source.id],
+                                                "error_count" => errors[source.id] }}
       render :partial => "index"
     else
       render :index
