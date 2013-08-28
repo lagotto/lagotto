0f89686a2c1ca283082d1054cc8e6a2cf1c6f7b4
Martin Fenner <mfenner@plos.org>
2013-08-28 20:29:25 +0200

    ALM-431 added remote_ip to what is called in error messages

--- app/models/error_message.rb
+++ app/models/error_message.rb
@@ -45,6 +45,7 @@ class ErrorMessage < ActiveRecord::Base
 
     if request
       self.target_url   = target_url || request.original_url
+      self.remote_ip    = remote_ip || request.remote_ip
       self.user_agent   = user_agent || request.user_agent
       self.content_type = content_type || request.formats.first.to_s
     end
