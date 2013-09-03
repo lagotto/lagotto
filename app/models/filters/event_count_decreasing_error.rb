24373b6c09c815efa5595454370fe938f72249e3
Martin Fenner <mfenner@plos.org>
2013-09-04 00:15:27 +0200

    ALM-393 fixed typo in decreasing event count error message

--- app/models/filters/event_count_decreasing_error.rb
+++ app/models/filters/event_count_decreasing_error.rb
@@ -26,7 +26,7 @@ class EventCountDecreasingError < Filter
     if responses.count > 0
       responses = responses.all.map { |response| { source_id: response.source_id,
                                                    article_id: response.article_id,
-                                                   message: "Event count decreased from #{response.previous_count} to #{response.previous_count}" }}
+                                                   message: "Event count decreased from #{response.previous_count} to #{response.event_count}" }}
       raise_alerts(responses)
     end
 
