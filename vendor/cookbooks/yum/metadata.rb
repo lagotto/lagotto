5d70fc1841e906e1feb827796cdb89d090fd3219
Martin Fenner <mfenner@plos.org>
2013-09-03 17:12:52 +0200

    ALM-392 added filters to ALM cookbook

--- vendor/cookbooks/yum/metadata.rb
+++ vendor/cookbooks/yum/metadata.rb
@@ -3,7 +3,7 @@ maintainer        "Opscode, Inc."
 maintainer_email  "cookbooks@opscode.com"
 license           "Apache 2.0"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           "2.3.0"
+version           "2.3.2"
 recipe            "yum", "Empty recipe."
 recipe            "yum::yum", "Manages yum configuration"
 
