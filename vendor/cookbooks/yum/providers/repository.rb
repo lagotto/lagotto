d400ef335f8c96a2bfeab47164b200a3e09ab79b
Martin Fenner <mfenner@plos.org>
2013-11-01 13:27:19 +0100

    ALM-579 updated Chef cookbooks

--- vendor/cookbooks/yum/providers/repository.rb
+++ vendor/cookbooks/yum/providers/repository.rb
@@ -117,7 +117,10 @@ def repo_config
                 :exclude => new_resource.exclude,
                 :priority => new_resource.priority,
                 :metadata_expire => new_resource.metadata_expire,
-                :type => new_resource.type
+                :type => new_resource.type,
+                :proxy => new_resource.proxy,
+                :proxy_username => new_resource.proxy_username,
+                :proxy_password => new_resource.proxy_password
               })
     if new_resource.make_cache
       notifies :run, "execute[yum-makecache-#{new_resource.repo_name}]", :immediately
