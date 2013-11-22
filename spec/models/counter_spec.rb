2e6bba9e21768afac6de3212be64ae3d0df4269b
Martin Fenner <mfenner@plos.org>
2013-11-22 17:33:24 +0100

    ALM-581 replace http-net with http-net-persistent to better handle pmc updates

--- spec/models/counter_spec.rb
+++ spec/models/counter_spec.rb
@@ -13,7 +13,7 @@ describe Counter do
       article = FactoryGirl.build(:article, :doi => "10.1371/journal.pone.0044294")
       body = File.read(fixture_path + 'counter_nil.xml')
       stub = stub_request(:get, counter.get_query_url(article)).to_return(:body => body, :status => 404)
-      counter.get_data(article).should eq({ :events => [], :event_count => 0, :events_url => counter.get_query_url(article), :event_metrics => { :pdf=>0, :html=>0, :shares=>nil, :groups=>nil, :comments=>nil, :likes=>nil, :citations=>nil, :total=>0 }, :attachment => nil })
+      counter.get_data(article).should eq({ :events => [], :event_count => 0, :event_metrics => { :pdf=>0, :html=>0, :shares=>nil, :groups=>nil, :comments=>nil, :likes=>nil, :citations=>nil, :total=>0 }, :attachment => nil })
       stub.should have_been_requested
     end
 
@@ -23,7 +23,6 @@ describe Counter do
       stub = stub_request(:get, counter.get_query_url(article)).to_return(:body => body, :status => 200)
       response = counter.get_data(article)
       response[:events].length.should eq(37)
-      response[:events_url].should eq(counter.get_query_url(article))
       response[:event_count].should eq(3387)
       response[:attachment][:data].should eq(body)
       response[:event_metrics].should eq({ :pdf=>447, :html=>2919, :shares=>nil, :groups=>nil, :comments=>nil, :likes=>nil, :citations=>nil, :total=>3387 })
