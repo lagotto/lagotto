396c6b6ec8292860e3340f2048107edc15cc3ed4
Martin Fenner <mfenner@plos.org>
2013-12-02 00:45:41 +0100

    ALM-600 fixed rspec test

--- spec/models/wikipedia_spec.rb
+++ spec/models/wikipedia_spec.rb
@@ -12,14 +12,14 @@ describe Wikipedia do
   context "use the Wikipedia API" do
     it "should report if there are no events and event_count returned by the Wikipedia API" do
       article_without_events = FactoryGirl.build(:article, :doi => "10.1371/journal.pone.0044294")
-      stub = stub_request(:get, /.*wiki/).to_return(:body => File.read(fixture_path + 'wikipedia_nil.json'), :status => 200)
+      stub = stub_request(:get, /en.wikipedia.org/).to_return(:body => File.read(fixture_path + 'wikipedia_nil.json'), :status => 200)
       response = wikipedia.get_data(article_without_events)
       response[:event_count].should == 0
     end
 
     it "should report if there are events and event_count returned by the Wikipedia API" do
       article = FactoryGirl.build(:article, :doi => "10.1371/journal.pcbi.1002445")
-      stub = stub_request(:get, /.*wiki/).to_return(:headers => { "Content-Type" => "application/json" }, :body => File.read(fixture_path + 'wikipedia.json'), :status => 200)
+      stub = stub_request(:get, /en.wikipedia.org/).to_return(:headers => { "Content-Type" => "application/json" }, :body => File.read(fixture_path + 'wikipedia.json'), :status => 200)
       response = wikipedia.get_data(article)
       response[:events].length.should eq(1 + 1)
       response[:event_count].should eq(1 * 12)
@@ -27,7 +27,7 @@ describe Wikipedia do
 
     it "should catch errors with the Wikipedia API" do
       article = FactoryGirl.build(:article, :doi => "10.1371/journal.pone.0000001")
-      stub = stub_request(:get, /.*wiki/).to_return(:headers => { "Content-Type" => "application/json" }, :body => File.read(fixture_path + 'wikipedia_error.json'), :status => [408])
+      stub = stub_request(:get, /en.wikipedia.org/).to_return(:headers => { "Content-Type" => "application/json" }, :body => File.read(fixture_path + 'wikipedia_error.json'), :status => [408])
       wikipedia.get_data(article, options = { :source_id => wikipedia.id }).should be_nil
       stub.should have_been_requested
       Alert.count.should == 1
