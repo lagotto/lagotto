987eb88a3c1155d75855193911b576d10ada4846
Martin Fenner <mfenner@plos.org>
2014-01-03 11:43:08 +0100

    ALM-594 use memcached to store sessions

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Alm::Application.config.session_store :cookie_store, key: '_alm_session'
+Alm::Application.config.session_store :mem_cache_store, key: '_alm_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
