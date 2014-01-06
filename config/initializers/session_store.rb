4153d5a84d96448a492b719fef71f1414c476018
Martin Fenner <mfenner@plos.org>
2014-01-06 10:00:40 +0100

    ALM-594 don't expire sessions after a day

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store ActionDispatch::Session::CacheStore, :expire_after => 1.day
+Rails.application.config.session_store ActionDispatch::Session::CacheStore
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
