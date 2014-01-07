d0245a97af743fcebaff0ddb552e8bb28822a834
Martin Fenner <mfenner@plos.org>
2014-01-07 14:55:26 +0100

    ALM-401 drop url column

--- db/migrate/20140107120549_add_description_column.rb
+++ db/migrate/20140107120549_add_description_column.rb
@@ -2,14 +2,12 @@ class AddDescriptionColumn < ActiveRecord::Migration
   def up
     add_column :reports, :description, :string
     add_column :reports, :interval, :string
-    add_column :reports, :url, :string
     add_column :reports, :private, :boolean, default: 1
   end
 
   def down
     remove_column :reports, :description
     remove_column :reports, :interval
-    remove_column :reports, :url
     remove_column :reports, :private
   end
 end
