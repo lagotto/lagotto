691d4a645d20f3625a94868e633120f64bd1f716
Martin Fenner <mfenner@plos.org>
2014-01-11 12:36:29 +0100

    ALM-401 allow user to subscribe to monthly report

--- features/step_definitions/report_steps.rb
+++ features/step_definitions/report_steps.rb
@@ -15,15 +15,20 @@ When /^I check the checkbox for the report "(.*?)"$/ do |name|
 end
 
 When /^I uncheck the checkbox for the report "(.*?)"$/ do |name|
+  page.driver.render("tmp/capybara/#{name}.png")
   uncheck name
 end
 
 ### THEN ###
 
 Then /^I should see the report "(.*?)"$/ do |name|
-  page.has_css?('dd', :text => name, :visible => true).should be_true
+  page.has_css?('td', :text => name, :visible => true).should be_true
 end
 
 Then /^I should not see the report "(.*?)"$/ do |name|
-  page.has_css?('dd', :text => name, :visible => true).should_not be_true
+  page.has_css?('td', :text => name, :visible => true).should_not be_true
+end
+
+Then(/^I should see the "(.*?)" link$/) do |name|
+  page.has_link?(name).should be_true
 end
