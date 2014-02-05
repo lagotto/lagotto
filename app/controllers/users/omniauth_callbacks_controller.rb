b66e1a36021a82f48645f65c97ca361f3f3d8f71
Martin Fenner <mfenner@plos.org>
2014-02-05 13:28:33 +0100

    remove obsolete code for Github OAuth

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,18 +1,7 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
-      
-  def github
-    @user = User.find_for_github_oauth(request.env["omniauth.auth"], current_user)
-
-    if @user.persisted?
-      sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
-    else
-      session["devise.github_data"] = request.env["omniauth.auth"]
-      redirect_to root_url
-    end
-  end
 
   def persona
-    @user = User.find_for_persona_oauth(request.env["omniauth.auth"], current_user)
+    @user = User.find_for_persona_oauth(request.env["omniauth.auth"], current_user) if request.env["omniauth.auth"]
 
     if @user.persisted?
       sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
