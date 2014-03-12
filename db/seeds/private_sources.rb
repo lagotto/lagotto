6b018ee4fcb67a256dfcdf8a81dd28709c07f134
mfenner <mfenner@plos.org>
2014-03-12 23:15:40 +0100

    removed duplicate factory for scopus

--- db/seeds/private_sources.rb
+++ db/seeds/private_sources.rb
@@ -11,7 +11,6 @@ counter = Counter.find_or_create_by_name(
   :name => "counter",
   :display_name => "Counter",
   :description => "Usage stats from the PLOS website",
-  :state_event => "activate",
   :queueable => false,
   :group_id => viewed.id)
 wos = Wos.find_or_create_by_name(
@@ -21,25 +20,15 @@ wos = Wos.find_or_create_by_name(
   :private => 1,
   :workers => 1,
   :group_id => cited.id)
-scopus = Scopus.find_or_create_by_name(
-  :name => "scopus",
-  :display_name => "Scopus",
-  :description => "The world's largest abstract and citation database of peer-reviewed literature.",
-  :group_id => cited.id,
-  :username => "<%= node[:alm][:scopus][:username] %>",
-  :salt => "<%= node[:alm][:scopus][:salt] %>",
-  :partner_id => "<%= node[:alm][:scopus][:partner_id] %>")
 f1000 = F1000.find_or_create_by_name(
   :name => "f1000",
   :display_name => "F1000Prime",
   :description => "Post-publication peer review of the biomedical literature.",
-  :state_event => "install",
   :group_id => recommended.id)
 figshare = Figshare.find_or_create_by_name(
   :name => "figshare",
   :display_name => "Figshare",
   :description => "Figures, tables and supplementary files hosted by figshare",
-  :state_event => "install",
   :group_id => viewed.id)
 articleconverage = ArticleCoverage.find_or_create_by_name(
   :name => "articlecoverage",
@@ -50,13 +39,11 @@ articlecoveragecurated = ArticleCoverageCurated.find_or_create_by_name(
   :name => "articlecoveragecurated",
   :display_name => "Article Coverage Curated",
   :description => "Article Coverage Curated",
-  :state_event => "activate",
   :group_id => discussed.id)
 plos_comments = PlosComments.find_or_create_by_name(
   :name => "plos_comments",
   :display_name => "Journal Comments",
   :description => "Comments from the PLOS website.",
-  :state_event => "activate",
   :group_id => discussed.id)
 
 # These sources are retired, but we need to keep them around for the data we collected
