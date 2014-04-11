9630bbdf6d9aeabb60eb7e82b1710b8091b825fd
mfenner <mfenner@plos.org>
2014-04-11 11:15:01 +0200

    use key for session cookie

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,9 +1,2 @@
 # Be sure to restart your server when you modify this file.
-
-#Rails.application.config.session_store ActionDispatch::Session::CacheStore
-Rails.application.config.session_store ActionDispatch::Session::CookieStore
-
-# Use the database for sessions instead of the cookie-based default,
-# which shouldn't be used to store highly confidential information
-# (create the session table with "rails generate session_migration")
-# Alm::Application.config.session_store :active_record_store
+Alm::Application.config.session_store :cookie_store, key: '_alm'
