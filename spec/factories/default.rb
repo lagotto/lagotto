2e5566ae321bc0ccaf3d324557e9d9a46bb016dd
mfenner <mfenner@plos.org>
2014-04-11 14:19:15 +0200

    ALM-657 reorganized factories

--- spec/factories/default.rb
+++ spec/factories/default.rb
@@ -2,11 +2,119 @@
 
 FactoryGirl.define do
 
-  factory :group do
-    name 'saved'
-    display_name 'Saved'
+  factory :article do
+    sequence(:doi) {|n| "10.1371/journal.pone.00000#{n}" }
+    sequence(:pmid) {|n| "1897483#{n}" }
+    pmcid 2568856
+    mendeley_uuid "46cb51a0-6d08-11df-afb8-0026b95d30b2"
+    title 'Defrosting the Digital Library: Bibliographic Tools for the Next Generation Web'
+    year { Time.zone.now.year - 1 }
+    month { Time.zone.now.month }
+    day { Time.zone.now.day }
 
-    initialize_with { Group.find_or_create_by_name(name) }
+    trait(:cited) { doi '10.1371/journal.pone.0000001' }
+    trait(:uncited) { doi '10.1371/journal.pone.0000002' }
+    trait(:not_publisher) { doi '10.1007/s00248-010-9734-2' }
+    trait(:missing_mendeley) { mendeley_uuid nil }
+
+    factory :article_with_events do
+      retrieval_statuses { |article| [article.association(:retrieval_status)] }
+    end
+
+    factory :article_with_events_and_alerts do
+      retrieval_statuses { |article| [article.association(:retrieval_status)] }
+      alerts { |article| [article.association(:alert)] }
+    end
+
+    factory :article_for_feed do
+      year { Time.zone.now.year }
+      day { Time.zone.now.day - 1 }
+      retrieval_statuses { |article| [article.association(:retrieval_status, retrieved_at: Time.zone.today - 1.day)] }
+    end
+
+    factory :article_published_today do
+      year { Time.zone.now.year }
+      retrieval_statuses { |article| [article.association(:retrieval_status, retrieved_at: Time.zone.today)] }
+    end
+
+    factory :article_with_errors do
+      retrieval_statuses { |article| [article.association(:retrieval_status, :with_errors)] }
+    end
+
+    factory :article_with_private_citations do
+      retrieval_statuses { |article| [article.association(:retrieval_status, :with_private)] }
+    end
+
+    factory :article_with_crossref_citations do
+      retrieval_statuses { |article| [article.association(:retrieval_status, :with_crossref)] }
+    end
+
+    factory :article_with_pubmed_citations do
+      retrieval_statuses { |article| [article.association(:retrieval_status, :with_pubmed)] }
+    end
+
+    factory :article_with_mendeley_events do
+      retrieval_statuses { |article| [article.association(:retrieval_status, :with_mendeley)] }
+    end
+
+    factory :article_with_nature_citations do
+      retrieval_statuses { |article| [article.association(:retrieval_status, :with_pubmed)] }
+    end
+
+    factory :article_with_researchblogging_citations do
+      retrieval_statuses { |article| [article.association(:retrieval_status, :with_researchblogging)] }
+    end
+
+    factory :article_with_wos_citations do
+      retrieval_statuses { |article| [article.association(:retrieval_status, :with_wos)] }
+    end
+
+    factory :article_with_counter_citations do
+      retrieval_statuses { |article| [article.association(:retrieval_status, :with_counter)] }
+    end
+
+    factory :article_with_tweets do
+      retrieval_statuses { |article| [article.association(:retrieval_status, :with_twitter_search)] }
+    end
+  end
+
+  factory :retrieval_status do
+    event_count 50
+    retrieved_at { Time.zone.now - 1.month }
+    sequence(:scheduled_at) {|n| Time.zone.now - 1.day + n.minutes }
+
+    association :article
+    association :source, factory: :citeulike
+
+    trait(:missing_mendeley) do
+      association :article, :missing_mendeley, factory: :article
+      association :source, factory: :mendeley
+    end
+    trait(:staleness) { association :source, factory: :citeulike }
+    trait(:with_errors) { event_count 0 }
+    trait(:with_private) { association :source, private: true }
+    trait(:with_crossref) { association :source, factory: :cross_ref }
+    trait(:with_mendeley) { association :source, factory: :mendeley }
+    trait(:with_pubmed) { association :source, factory: :pub_med }
+    trait(:with_nature) { association :source, factory: :nature }
+    trait(:with_wos) { association :source, factory: :wos }
+    trait(:with_researchblogging) { association :source, factory: :researchblogging }
+    trait(:with_scienceseeker) { association :source, factory: :scienceseeker }
+    trait(:with_wikipedia) { association :source, factory: :wikipedia }
+    trait(:with_counter) { association :source, factory: :counter }
+    trait(:with_twitter_search) { association :source, factory: :twitter_search }
+    trait(:with_article_published_today) { association :article, factory: :article_published_today }
+
+    before(:create) do |retrieval_status|
+      FactoryGirl.create(:retrieval_history,
+                          retrieved_at: Time.zone.today - 2.years + 1.day,
+                          event_count: 50,
+                          retrieval_status: retrieval_status,
+                          article: retrieval_status.article,
+                          source: retrieval_status.source)
+    end
+
+    initialize_with { RetrievalStatus.find_or_create_by_article_id_and_source_id(article.id, source.id) }
   end
 
   factory :delayed_job do
@@ -66,248 +174,6 @@ FactoryGirl.define do
     status { event_count > 0 ? "SUCCESS" : "ERROR" }
   end
 
-  factory :citeulike, aliases: [:source], class: Citeulike do
-    type "Citeulike"
-    name "citeulike"
-    display_name "CiteULike"
-    state_event "activate"
-
-    cached_at { Time.zone.now - 10.minutes }
-
-    group
-
-    initialize_with { Citeulike.find_or_create_by_name(name) }
-
-    factory :source_with_api_responses do
-      ignore do
-        api_responses_count 5
-      end
-
-      after(:create) do |source, evaluator|
-        FactoryGirl.create_list(:api_response, evaluator.api_responses_count, source: source)
-      end
-    end
-  end
-
-  factory :copernicus, class: Copernicus do
-    type "Copernicus"
-    name "copernicus"
-    display_name "Copernicus"
-    state_event "activate"
-    url "http://harvester.copernicus.org/api/v1/articleStatisticsDoi/doi:%{doi}"
-    username "EXAMPLE"
-    password "EXAMPLE"
-
-    group
-
-    initialize_with { Copernicus.find_or_create_by_name(name) }
-  end
-
-  factory :cross_ref, class: CrossRef do
-    type "CrossRef"
-    name "crossref"
-    display_name "CrossRef"
-    state_event "activate"
-    username "EXAMPLE"
-    password "EXAMPLE"
-
-    group
-
-    initialize_with { CrossRef.find_or_create_by_name(name) }
-  end
-
-  factory :nature, class: Nature do
-    type "Nature"
-    name "nature"
-    display_name "Nature"
-    state_event "activate"
-
-    group
-
-    initialize_with { Nature.find_or_create_by_name(name) }
-  end
-
-  factory :openedition, class: Openedition do
-    type "Openedition"
-    name "openedition"
-    display_name "OpenEdition"
-    state_event "activate"
-
-    group
-
-    initialize_with { Openedition.find_or_create_by_name(name) }
-  end
-
-  factory :pmc, class: Pmc do
-    type "Pmc"
-    name "pmc"
-    display_name "PubMed Central Usage Stats"
-    state_event "activate"
-    url "http://127.0.0.1:5984/pmc_usage_stats_test/"
-    journals "ajrccm"
-    username "EXAMPLE"
-    password "EXAMPLE"
-
-    group
-
-    initialize_with { Pmc.find_or_create_by_name(name) }
-  end
-
-  factory :pub_med, class: PubMed do
-    type "PubMed"
-    name "pubmed"
-    display_name "PubMed"
-    state_event "activate"
-
-    group
-
-    initialize_with { PubMed.find_or_create_by_name(name) }
-  end
-
-  factory :pmc_europe, class: PmcEurope do
-    type "PmcEurope"
-    name "pmceurope"
-    display_name "PMC Europe Citations"
-    state_event "activate"
-
-    group
-
-    initialize_with { PmcEurope.find_or_create_by_name(name) }
-  end
-
-    factory :pmc_europe_data, class: PmcEuropeData do
-    type "PmcEuropeData"
-    name "pmceuropedata"
-    display_name "PMC Europe Database Citations"
-    state_event "activate"
-
-    group
-
-    initialize_with { PmcEuropeData.find_or_create_by_name(name) }
-  end
-
-  factory :researchblogging, class: Researchblogging do
-    type "Researchblogging"
-    name "researchblogging"
-    display_name "Research Blogging"
-    state_event "activate"
-    username "EXAMPLE"
-    password "EXAMPLE"
-
-    group
-
-    initialize_with { Researchblogging.find_or_create_by_name(name) }
-  end
-
-  factory :science_seeker, class: ScienceSeeker do
-    type "ScienceSeeker"
-    name "scienceseeker"
-    display_name "ScienceSeeker"
-    state_event "activate"
-
-    group
-
-    initialize_with { ScienceSeeker.find_or_create_by_name(name) }
-  end
-
-  factory :datacite, class: Datacite do
-    type "Datacite"
-    name "datacite"
-    display_name "DataCite"
-    state_event "activate"
-
-    group
-
-    initialize_with { Datacite.find_or_create_by_name(name) }
-  end
-
-  factory :wordpress, class: Wordpress do
-    type "Wordpress"
-    name "wordpress"
-    display_name "Wordpress.com"
-    state_event "activate"
-
-    group
-
-    initialize_with { Wordpress.find_or_create_by_name(name) }
-  end
-
-  factory :reddit, class: Reddit do
-    type "Reddit"
-    name "reddit"
-    display_name "Reddit"
-    state_event "activate"
-
-    group
-
-    initialize_with { Reddit.find_or_create_by_name(name) }
-  end
-
-  factory :twitter_search, class: TwitterSearch do
-    type "TwitterSearch"
-    name "twitter_search"
-    display_name "Twitter"
-    state_event "activate"
-    api_key "EXAMPLE"
-    api_secret "EXAMPLE"
-    access_token "EXAMPLE"
-
-    group
-
-    initialize_with { TwitterSearch.find_or_create_by_name(name) }
-  end
-
-  factory :wikipedia, class: Wikipedia do
-    type "Wikipedia"
-    name "wikipedia"
-    display_name "Wikipedia"
-    state_event "activate"
-    languages "en"
-
-    group
-
-    initialize_with { Wikipedia.find_or_create_by_name(name) }
-  end
-
-  factory :mendeley, class: Mendeley do
-    type "Mendeley"
-    name "mendeley"
-    display_name "Mendeley"
-    state_event "activate"
-    client_id "EXAMPLE"
-    secret "EXAMPLE"
-    access_token "EXAMPLE"
-    expires_at { Time.zone.now + 1.hour }
-
-    group
-
-    initialize_with { Mendeley.find_or_create_by_name(name) }
-  end
-
-  factory :facebook, class: Facebook do
-    type "Facebook"
-    name "facebook"
-    display_name "Facebook"
-    state_event "activate"
-    access_token "EXAMPLE"
-
-    group
-
-    initialize_with { Facebook.find_or_create_by_name(name) }
-  end
-
-  factory :scopus, class: Scopus do
-    type "Scopus"
-    name "scopus"
-    display_name "Scopus"
-    api_key "EXAMPLE"
-    insttoken "EXAMPLE"
-
-    group
-
-    initialize_with { Scopus.find_or_create_by_name(name) }
-  end
-
   factory :alert do
     exception "An exception"
     class_name "Net::HTTPRequestTimeOut"
@@ -347,6 +213,31 @@ FactoryGirl.define do
     message "Found 0 article not updated errors in 29,899 API responses, taking 29.899 ms"
     input 10
     created_at { Time.zone.now }
+  end
+
+  factory :user do
+    sequence(:username) {|n| "joesmith#{n}" }
+    sequence(:name) {|n| "Joe Smith#{n}" }
+    sequence(:email) {|n| "joe#{n}@example.com" }
+    password "joesmith"
+    sequence(:authentication_token) {|n| "q9pWP8QxzkR24Mvs9BEy#{n}" }
+    role "admin"
+    provider "cas"
+    uid "12345"
+
+    factory :admin_user do
+      role "admin"
+      authentication_token "12345"
+    end
+  end
+
+  factory :html_ratio_too_high_error, class: HtmlRatioTooHighError do
+    type "HtmlRatioTooHighError"
+    name "HtmlRatioTooHighError"
+    display_name "html ratio too high error"
+    active true
+
+    initialize_with { HtmlRatioTooHighError.find_or_create_by_name(name) }
   end
 
   factory :article_not_updated_error, aliases: [:filter], class: ArticleNotUpdatedError do
---
+++ spec/factories/sources.rb
@@ -0,0 +1,375 @@
+# encoding: UTF-8
+
+FactoryGirl.define do
+
+  factory :group do
+    name 'saved'
+    display_name 'Saved'
+
+    initialize_with { Group.find_or_create_by_name(name) }
+  end
+
+  factory :citeulike, aliases: [:source], class: Citeulike do
+    type "Citeulike"
+    name "citeulike"
+    display_name "CiteULike"
+    state_event "activate"
+
+    cached_at { Time.zone.now - 10.minutes }
+
+    group
+
+    initialize_with { Citeulike.find_or_create_by_name(name) }
+
+    factory :source_with_api_responses do
+      ignore do
+        api_responses_count 5
+      end
+
+      after(:create) do |source, evaluator|
+        FactoryGirl.create_list(:api_response, evaluator.api_responses_count, source: source)
+      end
+    end
+  end
+
+  factory :copernicus, class: Copernicus do
+    type "Copernicus"
+    name "copernicus"
+    display_name "Copernicus"
+    state_event "activate"
+    url "http://harvester.copernicus.org/api/v1/articleStatisticsDoi/doi:%{doi}"
+    username "EXAMPLE"
+    password "EXAMPLE"
+
+    group
+
+    initialize_with { Copernicus.find_or_create_by_name(name) }
+  end
+
+  factory :cross_ref, class: CrossRef do
+    type "CrossRef"
+    name "crossref"
+    display_name "CrossRef"
+    state_event "activate"
+    username "EXAMPLE"
+    password "EXAMPLE"
+
+    group
+
+    initialize_with { CrossRef.find_or_create_by_name(name) }
+  end
+
+  factory :nature, class: Nature do
+    type "Nature"
+    name "nature"
+    display_name "Nature"
+    state_event "activate"
+
+    group
+
+    initialize_with { Nature.find_or_create_by_name(name) }
+  end
+
+  factory :openedition, class: Openedition do
+    type "Openedition"
+    name "openedition"
+    display_name "OpenEdition"
+    state_event "activate"
+
+    group
+
+    initialize_with { Openedition.find_or_create_by_name(name) }
+  end
+
+  factory :pmc, class: Pmc do
+    type "Pmc"
+    name "pmc"
+    display_name "PubMed Central Usage Stats"
+    state_event "activate"
+    url "http://127.0.0.1:5984/pmc_usage_stats_test/"
+    journals "ajrccm"
+    username "EXAMPLE"
+    password "EXAMPLE"
+
+    group
+
+    initialize_with { Pmc.find_or_create_by_name(name) }
+  end
+
+  factory :pub_med, class: PubMed do
+    type "PubMed"
+    name "pubmed"
+    display_name "PubMed"
+    state_event "activate"
+
+    group
+
+    initialize_with { PubMed.find_or_create_by_name(name) }
+  end
+
+  factory :pmc_europe, class: PmcEurope do
+    type "PmcEurope"
+    name "pmceurope"
+    display_name "PMC Europe Citations"
+    state_event "activate"
+
+    group
+
+    initialize_with { PmcEurope.find_or_create_by_name(name) }
+  end
+
+    factory :pmc_europe_data, class: PmcEuropeData do
+    type "PmcEuropeData"
+    name "pmceuropedata"
+    display_name "PMC Europe Database Citations"
+    state_event "activate"
+
+    group
+
+    initialize_with { PmcEuropeData.find_or_create_by_name(name) }
+  end
+
+  factory :researchblogging, class: Researchblogging do
+    type "Researchblogging"
+    name "researchblogging"
+    display_name "Research Blogging"
+    state_event "activate"
+    username "EXAMPLE"
+    password "EXAMPLE"
+
+    group
+
+    initialize_with { Researchblogging.find_or_create_by_name(name) }
+  end
+
+  factory :science_seeker, class: ScienceSeeker do
+    type "ScienceSeeker"
+    name "scienceseeker"
+    display_name "ScienceSeeker"
+    state_event "activate"
+
+    group
+
+    initialize_with { ScienceSeeker.find_or_create_by_name(name) }
+  end
+
+  factory :datacite, class: Datacite do
+    type "Datacite"
+    name "datacite"
+    display_name "DataCite"
+    state_event "activate"
+
+    group
+
+    initialize_with { Datacite.find_or_create_by_name(name) }
+  end
+
+  factory :wordpress, class: Wordpress do
+    type "Wordpress"
+    name "wordpress"
+    display_name "Wordpress.com"
+    state_event "activate"
+
+    group
+
+    initialize_with { Wordpress.find_or_create_by_name(name) }
+  end
+
+  factory :reddit, class: Reddit do
+    type "Reddit"
+    name "reddit"
+    display_name "Reddit"
+    state_event "activate"
+
+    group
+
+    initialize_with { Reddit.find_or_create_by_name(name) }
+  end
+
+  factory :twitter_search, class: TwitterSearch do
+    type "TwitterSearch"
+    name "twitter_search"
+    display_name "Twitter"
+    state_event "activate"
+    api_key "EXAMPLE"
+    api_secret "EXAMPLE"
+    access_token "EXAMPLE"
+
+    group
+
+    initialize_with { TwitterSearch.find_or_create_by_name(name) }
+  end
+
+  factory :wikipedia, class: Wikipedia do
+    type "Wikipedia"
+    name "wikipedia"
+    display_name "Wikipedia"
+    state_event "activate"
+    languages "en"
+
+    group
+
+    initialize_with { Wikipedia.find_or_create_by_name(name) }
+  end
+
+  factory :mendeley, class: Mendeley do
+    type "Mendeley"
+    name "mendeley"
+    display_name "Mendeley"
+    state_event "activate"
+    client_id "EXAMPLE"
+    secret "EXAMPLE"
+    access_token "EXAMPLE"
+    expires_at { Time.zone.now + 1.hour }
+
+    group
+
+    initialize_with { Mendeley.find_or_create_by_name(name) }
+  end
+
+  factory :facebook, class: Facebook do
+    type "Facebook"
+    name "facebook"
+    display_name "Facebook"
+    state_event "activate"
+    access_token "EXAMPLE"
+
+    group
+
+    initialize_with { Facebook.find_or_create_by_name(name) }
+  end
+
+  factory :scopus, class: Scopus do
+    type "Scopus"
+    name "scopus"
+    display_name "Scopus"
+    api_key "EXAMPLE"
+    insttoken "EXAMPLE"
+
+    group
+
+    initialize_with { Scopus.find_or_create_by_name(name) }
+  end
+
+  factory :counter, class: Counter do
+    type "Counter"
+    name "counter"
+    display_name "Counter"
+    state_event "activate"
+    url "http://example.org?doi=%{doi}"
+
+    group
+
+    initialize_with { Counter.find_or_create_by_name(name) }
+  end
+
+  factory :biod, class: Biod do
+    type "Biod"
+    name "biod"
+    display_name "Biod"
+    state_event "activate"
+    url "http://example.org?doi=%{doi}"
+
+    group
+
+    initialize_with { Counter.find_or_create_by_name(name) }
+  end
+
+  factory :f1000, class: F1000 do
+    type "F1000"
+    name "f1000"
+    display_name "F1000Prime"
+    state_event "activate"
+    url "http://example.org/example.xml"
+    filename "example.xml"
+
+    group
+
+    initialize_with { F1000.find_or_create_by_name(name) }
+  end
+
+  factory :figshare, class: Figshare do
+    type "Figshare"
+    name "figshare"
+    display_name "Figshare"
+    state_event "activate"
+    url "http://api.figshare.com/v1/publishers/search_for?doi=%{doi}"
+
+    group
+
+    initialize_with { Figshare.find_or_create_by_name(name) }
+  end
+
+  factory :plos_comments, class: PlosComments do
+    type "PlosComments"
+    name "plos_comments"
+    display_name "PLOS Comments"
+    state_event "activate"
+    url "http://example.org?doi={doi}"
+
+    group
+
+    initialize_with { PlosComments.find_or_create_by_name(name) }
+  end
+
+  factory :twitter, class: Twitter do
+    type "Twitter"
+    name "twitter"
+    display_name "Twitter"
+    state_event "activate"
+    url "http://example.org?doi=%{doi}"
+
+    group
+
+    initialize_with { Twitter.find_or_create_by_name(name) }
+  end
+
+  factory :wos, class: Wos do
+    type "Wos"
+    name "wos"
+    display_name "Web of Science"
+    state_event "activate"
+    private true
+    url "https://ws.isiknowledge.com:80/cps/xrpc"
+
+    group
+
+    initialize_with { Wos.find_or_create_by_name(name) }
+  end
+
+  factory :relative_metric, class: RelativeMetric do
+    type "RelativeMetric"
+    name "relativemetric"
+    display_name "Relative Metric"
+    state_event "activate"
+    url "http://example.org?doi=%{doi}"
+
+    group
+
+    initialize_with { RelativeMetric.find_or_create_by_name(name) }
+  end
+
+  factory :article_coverage, class: ArticleCoverage do
+    type "ArticleCoverage"
+    name "articlecoverage"
+    display_name "Article Coverage"
+    state_event "activate"
+    url "http://example.org?doi=%{doi}"
+
+    group
+
+    initialize_with { ArticleCoverage.find_or_create_by_name(name) }
+  end
+
+  factory :article_coverage_curated, class: ArticleCoverageCurated do
+    type "ArticleCoverageCurated"
+    name "articlecoveragecurated"
+    display_name "Article Coverage Curated"
+    state_event "activate"
+    url "http://example.org?doi=%{doi}"
+
+    group
+
+    initialize_with { ArticleCoverageCurated.find_or_create_by_name(name) }
+  end
+end
