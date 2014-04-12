f6df151a0f5878813b6d398ee5fd4002874aa600
mfenner <mfenner@plos.org>
2014-04-12 06:12:14 +0200

    ALM-657 small changes to ALM cookbook

--- vendor/cookbooks/alm/metadata.rb
+++ vendor/cookbooks/alm/metadata.rb
@@ -4,7 +4,7 @@ maintainer_email  "mfenner@plos.org"
 license           "Apache 2.0"
 description       "Configures ALM server"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           "1.6.1"
+version           "1.6.2"
 depends           "git"
 depends           "mysql"
 depends           "database"
