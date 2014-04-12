d1dc80ee0e93c0afce4ad081766f7deb26f44ee1
mfenner <mfenner@plos.org>
2014-04-12 16:11:09 +0200

    ALM-657 set path in whenever

--- config/schedule.rb
+++ config/schedule.rb
@@ -3,9 +3,8 @@
 # It's helpful, but not entirely necessary to understand cron before proceeding.
 # http://en.wikipedia.org/wiki/Cron
 
-#env :PATH, ENV['PATH']
+set :path, ENV['PATH']
 set :environment, ENV['RAILS_ENV']
-
 set :output, "#{path}/log/cron.log"
 
 # Schedule jobs
