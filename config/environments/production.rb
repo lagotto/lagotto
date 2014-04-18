4293bbb8344a118189fe8ad1218619dc970ebb59
Martin Fenner <mfenner@plos.org>
2014-04-18 16:45:37 +0200

    ALM-640 started to move d3 scripts into javascript assets folder

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -43,7 +43,7 @@ Alm::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  # config.assets.precompile += %w()
+  config.assets.precompile += %w( articles/* status/* )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
