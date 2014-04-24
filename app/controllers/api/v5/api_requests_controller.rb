93048fc258e49547421e62c6a32e6d4e470a4096
Martin Fenner <mfenner@plos.org>
2014-04-24 21:09:51 +0200

    ALM-657 added Postgres version of api_requests call

--- app/controllers/api/v5/api_requests_controller.rb
+++ app/controllers/api/v5/api_requests_controller.rb
@@ -2,14 +2,26 @@ class Api::V5::ApiRequestsController < Api::V5::BaseController
   load_and_authorize_resource
 
   def index
-    if params[:key] == "local"
-      collection = ApiRequest.where("api_key = ?", CONFIG[:api_key])
-    elsif params[:key] == "external"
-      collection = ApiRequest.where("api_key != ?", CONFIG[:api_key])
-    elsif params[:key]
-      collection = ApiRequest.where("api_key = ?", params[:key])
+    if ActiveRecord::Base.configurations[Rails.env]['adapter'] == "mysql2"
+      if params[:key] == "local"
+        collection = ApiRequest.where("api_key = ?", CONFIG[:api_key])
+      elsif params[:key] == "external"
+        collection = ApiRequest.where("api_key != ?", CONFIG[:api_key])
+      elsif params[:key]
+        collection = ApiRequest.where("api_key = ?", params[:key])
+      else
+        collection = ApiRequest.where("ids LIKE ?", "Api::%")
+      end
     else
-      collection = ApiRequest.where("ids LIKE ?", "Api::%")
+      if params[:key] == "local"
+        collection = ApiRequest.where("api_key = '?'", CONFIG[:api_key])
+      elsif params[:key] == "external"
+        collection = ApiRequest.where("api_key != '?'", CONFIG[:api_key])
+      elsif params[:key]
+        collection = ApiRequest.where("api_key = '?'", params[:key])
+      else
+        collection = ApiRequest.where("ids LIKE ?", "Api::%")
+      end
     end
 
     @api_requests = collection.order("created_at DESC").paginate(:page => params[:page])
