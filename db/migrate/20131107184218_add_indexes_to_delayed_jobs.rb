aaacaf3864a3e52824b8c8620056cbfe609e17bc
Martin Fenner <mfenner@plos.org>
2014-04-24 17:36:39 +0200

    ALM-657 don't use index names longer than 63 characters for postgres compatibility

--- db/migrate/20131107184218_add_indexes_to_delayed_jobs.rb
+++ db/migrate/20131107184218_add_indexes_to_delayed_jobs.rb
@@ -6,6 +6,6 @@ class AddIndexesToDelayedJobs < ActiveRecord::Migration
     # add index for this too: SELECT COUNT(`delayed_jobs`.`id`) FROM `delayed_jobs`  WHERE (queue = 'crossref');
     add_index :delayed_jobs, [:queue], :name => 'index_delayed_jobs_queue'
 
-    add_index :delayed_jobs, [:run_at, :locked_at, :locked_by, :failed_at, :priority], :name => 'index_delayed_jobs_run_at_locked_at_locked_by_failed_at_priority'
+    add_index :delayed_jobs, [:run_at, :locked_at, :locked_by, :failed_at, :priority], :name => 'index_delayed_jobs_run_at_locked_at_failed_at_priority'
   end
 end
