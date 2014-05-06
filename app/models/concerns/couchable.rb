5ba4b4dccb22303608c35435cafee20f54646ac5
Martin Fenner <mfenner@plos.org>
2014-05-06 20:18:44 +0200

    small code refactoring

--- app/models/concerns/couchable.rb
+++ app/models/concerns/couchable.rb
@@ -40,6 +40,7 @@ module Couchable
       conn.basic_auth(options[:username], options[:password]) if options[:username]
       conn.options[:timeout] = options[:timeout]
       response = conn.head url
+
       # CouchDB revision is in etag header. We need to remove extra double quotes
       { rev: response.env[:response_headers][:etag][1..-2] }
     rescue *NETWORKABLE_EXCEPTIONS => e
@@ -58,17 +59,14 @@ module Couchable
 
     def put_alm_data(url, options = { data: nil })
       return nil unless options[:data] || Rails.env.test?
+
       conn = faraday_conn('json')
       conn.options[:timeout] = DEFAULT_TIMEOUT
       response = conn.put url do |request|
         request.body = options[:data]
       end
-      if is_json?(response.body)
-        json = JSON.parse(response.body)
-        json['ok'] ? json['rev'] : nil
-      else
-        { error: 'malformed JSON response' }
-      end
+
+      parse_rev(response.body)
     rescue *NETWORKABLE_EXCEPTIONS => e
       rescue_faraday_error(url, e, options)
     end
@@ -82,12 +80,8 @@ module Couchable
       return nil unless url != couchdb_url || Rails.env.test?
       conn = faraday_conn('json')
       response = conn.delete url
-      if is_json?(response.body)
-        json = JSON.parse(response.body)
-        json['ok'] ? json['rev'] : nil
-      else
-        { error: 'malformed JSON response' }
-      end
+
+      parse_rev(response.body)
     rescue *NETWORKABLE_EXCEPTIONS => e
       rescue_faraday_error(url, e, options)
     end
@@ -109,5 +103,13 @@ module Couchable
       delete_alm_data(couchdb_url)
     end
 
+    def parse_rev(string)
+      if is_json?(string)
+        json = JSON.parse(string)
+        json['ok'] ? json['rev'] : nil
+      else
+        { error: 'malformed JSON response' }
+      end
+    end
   end
 end
