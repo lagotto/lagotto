5638e179cf76ab2715d7b8fdd2c199ac3c7af899
Martin Fenner <mfenner@plos.org>
2014-05-23 19:32:13 -0400

    remove unnecessary begin block

--- app/middleware/catch_json_parse_errors.rb
+++ app/middleware/catch_json_parse_errors.rb
@@ -4,18 +4,16 @@ class CatchJsonParseErrors
   end
 
   def call(env)
-    begin
-      @app.call(env)
-    rescue MultiJson::LoadError => error
-      if env['HTTP_ACCEPT'] =~ /application\/json/
-        error_output = "There was a problem in the JSON you submitted: #{error}"
-        return [
-          400, { "Content-Type" => "application/json" },
-          [ { status: 400, error: error_output }.to_json ]
-        ]
-      else
-        raise error
-      end
+    @app.call(env)
+  rescue MultiJson::LoadError => error
+    if env['HTTP_ACCEPT'] =~ /application\/json/
+      error_output = "There was a problem in the JSON you submitted: #{error}"
+      return [
+        400, { "Content-Type" => "application/json" },
+        [ { status: 400, error: error_output }.to_json ]
+      ]
+    else
+      raise error
     end
   end
 end
