acc58d2e1b7c133734528afebe42037a28261263
Martin Fenner <mfenner@plos.org>
2014-07-08 19:52:13 +0200

    ALM-616 use different permissions for publisher role

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -12,8 +12,10 @@ class Ability
       can :read, :all
       can :destroy, Alert
       can :create, Article
-      can :update, User, :id => user.id
-    elsif user.role == "user" || user.role == "publisher"
+      can :manage, User, :id => user.id
+    elsif user.role == "publisher"
+      can :manage, User, :id => user.id
+    elsif user.role == "user"
       can [:update, :show], User, :id => user.id
     else
     end
