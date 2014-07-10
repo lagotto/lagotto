9d2dc56b8f2cc22ad59827c039faf18cd8f6e986
Martin Fenner <mfenner@plos.org>
2014-07-10 10:36:58 +0200

    ALM-682 moved status controller out of admin area

--- app/controllers/status_controller.rb
+++ app/controllers/status_controller.rb
@@ -16,10 +16,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-class Admin::StatusController < Admin::ApplicationController
-  load_and_authorize_resource :alert, :parent => false
-
+class StatusController < ApplicationController
   def index
-    @workers = Worker.all
+
   end
 end
