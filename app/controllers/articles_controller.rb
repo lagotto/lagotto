46ca55a0e43acbfa913ababb2ea6684df2b965ed
Martin Fenner <mfenner@plos.org>
2014-07-10 12:45:50 +0200

    ALM-682 skip authorization for articles/index

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -20,6 +20,7 @@ class ArticlesController < ApplicationController
   before_filter :load_article, :only => [ :show, :edit, :update, :destroy ]
   before_filter :new_article, :only => [:create]
   load_and_authorize_resource
+  skip_authorize_resource :only => [:show, :index]
 
   respond_to :html, :js
 
