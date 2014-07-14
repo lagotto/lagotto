1496473db99d1d45a7a622e1d7c9b67df7a5e4c8
Martin Fenner <mfenner@plos.org>
2014-07-14 12:17:00 +0200

    ALM-678 don't filter sources

--- app/controllers/concerns/articable.rb
+++ app/controllers/concerns/articable.rb
@@ -28,7 +28,7 @@ module Articable
       # Translate type query parameter into column name
       # Paginate query results (50 per page)
       source_ids = get_source_ids(params[:source])
-      collection = collection = ArticleDecorator.joins(:retrieval_statuses).where(:retrieval_statuses => { :source_id => source_ids })
+      collection = collection = ArticleDecorator.joins(:retrieval_statuses)
 
       if params[:ids]
         type = ["doi", "pmid", "pmcid", "mendeley_uuid"].find { |t| t == params[:type] } || Article.uid
