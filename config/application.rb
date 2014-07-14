0b41cdaf3df06ba5dddae9805e8e27e30c939f35
Martin Fenner <mfenner@plos.org>
2014-07-14 08:05:30 +0200

    removed middleware that checks JSON parsing errors

--- config/application.rb
+++ config/application.rb
@@ -70,8 +70,5 @@ module Alm
 
     # Disable IP spoofing check
     config.action_dispatch.ip_spoofing_check = false
-
-    # Catch JSON parse errors
-    config.middleware.insert_before ActionDispatch::ParamsParser, "CatchJsonParseErrors"
   end
 end
