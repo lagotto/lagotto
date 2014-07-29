64d41ff21bee160d94bf118846b875aafe04f62c
Martin Fenner <mfenner@plos.org>
2014-07-29 11:58:25 +0200

    ALM-686 can't use rails variable in capistrano

--- config/deploy/development.rb
+++ config/deploy/development.rb
@@ -20,6 +20,4 @@ role :db,  %w{33.33.33.44}
 set :ssh_options, user: "vagrant", keys: %w(~/.vagrant.d/insecure_private_key), auth_methods: %w(publickey)
 
 # Set number of delayed_job workers
-if CONFIG[:workers]
-  set :delayed_job_args, "-n #{CONFIG[:workers]}"
-end
+set :delayed_job_args, "-n 3"
