a819545659458108d057078688e97548d260bf11
Martin Fenner <mfenner@plos.org>
2014-08-01 10:02:22 +0200

    ALM-213 use separate cache_key for oembed

--- app/decorators/article_decorator.rb
+++ app/decorators/article_decorator.rb
@@ -7,6 +7,9 @@ class ArticleDecorator < Draper::Decorator
     PaginatingDecorator
   end
 
+  # Filter by source parameter, filter out private sources unless admin
+  # source_ids = get_source_ids(params[:source])
+
   def publication_date
     published_on.nil? ? nil : published_on.to_time.utc.iso8601
   end
@@ -25,9 +28,14 @@ class ArticleDecorator < Draper::Decorator
 
   def cache_key
     { :article_id => id,
-      :timestamp => updated_at.to_s(:number),
+      :update_date => updated_at.utc.iso8601,
       :source => context[:source],
       :info => context[:info] }
+  end
+
+  def oembed_key
+    { :article_id => id,
+      :update_date => updated_at.utc.iso8601 }
   end
 
   def version
--- app/views/oembed/show.rabl
+++ app/views/oembed/show.rabl
@@ -1,5 +1,5 @@
 object @article => :oembed
-cache ['oembed', @article]
+cache ['oembed', @article.oembed_key]
 
 attributes :type, :version, :width, :height, :provider_name, :provider_url, :title, :html
 attribute :doi_as_url => :url
