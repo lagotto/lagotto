9672e8354dbab6ecc4d63d30e93f8f76fbfe5b3c
Martin Fenner <mfenner@plos.org>
2014-08-19 21:21:45 +0200

    ALM-704 added pagination

--- app/decorators/alert_decorator.rb
+++ app/decorators/alert_decorator.rb
@@ -2,6 +2,10 @@ class AlertDecorator < Draper::Decorator
   delegate_all
   decorates_finders
 
+  def self.collection_decorator_class
+    PaginatingDecorator
+  end
+
   def level
     model.human_level_name
   end
