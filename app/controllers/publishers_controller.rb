e23cbd9f31f9de2df0f46dfcfb64645f15ab60ed
Martin Fenner <mfenner@plos.org>
2014-08-22 02:24:04 +0200

    ALM-616 only show other names

--- app/controllers/publishers_controller.rb
+++ app/controllers/publishers_controller.rb
@@ -1,6 +1,6 @@
 class PublishersController < ApplicationController
   before_filter :load_publisher, :only => [:show, :update, :destroy]
-  before_filter :load_index, :only => [:index, :create]
+  before_filter :load_index, :only => [:index, :create, :destroy]
   before_filter :new_publisher, :only => [:create]
   load_and_authorize_resource
 
