053519ebcae11d98f78ec440df3add2458e414d1
Martin Fenner <mfenner@plos.org>
2014-08-24 22:21:04 +0200

    include CORS header in heartbeat endpoint

--- app/controllers/heartbeat_controller.rb
+++ app/controllers/heartbeat_controller.rb
@@ -17,6 +17,14 @@
 # limitations under the License.
 
 class HeartbeatController < ApplicationController
+  # include base controller methods
+  include Authenticable
+
+  respond_to :json
+
+  before_filter :default_format_json, :cors_preflight_check
+  after_filter :cors_set_access_control_headers
+
   def index
     @status = Status.new
   end
