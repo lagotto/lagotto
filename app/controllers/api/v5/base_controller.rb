2018a763188b033c1196bef30cecb42133ff2655
Martin Fenner <mfenner@plos.org>
2014-10-11 16:42:28 +0200

    Include internal API calls in rack-mini-profiler results. Closes #135

--- app/controllers/api/v5/base_controller.rb
+++ app/controllers/api/v5/base_controller.rb
@@ -4,6 +4,15 @@ class Api::V5::BaseController < ActionController::Base
 
   respond_to :json
 
-  before_filter :default_format_json, :authenticate_user_from_token!, :cors_preflight_check
+  before_filter :miniprofiler,
+                :default_format_json,
+                :authenticate_user_from_token!,
+                :cors_preflight_check
   after_filter :cors_set_access_control_headers, :set_jsonp_format
+
+  private
+
+  def miniprofiler
+    Rack::MiniProfiler.authorize_request if current_user.try(:is_admin?)
+  end
 end
