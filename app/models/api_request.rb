b3c6fe309d6a3ee4b34b13ed56b43533273916ea
Martin Fenner <mfenner@plos.org>
2014-10-12 08:41:41 +0200

    added scope for api_requests from the last day

--- app/models/api_request.rb
+++ app/models/api_request.rb
@@ -1,9 +1,11 @@
 class ApiRequest < ActiveRecord::Base
-  def date
-    created_at.utc.iso8601
-  end
+  scope :total, lambda { |duration| where("created_at > ?", Time.zone.now - duration.days) }
 
   def self.per_page
     10000
+  end
+
+  def date
+    created_at.utc.iso8601
   end
 end
