7ffed5db2902a3aefbd0a397c4d1a07970dd95ce
Martin Fenner <mfenner@plos.org>
2014-10-22 01:02:02 +0200

    remove obsolete option

--- config/initializers/custom.rb
+++ config/initializers/custom.rb
@@ -5,5 +5,3 @@ Dir[File.join(Rails.root, 'lib', '*.rb')].each { |f| require f }
 include CustomError
 
 ActiveSupport::XmlMini.backend = 'Nokogiri'
-
-Rails.application.routes.default_url_options[:host] = CONFIG[:public_server]
