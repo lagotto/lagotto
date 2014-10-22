8c59f4fa9d603a38727f063aefd11f97cffdc181
Martin Fenner <mfenner@plos.org>
2014-10-22 21:24:53 +0200

    remove reference to seeding articles

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,10 +4,4 @@
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 # We have created individual seeds files for each model, and have put them into the db/seeds directory
 
-# to load sample articles
-# rake db:seed ARTICLES=1
-
-# to update data to a specific version
-# rake db:seed VERSION=2.11
-
 Dir[File.join(Rails.root, 'db', 'seeds', '*.rb')].each { |seed| load seed }
