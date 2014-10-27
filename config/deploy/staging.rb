12948308f7cba7e9537d15b90250572e36c275e2
Martin Fenner <mfenner@plos.org>
2014-10-27 19:05:31 +0100

    use ENV DEPLOY_USER for capistrano

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -4,5 +4,5 @@ ENV['SERVERS'].split(",").each_with_index do |s, i|
   # only primary server has db and workers role
   r = i > 0 ? %w(web app) : %w(web app db workers)
 
-  server s, user: ENV['USER'], roles: r
+  server s, user: ENV['DEPLOY_USER'], roles: r
 end
