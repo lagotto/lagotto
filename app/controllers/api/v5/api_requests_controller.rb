d369b08249a5541d6e35a49d990a1129ef45c1b8
Martin Fenner <mfenner@plos.org>
2014-10-28 12:38:05 +0100

    simplified navigation menu. Use pills instead of tabs for navigation. Closes #165.

--- app/controllers/api/v5/api_requests_controller.rb
+++ app/controllers/api/v5/api_requests_controller.rb
@@ -2,25 +2,29 @@ class Api::V5::ApiRequestsController < Api::V5::BaseController
   load_and_authorize_resource
 
   def index
+    collection = ApiRequest
+
     if ActiveRecord::Base.configurations[Rails.env]['adapter'] == "mysql2"
-      if params[:key] == "local"
-        collection = ApiRequest.where("api_key = ?", ENV['API_KEY'])
+      if params[:q]
+        collection = collection.where("api_key LIKE ?", "#{params[:q]}%")
+      elsif params[:key] == "internal"
+        collection = collection.where("api_key = ?", ENV['API_KEY'])
       elsif params[:key] == "external"
-        collection = ApiRequest.where("api_key != ?", ENV['API_KEY'])
-      elsif params[:key]
-        collection = ApiRequest.where("api_key = ?", params[:key])
-      else
-        collection = ApiRequest.where("ids LIKE ?", "Api::%")
+        collection = collection.where("api_key != ?", ENV['API_KEY'])
+      elsif params[:key] == "other"
+        collection = collection.where("ids LIKE ?", "Api::%")
       end
     else
-      if params[:key] == "local"
-        collection = ApiRequest.where("api_key = '?'", ENV['API_KEY'])
+      if params[:q]
+        collection = collection.where("api_key LIKE '?'", "#{params[:q]}%")
+      elsif params[:key] == "internal"
+        collection = collection.where("api_key = '?'", ENV['API_KEY'])
       elsif params[:key] == "external"
-        collection = ApiRequest.where("api_key != '?'", ENV['API_KEY'])
+        collection = collection.where("api_key != '?'", ENV['API_KEY'])
+      elsif params[:key] == "other"
+        collection = collection.where("ids LIKE ?", "Api::%")
       elsif params[:key]
-        collection = ApiRequest.where("api_key = '?'", params[:key])
-      else
-        collection = ApiRequest.where("ids LIKE ?", "Api::%")
+        collection = collection.where("api_key LIKE '?'", "#{params[:key]}%")
       end
     end
 
