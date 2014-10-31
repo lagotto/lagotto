56334fa67afc88eba937a9763bbc102e85996b9e
Martin Fenner <mfenner@plos.org>
2014-10-31 14:07:58 +0100

    set capistrano stage via ENV variable #174

--- config/deploy/production.rb
+++
@@ -1,8 +0,0 @@
-set :rails_env, 'production'
-
-ENV['SERVERS'].split(",").each_with_index do |s, i|
-  # only primary server has db and workers role
-  r = i > 0 ? %w(web app) : %w(web app db workers)
-
-  server s, user: ENV['DEPLOY_USER'], roles: r
-end
