d4087bd6a1042a6264abf9522ff2139d20b1ede4
Martin Fenner <mfenner@plos.org>
2014-11-02 12:23:52 +0100

    move list of assets to precompile into initializer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,9 +28,6 @@ Lagotto::Application.configure do
   # Enable serving of images, stylesheets, and JavaScripts from an asset server
   # config.action_controller.asset_host = "http://assets.example.com"
 
-  # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  config.assets.precompile += %w( api_requests/* articles/* sources/* )
-
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
 
