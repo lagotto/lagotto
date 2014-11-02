5de17ccc8007acd2cc6ce8e2671c6efb569533d0
Martin Fenner <mfenner@plos.org>
2014-11-02 15:48:25 +0100

    don't use depreciated whiny nils

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -10,9 +10,6 @@ Lagotto::Application.configure do
   # Configure static asset server for tests with Cache-Control for performance
   config.serve_static_assets = true
 
-  # Log error messages when you accidentally call methods on nil
-  config.whiny_nils = true
-
   # Show full error reports and disable caching
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
