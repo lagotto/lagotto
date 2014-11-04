2348d7d2c91881abf4e5f75329a6cb574deb30f6
Martin Fenner <mfenner@plos.org>
2014-11-04 13:55:32 +0100

    fixed publisher_path

--- features/step_definitions/publisher_steps.rb
+++ features/step_definitions/publisher_steps.rb
@@ -30,7 +30,7 @@ end
 
 When /^I go to the page of publisher "(.*?)"$/ do |name|
   publisher = Publisher.where(name: name).first
-  visit publisher_path(publisher)
+  visit publisher_path(publisher.crossref_id)
 end
 
 ### THEN ###
