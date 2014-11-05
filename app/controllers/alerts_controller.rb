3c13ab718aff5fc42ec95d58e0fcc1d9d06ced82
Martin Fenner <mfenner@plos.org>
2014-11-05 08:15:08 +0100

    filter alerts by host

--- app/controllers/alerts_controller.rb
+++ app/controllers/alerts_controller.rb
@@ -12,10 +12,17 @@ class AlertsController < ApplicationController
                    .references(:source)
       @source = Source.where(name: params[:source]).first
     end
+
+    if params[:hostname]
+      collection = collection.where(:hostname => params[:hostname])
+      @hostname = params[:hostname]
+    end
+
     if params[:class_name]
       collection = collection.where(:class_name => params[:class_name])
       @class_name = params[:class_name]
     end
+
     if params[:level]
       level = Alert::LEVELS.index(params[:level].upcase) || 0
       collection = collection.where("level >= ?", level)
