7d6450ae7f9cbe7f4e1e02554182617148765e38
Martin Fenner <mfenner@plos.org>
2014-11-07 08:56:42 +0100

    drop xml support in v3 API #179

--- app/controllers/api/v3/base_controller.rb
+++ app/controllers/api/v3/base_controller.rb
@@ -2,7 +2,7 @@ class Api::V3::BaseController < ActionController::Base
   # include base controller methods
   include Authenticable
 
-  respond_to :json, :xml
+  respond_to :json
 
   before_filter :default_format_json, :authenticate_user_from_token!, :cors_preflight_check
   after_filter :cors_set_access_control_headers, :set_jsonp_format
