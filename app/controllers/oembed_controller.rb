2f7251c4d7edc3fb099d95f21f7339a17d42dbd1
Martin Fenner <mfenner@plos.org>
2014-11-08 11:19:39 +0100

    properly handle missing url parameter in oembed controller

--- app/controllers/oembed_controller.rb
+++ app/controllers/oembed_controller.rb
@@ -8,17 +8,21 @@ class OembedController < ApplicationController
   respond_to :json, :xml
 
   def show
-    url = CGI.unescape(params[:url])
-    url = Rails.application.routes.recognize_path(url)
+    if params[:url]
+      url = CGI.unescape(params[:url])
+      url = Rails.application.routes.recognize_path(url)
+    else
+      url = {}
+    end
 
     # proceed if url was recognized
-    if url["action"] != "routing_error"
+    if url["action"] && url["action"] != "routing_error"
       id_hash = Article.from_uri(url[:id])
       article = Article.where(id_hash)
     end
 
     # proceed if article was found
-    if url["action"] != "routing_error" && article.first
+    if url["action"] && url["action"] != "routing_error" && article.first
       @article = article.first.decorate(context: { maxwidth: params[:maxwidth], maxheight: params[:maxheight] })
     else
       render :template => "oembed/not_found", :status => :not_found
