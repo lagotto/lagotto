95a1166a77c7404493276eba7d36f98aa520ee0a
Martin Fenner <mfenner@plos.org>
2014-12-17 22:40:54 +0100

    increase session duration to one day

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,2 +1,2 @@
 # use memcached for session store
-Rails.application.config.session_store ActionDispatch::Session::CacheStore, :expire_after => 1.hour
+Rails.application.config.session_store ActionDispatch::Session::CacheStore, :expire_after => 1.day
