8695e34f2ecc97dbdaaa0ad9fe54d42ecf0b03ea
Martin Fenner <mfenner@plos.org>
2014-12-17 19:48:05 +0100

    define servers that don't have capistrano db and workers roles

--- config/deploy/stage.rb
+++ config/deploy/stage.rb
@@ -1,8 +1,13 @@
 set :rails_env, ENV['RAILS_ENV']
 
 ENV['SERVERS'].split(",").each_with_index do |s, i|
-  # only primary server has db and workers role
-  r = i > 0 ? %w(web app) : %w(web app db workers)
+
+  # define servers that don't run db and workers
+  if ENV['APP_SERVERS'].to_s.split(",").include?(s)
+    r = %w(web app)
+  else
+    r = %w(web app db workers)
+  end
 
   server s, user: ENV['DEPLOY_USER'], roles: r
 end
