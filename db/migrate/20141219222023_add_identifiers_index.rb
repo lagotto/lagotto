e46757e0877e56d3f5ae08c050d42c9be3b28ee5
Martin Fenner <mfenner@plos.org>
2014-12-24 01:53:03 +0100

    don't create unique index on column that can be null

--- db/migrate/20141219222023_add_identifiers_index.rb
+++ db/migrate/20141219222023_add_identifiers_index.rb
@@ -8,7 +8,7 @@ class AddIdentifiersIndex < ActiveRecord::Migration
     add_index "works", ["pmcid", "published_on", "id"], name: "index_works_on_pmcid_published_on_id"
     add_index "works", ["pmcid"], name: "index_works_on_pmcid", unique: true
     add_index "works", ["canonical_url", "published_on", "id"], name: "index_works_on_url_published_on_id", length: { "canonical_url" => 100 }
-    add_index "works", ["canonical_url"], name: "index_works_on_url", length: 100, unique: true
+    add_index "works", ["canonical_url"], name: "index_works_on_url", length: 100
   end
 
   def down
