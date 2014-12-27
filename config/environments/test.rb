1aab090488b1a774ee8ee8d9db21e0031a6c91da
Martin Fenner <mfenner@plos.org>
2014-12-27 23:07:56 +0100

    replaced config.serve_static_assets with config.serve_static_files

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -8,7 +8,7 @@ Lagotto::Application.configure do
   config.cache_classes = false
 
   # Configure static asset server for tests with Cache-Control for performance
-  config.serve_static_assets = true
+  config.serve_static_files = true
 
   # Show full error reports and disable caching
   config.consider_all_requests_local       = true
