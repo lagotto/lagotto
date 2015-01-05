409172210fa4e9512f08811e15201d26ccb94416
Martin Fenner <mfenner@plos.org>
2015-01-05 08:51:00 +0100

    Make logging of external API responses optional via ENV variable. #214

--- app/models/retrieval_status.rb
+++ app/models/retrieval_status.rb
@@ -41,8 +41,10 @@ class RetrievalStatus < ActiveRecord::Base
   def perform_get_data
     result = source.get_data(work, timeout: source.timeout, work_id: work_id, source_id: source_id)
 
-    # write API response from external source to log/agent.log, using source name and work pid as tags
-    AGENT_LOGGER.tagged(source.name, work.pid) { AGENT_LOGGER.info "#{result.inspect}" }
+    if ENV["LOGSTASH_PATH"].present?
+      # write API response from external source to log/agent.log, using source name and work pid as tags
+      AGENT_LOGGER.tagged(source.name, work.pid) { AGENT_LOGGER.info "#{result.inspect}" }
+    end
 
     data = source.parse_data(result, work, work_id: work_id, source_id: source_id)
     history = History.new(id, data)
--- config/initializers/logstash.rb
+++ config/initializers/logstash.rb
@@ -1,11 +1,13 @@
 # configuration for custom logger of external API responses
 
-# log to file, using logstash JSON format
-logstash_type = ENV["LOGSTASH_TYPE"] ? ENV["LOGSTASH_TYPE"].to_sym : :file
+if ENV["LOGSTASH_PATH"].present?
+  # log to file, using logstash JSON format
+  logstash_type = ENV["LOGSTASH_TYPE"] ? ENV["LOGSTASH_TYPE"].to_sym : :file
 
-# Optional, Redis will default to localhost
-logstash_host = ENV["LOGSTASH_HOST"] || "localhost"
+  # Optional, Redis will default to localhost
+  logstash_host = ENV["LOGSTASH_HOST"] || "localhost"
 
-logstash_path = "log/agent.log"
+  logstash_path = ENV["LOGSTASH_PATH"]
 
-AGENT_LOGGER = LogStashLogger.new(type: logstash_type, path: logstash_path, host: logstash_host)
+  AGENT_LOGGER = LogStashLogger.new(type: logstash_type, path: logstash_path, host: logstash_host)
+end
