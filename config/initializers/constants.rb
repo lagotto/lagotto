60a576ada2aba2b56ea5a239f94e46442cbfc217
Martin Fenner <mfenner@plos.org>
2015-01-08 00:22:04 +0100

    removed delayed_job initializer

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -1,7 +1,6 @@
 # Networking constants
 DEFAULT_TIMEOUT = 60
 NETWORKABLE_EXCEPTIONS = [Faraday::Error::ClientError,
-                          Delayed::WorkerTimeout,
                           URI::InvalidURIError,
                           Encoding::UndefinedConversionError,
                           ArgumentError,
@@ -14,6 +13,9 @@ DOI_FORMAT = %r(\A10\.\d{4,5}/.+)
 
 # Format used for URL validation
 URL_FORMAT = %r(\A(http|https|ftp):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(([0-9]{1,5})?\/.*)?\z)
+
+# Form queue options
+QUEUE_OPTIONS = ["high", "default", "low"]
 
 # Form interval options
 INTERVAL_OPTIONS = [['½ hour', 30.minutes],
--- config/initializers/delayed_job.rb
+++
@@ -1,9 +0,0 @@
-sleep_delay = ENV["DJ_SLEEP_DELAY"].to_i
-
-Delayed::Worker.destroy_failed_jobs = true
-Delayed::Worker.sleep_delay = sleep_delay > 0 ? sleep_delay : 5
-Delayed::Worker.max_attempts = 5
-Delayed::Worker.default_priority = 5
-Delayed::Worker.max_run_time = 90.minutes
-Delayed::Worker.read_ahead = 10
-Delayed::Worker.delay_jobs = !Rails.env.test?
