8c6fa8c345df0e15dbd87542e1f1987e8cf97c48
Martin Fenner <mfenner@plos.org>
2015-01-08 16:06:56 +0100

    change sidekiq log level

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,3 +1,6 @@
 Sidekiq.configure_server do |config|
   config.error_handlers << Proc.new { |exception, hash| Alert.create(exception: exception) }
 end
+
+log_level = ENV["LOG_LEVEL"] ? ENV["LOG_LEVEL"].to_sym : :info
+Sidekiq::Logging.logger.level = log_level
