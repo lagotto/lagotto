f4e90266da2249457af3063637c7958339dd0273
Martin Fenner <mfenner@plos.org>
2015-01-08 15:37:30 +0100

    pass sidekiq exception to internal handler

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,3 +1,6 @@
 Sidekiq.configure_server do |config|
-  config.error_handlers << Proc.new { |exception, hash| Alert.create(exception: exception, hash) }
+  config.error_handlers << Proc.new do |exception, hash|
+    hash[:exception] = exception
+    Alert.create(hash)
+  end
 end
