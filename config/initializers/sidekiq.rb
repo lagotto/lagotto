0cde4c81a9b388e71c99ca387f26a9d313f81dfb
Martin Fenner <mfenner@plos.org>
2015-01-08 16:39:37 +0100

    no need for more detailed sidekiq logging

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,5 +1,3 @@
 Sidekiq.configure_server do |config|
   config.error_handlers << Proc.new { |exception, hash| Alert.create(exception: exception) }
 end
-
-Sidekiq::Logging.logger.level = Logger::DEBUG
