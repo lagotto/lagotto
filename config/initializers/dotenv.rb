e09f395f9134de08c0d09ddc2819cab26408f4bf
Martin Fenner <mfenner@plos.org>
2015-01-08 12:42:44 +0100

    require new concurrency ENV variable instead of workers

--- config/initializers/dotenv.rb
+++ config/initializers/dotenv.rb
@@ -1,5 +1,5 @@
 # Check for required ENV variables, can be set in .env file
 # ENV_VARS is hash of required ENV variables
-env_vars = %w{ DB_USERNAME DB_HOST HOSTNAME SERVERNAME SERVERS SITENAME COUCHDB_URL ADMIN_EMAIL WORKERS API_KEY SECRET_KEY_BASE MAIL_ADDRESS MAIL_PORT MAIL_DOMAIN OMNIAUTH }
+env_vars = %w{ DB_USERNAME DB_HOST HOSTNAME SERVERNAME SERVERS SITENAME COUCHDB_URL ADMIN_EMAIL CONCURRENCY API_KEY SECRET_KEY_BASE MAIL_ADDRESS MAIL_PORT MAIL_DOMAIN OMNIAUTH }
 env_vars.each { |env| fail ArgumentError,  "ENV[#{env}] is not set" if ENV[env].blank? }
 ENV_VARS = Hash[env_vars.map { |env| [env, ENV[env]] }]
