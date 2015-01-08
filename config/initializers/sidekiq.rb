e6c975949dcbcf258fefce8602ad22d52ce23006
Martin Fenner <mfenner@plos.org>
2015-01-08 16:16:05 +0100

    use debug log level with sidekiq

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -2,5 +2,4 @@ Sidekiq.configure_server do |config|
   config.error_handlers << Proc.new { |exception, hash| Alert.create(exception: exception) }
 end
 
-log_level = ENV["LOG_LEVEL"] ? ENV["LOG_LEVEL"].to_sym : :info
-Sidekiq::Logging.logger.level = log_level
+Sidekiq::Logging.logger.level = Logger::DEBUG
