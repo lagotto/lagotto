dd140b655e119a2d8183f87ae34b27fc4030f604
Martin Fenner <mfenner@plos.org>
2015-02-17 09:26:49 +0100

    set sidekiq concurrency in initializer. #246.

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -2,4 +2,5 @@ require 'resolv-replace.rb'
 
 Sidekiq.configure_server do |config|
   config.error_handlers << Proc.new { |exception, hash| Alert.create(exception: exception) }
+  config.options[:concurrency] = ENV["CONCURRENCY"].to_i
 end
