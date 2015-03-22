fb4d2d2def6e67f250e05887312a66e4a43f4686
Martin Fenner <mfenner@plos.org>
2015-03-22 09:32:51 +0100

    refactor europe_pmc common code into concern

---
+++ app/models/concerns/pmcable.rb
@@ -0,0 +1,69 @@
+module Pmcable
+  extend ActiveSupport::Concern
+
+  included do
+    def get_data(work, options={})
+      query_url = get_query_url(work, options)
+      if query_url.nil?
+        result = {}
+      else
+        result = get_result(query_url, options)
+        total = (result.fetch("hitCount", nil)).to_i
+
+        if total > rows
+          # walk through paginated results
+          total_pages = (total.to_f / rows).ceil
+
+          (2..total_pages).each do |page|
+            options[:page] = page
+            query_url = get_query_url(work, options)
+            paged_result = get_result(query_url, options)
+            result["#{result_key}List"][result_key] = result["#{result_key}List"][result_key] | paged_result.fetch("#{result_key}List", {}).fetch(result_key, [])
+          end
+        end
+      end
+
+      # extend hash fetch method to nested hashes
+      result.extend Hashie::Extensions::DeepFetch
+    end
+
+    def parse_data(result, work, options={})
+      return result if result[:error] || result["#{result_key}List"].nil?
+
+      events = get_events(result, work)
+      total = events.length
+      events_url = total > 0 ? get_events_url(work) : nil
+
+      { events: events,
+        events_by_day: [],
+        events_by_month: [],
+        events_url: events_url,
+        event_count: total,
+        event_metrics: get_event_metrics(citations: total) }
+    end
+
+    def get_events(result, work)
+      result.fetch("#{result_key}List", {}).fetch(result_key, []).map do |item|
+        doi = item.fetch("doi", nil)
+        pmid = item.fetch(pmid_key, nil)
+        url = doi ? "http://dx.doi.org/#{doi}" : "http://europepmc.org/abstract/MED/#{pmid}"
+        author_string = item.fetch("authorString", "").chomp(".")
+
+        { event: item,
+          event_url: url,
+
+          # the rest is CSL (citation style language)
+          event_csl: {
+            "author" => get_authors(author_string.split(", "), reversed: true),
+            "title" => item.fetch("title", "").chomp("."),
+            "container-title" => item.fetch(container_title_key, nil),
+            "issued" => get_date_parts_from_parts(item.fetch("pubYear", nil)),
+            "doi" => doi,
+            "pmid" => pmid,
+            "url" => url,
+            "type" => "article-journal" }
+        }
+      end
+    end
+  end
+end
--- app/models/sources/europe_pmc_fulltext.rb
+++ app/models/sources/europe_pmc_fulltext.rb
@@ -1,4 +1,7 @@
 class EuropePmcFulltext < Source
+  # include common methods for Europe PMC
+  include Pmcable
+
   def get_query_url(work, options = {})
     return nil unless work.get_url
 
@@ -28,70 +31,6 @@ class EuropePmcFulltext < Source
     end
   end
 
-  def get_data(work, options={})
-    query_url = get_query_url(work, options)
-    if query_url.nil?
-      result = {}
-    else
-      result = get_result(query_url, options)
-      total = (result.fetch("hitCount", nil)).to_i
-
-      if total > rows
-        # walk through paginated results
-        total_pages = (total.to_f / rows).ceil
-
-        (2..total_pages).each do |page|
-          options[:page] = page
-          query_url = get_query_url(work, options)
-          paged_result = get_result(query_url, options)
-          result["resultList"]["result"] = result["resultList"]["result"] | paged_result.fetch("resultList", {}).fetch("result", [])
-        end
-      end
-    end
-
-    # extend hash fetch method to nested hashes
-    result.extend Hashie::Extensions::DeepFetch
-  end
-
-  def parse_data(result, work, options={})
-    return result if result[:error] || result["resultList"].nil?
-
-    events = get_events(result, work)
-    total = events.length
-    events_url = total > 0 ? get_events_url(work) : nil
-
-    { events: events,
-      events_by_day: [],
-      events_by_month: [],
-      events_url: events_url,
-      event_count: total,
-      event_metrics: get_event_metrics(citations: total) }
-  end
-
-  def get_events(result, work)
-    result.fetch("resultList", {}).fetch("result", []).map do |item|
-      doi = item.fetch("doi", nil)
-      pmid = item.fetch("pmid", nil)
-      url = doi ? "http://dx.doi.org/#{doi}" : "http://europepmc.org/abstract/MED/#{pmid}"
-      author_string = item.fetch("authorString", "").chomp(".")
-
-      { event: item,
-        event_url: url,
-
-        # the rest is CSL (citation style language)
-        event_csl: {
-          "author" => get_authors(author_string.split(", "), reversed: true),
-          "title" => item.fetch("title", "").chomp("."),
-          "container-title" => item.fetch("journalTitle", nil),
-          "issued" => get_date_parts_from_parts(item.fetch("pubYear", nil)),
-          "doi" => doi,
-          "pmid" => pmid,
-          "url" => url,
-          "type" => "article-journal" }
-      }
-    end
-  end
-
   def config_fields
     [:url, :events_url]
   end
@@ -106,5 +45,17 @@ class EuropePmcFulltext < Source
 
   def rows
     25
+  end
+
+  def result_key
+    "result"
+  end
+
+  def pmid_key
+    "pmid"
+  end
+
+  def container_title_key
+    "journalTitle"
   end
 end
