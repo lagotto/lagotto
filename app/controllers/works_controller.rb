dc435d472dc6968e3b52171f3602cd0158dc776e
Martin Fenner <mfenner@plos.org>
2015-03-26 12:29:37 +0100

    allow tracked status in controller. #213

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -65,6 +65,6 @@ class WorksController < ApplicationController
   private
 
   def safe_params
-    params.require(:work).permit(:doi, :title, :pmid, :pmcid, :canonical_url, :year, :month, :day, :publisher_id, :work_type_id, :scp, :wos, :ark)
+    params.require(:work).permit(:doi, :title, :pmid, :pmcid, :canonical_url, :year, :month, :day, :publisher_id, :work_type_id, :scp, :wos, :ark, :tracked)
   end
 end
