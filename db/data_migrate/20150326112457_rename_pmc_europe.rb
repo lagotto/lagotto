5cd1f25edd4641831036362284c3595c3836d12a
Martin Fenner <mfenner@plos.org>
2015-03-26 12:57:41 +0100

    change source subclass in data migration. #283

--- db/data_migrate/20150326112457_rename_pmc_europe.rb
+++ db/data_migrate/20150326112457_rename_pmc_europe.rb
@@ -1,17 +1,17 @@
 class RenamePmcEurope < ActiveRecord::Migration
   def up
     europe_pmc = Source.where('pmceurope').first
-    europe_pmc.update_attributes(name: "europe_pmc") if europe_pmc
+    europe_pmc.update_attributes(name: "europe_pmc", type: "EuropePmc") if europe_pmc
 
     europe_pmc_data = Source.where('pmceuropedata').first
-    europe_pmc_data.update_attributes(name: "europe_pmc_data") if europe_pmc_data
+    europe_pmc_data.update_attributes(name: "europe_pmc_data", type: "EuropePmcData") if europe_pmc_data
   end
 
   def down
     europe_pmc = Source.where('europe_pmc').first
-    europe_pmc.update_attributes(name: "pmceurope") if europe_pmc
+    europe_pmc.update_attributes(name: "pmceurope", type: "PmcEurope") if europe_pmc
 
     europe_pmc_data = Source.where('europe_pmc_data').first
-    europe_pmc_data.update_attributes(name: "pmceuropedata") if europe_pmc_data
+    europe_pmc_data.update_attributes(name: "pmceuropedata", type: "PmcEuropeData") if europe_pmc_data
   end
 end
