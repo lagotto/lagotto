7cd58f3ae737087c58d4d2a6c21ff92a31f34a30
Martin Fenner <mfenner@plos.org>
2015-03-28 12:15:02 +0100

    symlink node_modules. #289

--- config/deploy.rb
+++ config/deploy.rb
@@ -61,7 +61,7 @@ set :log_level, log_level
 set :linked_files, %w{ .env }
 
 # Default value for linked_dirs is []
-set :linked_dirs, %w{ log data tmp/pids tmp/sockets vendor/bundle public/files }
+set :linked_dirs, %w{ log data tmp/pids tmp/sockets vendor/bundle public/files node_modules }
 
 # Default value for keep_releases is 5
 set :keep_releases, 5
