500eb49f35d65b7a15fbc79a97367e60a79a3d50
Martin Fenner <mfenner@plos.org>
2015-03-31 00:16:35 +0200

    use works_count instead of work_count

--- app/decorators/source_decorator.rb
+++ app/decorators/source_decorator.rb
@@ -17,7 +17,7 @@ class SourceDecorator < Draper::Decorator
   end
 
   def status
-    { "refreshed" => work_count - (stale_count + queued_count),
+    { "refreshed" => works_count - (stale_count + queued_count),
       "queued" => queued_count,
       "stale" => stale_count }
   end
@@ -25,12 +25,12 @@ class SourceDecorator < Draper::Decorator
   def by_day
     { "with_events" => with_events_by_day_count,
       "without_events" => without_events_by_day_count,
-      "not_updated" => work_count - (with_events_by_day_count + without_events_by_day_count) }
+      "not_updated" => works_count - (with_events_by_day_count + without_events_by_day_count) }
   end
 
   def by_month
     { "with_events" => with_events_by_month_count,
       "without_events" => without_events_by_month_count,
-      "not_updated" => work_count - (with_events_by_month_count + without_events_by_month_count) }
+      "not_updated" => works_count - (with_events_by_month_count + without_events_by_month_count) }
   end
 end
