695e5b13a2ecc4fe7c6ea8f135aebb898d727729
Martin Fenner <mfenner@plos.org>
2015-04-02 00:45:53 +0200

    optionally order metrics by count. #287

--- app/controllers/api/v6/metrics_controller.rb
+++ app/controllers/api/v6/metrics_controller.rb
@@ -5,11 +5,11 @@ class Api::V6::MetricsController < Api::BaseController
 
   swagger_api :index do
     summary "Returns list of metrics by work IDs and/or source names"
-    notes "If no ids are provided in the query, all works are returned, 50 per page and sorted by publication date (default), or source event count. Search is not supported by the API."
+    notes "If no work ids or source names are provided in the query, all metrics are returned, 1000 per page and sorted by update date."
     param :query, :work_ids, :string, :optional, "Work IDs"
     param :query, :source_ids, :string, :optional, "Source names"
     param :query, :page, :integer, :optional, "Page number"
-    param :query, :per_page, :integer, :optional, "Results per page, defaults to 50"
+    param :query, :per_page, :integer, :optional, "Results per page, defaults to 1000"
     response :ok
     response :unprocessable_entity
     response :not_found
@@ -27,9 +27,15 @@ class Api::V6::MetricsController < Api::BaseController
       collection = collection.joins(:source).where("sources.name IN (?)", params[:source_ids])
     end
 
-    per_page = params[:per_page] && (0..1000).include?(params[:per_page].to_i) ? params[:per_page].to_i : 1000
+    if params[:order]
+      order = ["pdf", "html", "readers", "comments", "likes", "total"].find { |t| t == params[:order] } || "total"
+      collection = collection.order("? DESC", order)
+    else
+      collection = collection.order("retrieval_statuses.updated_at DESC")
+    end
 
-    collection = collection.order("updated_at DESC").paginate(per_page: per_page, :page => params[:page])
+    per_page = params[:per_page] && (0..1000).include?(params[:per_page].to_i) ? params[:per_page].to_i : 1000
+    collection = collection.paginate(per_page: per_page, :page => params[:page])
 
     fresh_when last_modified: collection.maximum(:updated_at)
 
