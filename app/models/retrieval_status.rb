d1ac0092cf97363d021d32db43751705b489194f
Martin Fenner <mfenner@plos.org>
2015-04-09 07:43:49 +0200

    use symbols for by_year attribute. #213

--- app/models/retrieval_status.rb
+++ app/models/retrieval_status.rb
@@ -190,15 +190,14 @@ class RetrievalStatus < ActiveRecord::Base
   def by_year
     return [] if by_month.blank?
 
-    by_month.group_by { |event| event["year"] }.sort.map do |k, v|
-      if ['counter', 'pmc', 'figshare', 'copernicus'].include?(name)
-        { year: k.to_i,
-          pdf: v.reduce(0) { |sum, hash| sum + hash['pdf'].to_i },
-          html: v.reduce(0) { |sum, hash| sum + hash['html'].to_i } }
-      else
-        { year: k.to_i,
-          total: v.reduce(0) { |sum, hash| sum + hash['total'].to_i } }
-      end
+    by_month.group_by { |event| event[:year] }.sort.map do |k, v|
+      { year: k.to_i,
+        pdf: v.reduce(0) { |sum, hsh| sum + hsh.fetch(:pdf) },
+        html: v.reduce(0) { |sum, hsh| sum + hsh.fetch(:html) },
+        readers: v.reduce(0) { |sum, hsh| sum + hsh.fetch(:readers) },
+        comments: v.reduce(0) { |sum, hsh| sum + hsh.fetch(:comments) },
+        likes: v.reduce(0) { |sum, hsh| sum + hsh.fetch(:likes) },
+        total: v.reduce(0) { |sum, hsh| sum + hsh.fetch(:total) } }
     end
   end
 
