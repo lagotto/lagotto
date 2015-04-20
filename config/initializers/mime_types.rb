350a495fa1fde3aac7a4a77f1cc3435ce482d720
Martin Fenner <mfenner@plos.org>
2015-04-20 08:54:09 +0200

    register custom mime type as alias for json. #287

--- config/initializers/mime_types.rb
+++ config/initializers/mime_types.rb
@@ -4,4 +4,4 @@
 # Mime::Type.register "text/richtext", :rtf
 # Mime::Type.register_alias "text/html", :iphone
 
-Mime::Type.register "application/json", :json, %w( text/x-json application/jsonrequest application/vnd.lagotto+json )
+Mime::Type.register_alias "application/vnd.lagotto+json", :json
