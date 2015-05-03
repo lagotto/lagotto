b36b640f19545cebc051247f02543e375b31e95c
Martin Fenner <mfenner@plos.org>
2015-05-03 23:09:42 +0200

    fixed data migration. #213

--- db/data_migrate/20150326112457_rename_pmc_europe.rb
+++ db/data_migrate/20150326112457_rename_pmc_europe.rb
@@ -1,17 +1,11 @@
 class RenamePmcEurope < ActiveRecord::Migration
   def up
-    europe_pmc = Source.where(name: "pmceurope").first
-    europe_pmc.update_attributes(name: "europe_pmc", type: "EuropePmc") if europe_pmc
-
-    europe_pmc_data = Source.where(name: "pmceuropedata").first
-    europe_pmc_data.update_attributes(name: "europe_pmc_data", type: "EuropePmcData") if europe_pmc_data
+    europe_pmc = Source.where(type: "PmcEurope").update_attributes(name: "europe_pmc", type: "EuropePmc")
+    europe_pmc_data = Source.where(type: "PmcEuropeData").update_attributes(name: "europe_pmc_data", type: "EuropePmcData") if europe_pmc_data
   end
 
   def down
-    europe_pmc = Source.where(name: "europe_pmc").first
-    europe_pmc.update_attributes(name: "pmceurope", type: "PmcEurope") if europe_pmc
-
-    europe_pmc_data = Source.where(name: "europe_pmc_data").first
-    europe_pmc_data.update_attributes(name: "pmceuropedata", type: "PmcEuropeData") if europe_pmc_data
+    europe_pmc = Source.where(type: "EuropePmc").update_attributes(name: "pmceurope", type: "PmcEurope") if europe_pmc
+    europe_pmc_data = Source.where(type: "EuropePmcData").update_attributes(name: "pmceuropedata", type: "PmcEuropeData") if europe_pmc_data
   end
 end
