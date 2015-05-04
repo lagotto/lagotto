53c7d8818e56846e5c9d96c150cd4b910da6b411
Martin Fenner <mfenner@plos.org>
2015-05-04 19:49:54 +0200

    keep old europe_pmc names. #283

--- db/data_migrate/20150326112457_rename_pmc_europe.rb
+++ db/data_migrate/20150326112457_rename_pmc_europe.rb
@@ -1,11 +1,11 @@
 class RenamePmcEurope < ActiveRecord::Migration
   def up
-    europe_pmc = Source.where(type: "PmcEurope").update_all(name: "europe_pmc", type: "EuropePmc")
+    europe_pmc = Source.where(type: "PmcEurope").update_all(type: "EuropePmc")
     europe_pmc_data = Source.where(type: "PmcEuropeData").update_all(name: "europe_pmc_data", type: "EuropePmcData") if europe_pmc_data
   end
 
   def down
-    europe_pmc = Source.where(type: "EuropePmc").update_all(name: "pmceurope", type: "PmcEurope") if europe_pmc
-    europe_pmc_data = Source.where(type: "EuropePmcData").update_all(name: "pmceuropedata", type: "PmcEuropeData") if europe_pmc_data
+    europe_pmc = Source.where(type: "EuropePmc").update_all(type: "PmcEurope") if europe_pmc
+    europe_pmc_data = Source.where(type: "EuropePmcData").update_all(type: "PmcEuropeData") if europe_pmc_data
   end
 end
