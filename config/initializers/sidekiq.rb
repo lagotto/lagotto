75a22a1908f78566c7ffb57f20836c42c5ba679a
Martin Fenner <mfenner@plos.org>
2015-05-06 14:31:48 +0200

    ignore some alerts raised by sidekiq. #300

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -2,7 +2,11 @@ require 'resolv-replace.rb'
 
 Sidekiq.configure_server do |config|
   config.error_handlers << Proc.new do |exception, hash|
-    Alert.where(message: exception.message).where(unresolved: true).first_or_create(exception: exception)
+    unless ["ActiveRecord::RecordNotFound",
+            "ActionController::RoutingError",
+            "CustomError::TooManyRequestsError"].include?(exception.class.to_s)
+      Alert.where(message: exception.message).where(unresolved: true).first_or_create(exception: exception)
+    end
   end
   config.options[:concurrency] = ENV["CONCURRENCY"].to_i
 end
