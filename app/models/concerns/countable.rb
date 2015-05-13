b74ad5b4d099b3f36897ec07a4ecb85c9b87683f
Martin Fenner <mfenner@plos.org>
2015-05-13 15:44:42 +1000

    fix caching of source attributes. #306

--- app/models/concerns/countable.rb
+++ app/models/concerns/countable.rb
@@ -11,16 +11,16 @@ module Countable
       cache_read("event_count", retrieval_statuses.sum(:total))
     end
 
-    def event_count=
-      cache_write("event_count", retrieval_statuses.sum(:total))
+    def event_count=(time)
+      cache_write("event_count", retrieval_statuses.sum(:total), time)
     end
 
     def work_count
       cache_read("work_count", works.has_events.size)
     end
 
-    def work_count=
-      cache_write("work_count", works.has_events.size)
+    def work_count=(time)
+      cache_write("work_count", works.has_events.size, time)
     end
 
     def relative_work_count
@@ -35,40 +35,40 @@ module Countable
       cache_read("queued_count", retrieval_statuses.queued.size)
     end
 
-    def queued_count=
-      cache_write("queued_count", retrieval_statuses.queued.size)
+    def queued_count=(time)
+      cache_write("queued_count", retrieval_statuses.queued.size, time)
     end
 
     def stale_count
       cache_read("stale_count", retrieval_statuses.stale.size)
     end
 
-    def stale_count=
-      cache_write("stale_count", retrieval_statuses.stale.size)
+    def stale_count=(time)
+      cache_write("stale_count", retrieval_statuses.stale.size, time)
     end
 
     def response_count
       cache_read("response_count", api_responses.total(24).size)
     end
 
-    def response_count=
-      cache_write("response_count/", api_responses.total(24).size)
+    def response_count=(time)
+      cache_write("response_count/", api_responses.total(24).size, time)
     end
 
     def average_count
       cache_read("average_count", api_responses.total(1).average("duration").to_i)
     end
 
-    def average_count=
-      cache_write("average_count", api_responses.total(1).average("duration"))
+    def average_count=(time)
+      cache_write("average_count", api_responses.total(1).average("duration"), time)
     end
 
     def maximum_count
       cache_read("maximum_count", api_responses.total(1).maximum("duration").to_i)
     end
 
-    def maximum_count=
-      cache_write("maximum_count", api_responses.total(1).maximum("duration"))
+    def maximum_count=(time)
+      cache_write("maximum_count", api_responses.total(1).maximum("duration"), time)
     end
 
     def error_count
@@ -79,32 +79,32 @@ module Countable
       cache_read("with_events_by_day_count", retrieval_statuses.with_events.last_x_days(1).size)
     end
 
-    def with_events_by_day_count=
-      cache_write("with_events_by_day_count", retrieval_statuses.with_events.last_x_days(1).size)
+    def with_events_by_day_count=(time)
+      cache_write("with_events_by_day_count", retrieval_statuses.with_events.last_x_days(1).size, time)
     end
 
     def without_events_by_day_count
       cache_read("without_events_by_day_count", retrieval_statuses.without_events.last_x_days(1).size)
     end
 
-    def without_events_by_day_count=
-      cache_write("without_events_by_day_count", retrieval_statuses.without_events.last_x_days(1).size)
+    def without_events_by_day_count=(time)
+      cache_write("without_events_by_day_count", retrieval_statuses.without_events.last_x_days(1).size, time)
     end
 
     def with_events_by_month_count
       cache_read("with_events_by_month_count", retrieval_statuses.with_events.last_x_days(31).size)
     end
 
-    def with_events_by_month_count=
-      cache_write("with_events_by_month_count", retrieval_statuses.with_events.last_x_days(31).size)
+    def with_events_by_month_count=(time)
+      cache_write("with_events_by_month_count", retrieval_statuses.with_events.last_x_days(31).size, time)
     end
 
     def without_events_by_month_count
       cache_read("without_events_by_month_count", retrieval_statuses.without_events.last_x_days(31).size)
     end
 
-    def without_events_by_month_count=
-      cache_write("without_events_by_month_count", retrieval_statuses.without_events.last_x_days(31).size)
+    def without_events_by_month_count=(time)
+      cache_write("without_events_by_month_count", retrieval_statuses.without_events.last_x_days(31).size, time)
     end
 
     def cache_read(id, value)
@@ -115,8 +115,8 @@ module Countable
       end
     end
 
-    def cache_write(id, value)
-      Rails.cache.write("#{name}/#{id}/#{timestamp}", value)
+    def cache_write(id, value, time)
+      Rails.cache.write("#{name}/#{id}/#{time}", value)
     end
   end
 end
