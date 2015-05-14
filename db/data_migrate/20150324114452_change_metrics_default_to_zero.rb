60b4e05d04fe4c470a508804233196cf567d7e89
Martin Fenner <mfenner@plos.org>
2015-05-15 04:34:41 +1000

    change retrieval_status extra column to nil because we switch from openstruct to json. #213

--- db/data_migrate/20150324114452_change_metrics_default_to_zero.rb
+++ db/data_migrate/20150324114452_change_metrics_default_to_zero.rb
@@ -1,6 +1,6 @@
 class ChangeMetricsDefaultToZero < ActiveRecord::Migration
   def up
-    RetrievalStatus.update_all(pdf: 0, html: 0, readers: 0, comments: 0, likes: 0)
+    RetrievalStatus.update_all(pdf: 0, html: 0, readers: 0, comments: 0, likes: 0, extra: nil)
     Month.update_all(pdf: 0, html: 0, readers: 0, comments: 0, likes: 0)
     Day.update_all(pdf: 0, html: 0, readers: 0, comments: 0, likes: 0)
     ApiResponse.update_all(pdf: 0, html: 0, total: 0)
