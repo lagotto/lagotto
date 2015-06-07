368687e6f73d262375a73264ee96dc2e6905a103
Martin Fenner <mfenner@plos.org>
2015-06-07 19:59:01 +0200

    allow nil in source_id for agents

--- db/migrate/20150527110219_allow_null_in_source_column.rb
+++ db/migrate/20150527110219_allow_null_in_source_column.rb
@@ -1,9 +1,9 @@
 class AllowNullInSourceColumn < ActiveRecord::Migration
   def up
-    change_column :agents, :source_id, :integer, :null => false
+    change_column :agents, :source_id, :integer, :null => true, default: nil
   end
 
   def down
-    change_column :agents, :source_id, :integer, :null => true
+    change_column :agents, :source_id, :integer, :null => false
   end
 end
