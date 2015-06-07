ef5c87ca1df41cf0be15c79bb4523f0d8e990735
Martin Fenner <mfenner@plos.org>
2015-06-07 22:48:38 +0200

    use delete_all instead of destroy_all

--- db/data_migrate/20150607195120_remove_empty_event_rows.rb
+++ db/data_migrate/20150607195120_remove_empty_event_rows.rb
@@ -1,6 +1,8 @@
 class RemoveEmptyEventRows < ActiveRecord::Migration
   def up
-    Event.where(total: 0).destroy_all
+    Event.where(total: 0).delete_all
+    Month.where('event_id NOT IN (SELECT id FROM events)').delete_all
+    Day.where('event_id NOT IN (SELECT id FROM events)').delete_all
   end
 
   def down
