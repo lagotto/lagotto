c7b58ce0883014704b1b9415e40a214815731e71
Martin Fenner <mfenner@plos.org>
2015-06-07 19:19:49 +0200

    updated db schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150602022135) do
+ActiveRecord::Schema.define(version: 20150607091313) do
 
   create_table "agents", force: :cascade do |t|
     t.string   "type",        limit: 255,                                   null: false
@@ -24,7 +24,6 @@ ActiveRecord::Schema.define(version: 20150602022135) do
     t.string   "state_event", limit: 255
     t.text     "config",      limit: 65535
     t.integer  "group_id",    limit: 4,                                     null: false
-    t.boolean  "queueable",   limit: 1,     default: true
     t.boolean  "eventable",   limit: 1,     default: true
     t.datetime "run_at",                    default: '1970-01-01 00:00:00', null: false
     t.datetime "created_at",                                                null: false
@@ -53,7 +52,6 @@ ActiveRecord::Schema.define(version: 20150602022135) do
   create_table "api_responses", force: :cascade do |t|
     t.integer  "work_id",    limit: 4
     t.integer  "agent_id",   limit: 4
-    t.integer  "task_id",    limit: 4
     t.float    "duration",   limit: 24
     t.datetime "created_at"
     t.string   "status",     limit: 255
@@ -328,22 +326,6 @@ ActiveRecord::Schema.define(version: 20150602022135) do
   end
 
   add_index "status", ["created_at"], name: "index_status_created_at", using: :btree
-
-  create_table "tasks", force: :cascade do |t|
-    t.integer  "agent_id",     limit: 4,                                     null: false
-    t.integer  "work_id",      limit: 4
-    t.integer  "publisher_id", limit: 4
-    t.text     "config",       limit: 65535
-    t.datetime "queued_at"
-    t.datetime "retrieved_at",               default: '1970-01-01 00:00:00', null: false
-    t.datetime "scheduled_at",               default: '1970-01-01 00:00:00', null: false
-    t.datetime "created_at",                                                 null: false
-    t.datetime "updated_at",                                                 null: false
-  end
-
-  add_index "tasks", ["agent_id", "work_id"], name: "index_tasks_on_agent_id_and_work_id", unique: true, using: :btree
-  add_index "tasks", ["agent_id"], name: "index_tasks_on_agent_id", using: :btree
-  add_index "tasks", ["work_id"], name: "index_tasks_on_work_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  limit: 255, default: ""
