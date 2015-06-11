b67c1df56f49234e9c8ef879338bb42138570a97
Martin Fenner <mfenner@plos.org>
2015-06-11 16:55:20 +0200

    only change affected fields to utf8mb. #324

--- db/migrate/20150611092528_change_utf8_encoding.rb
+++ db/migrate/20150611092528_change_utf8_encoding.rb
@@ -7,11 +7,11 @@ class ChangeUtf8Encoding < ActiveRecord::Migration
   }
 
   def self.up
-    execute "ALTER DATABASE `#{ActiveRecord::Base.connection.current_database}` CHARACTER SET utf8mb4;"
+    # execute "ALTER DATABASE `#{ActiveRecord::Base.connection.current_database}` CHARACTER SET utf8mb4;"
 
-    ActiveRecord::Base.connection.tables.each do |table|
-      execute "ALTER TABLE `#{table}` CHARACTER SET = utf8mb4;"
-    end
+    # ActiveRecord::Base.connection.tables.each do |table|
+    #   execute "ALTER TABLE `#{table}` CHARACTER SET = utf8mb4;"
+    # end
 
     UTF8_PAIRS.each do |table, col|
       execute "ALTER TABLE `#{table}` CHANGE `#{col}` `#{col}` TEXT  CHARACTER SET utf8mb4  NULL;"
@@ -20,11 +20,11 @@ class ChangeUtf8Encoding < ActiveRecord::Migration
   end
 
   def self.down
-    execute "ALTER DATABASE `#{ActiveRecord::Base.connection.current_database}` CHARACTER SET utf8;"
+    # execute "ALTER DATABASE `#{ActiveRecord::Base.connection.current_database}` CHARACTER SET utf8;"
 
-    ActiveRecord::Base.connection.tables.each do |table|
-      execute "ALTER TABLE `#{table}` CHARACTER SET = utf8;"
-    end
+    # ActiveRecord::Base.connection.tables.each do |table|
+    #   execute "ALTER TABLE `#{table}` CHARACTER SET = utf8;"
+    # end
 
     UTF8_PAIRS.each do |table, col|
       execute "ALTER TABLE `#{table}` CHANGE `#{col}` `#{col}` TEXT  CHARACTER SET utf8  NULL;"
