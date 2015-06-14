5dfb8f57e61fa6a3cfe4bd34dd4e4bcfa1e67c7d
Martin Fenner <mfenner@plos.org>
2015-06-14 21:01:50 +0200

    use demoji gem to strip emoji characters as workaround. #324

--- db/migrate/20150611092528_change_utf8_encoding.rb
+++ db/migrate/20150611092528_change_utf8_encoding.rb
@@ -30,46 +30,46 @@ class ChangeUtf8Encoding < ActiveRecord::Migration
   }
 
   def self.up
-    execute "ALTER DATABASE `#{ActiveRecord::Base.connection.current_database}` CHARACTER SET utf8mb4;"
+    # execute "ALTER DATABASE `#{ActiveRecord::Base.connection.current_database}` CHARACTER SET utf8mb4;"
 
-    ActiveRecord::Base.connection.tables.each do |table|
-      execute "ALTER TABLE `#{table}` CHARACTER SET = utf8mb4;"
-    end
+    # ActiveRecord::Base.connection.tables.each do |table|
+    #   execute "ALTER TABLE `#{table}` CHARACTER SET = utf8mb4;"
+    # end
 
-    UTF8_TEXT_PAIRS.each do |table, col|
-      execute "ALTER TABLE `#{table}` CHANGE `#{col}` `#{col}` TEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;"
-    end
+    # UTF8_TEXT_PAIRS.each do |table, col|
+    #   execute "ALTER TABLE `#{table}` CHANGE `#{col}` `#{col}` TEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;"
+    # end
 
-    UTF8_MEDIUMTEXT_PAIRS.each do |table, col|
-      execute "ALTER TABLE `#{table}` CHANGE `#{col}` `#{col}` MEDIUMTEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;"
-    end
+    # UTF8_MEDIUMTEXT_PAIRS.each do |table, col|
+    #   execute "ALTER TABLE `#{table}` CHANGE `#{col}` `#{col}` MEDIUMTEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;"
+    # end
 
-    UTF8_INDEX_PAIRS.each do |table, col|
-      execute "ALTER TABLE `#{table}` CHANGE `#{col}` VARCHAR(191) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;"
-    end
+    # UTF8_INDEX_PAIRS.each do |table, col|
+    #   execute "ALTER TABLE `#{table}` CHANGE `#{col}` VARCHAR(191) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;"
+    # end
 
     remove_index :index_works_on_pid
     add_index "works", ["pid"], name: "index_works_on_pid", unique: true, length: {"pid"=>191}, using: :btree
   end
 
   def self.down
-    execute "ALTER DATABASE `#{ActiveRecord::Base.connection.current_database}` CHARACTER SET utf8;"
+    # execute "ALTER DATABASE `#{ActiveRecord::Base.connection.current_database}` CHARACTER SET utf8;"
 
-    ActiveRecord::Base.connection.tables.each do |table|
-      execute "ALTER TABLE `#{table}` CHARACTER SET = utf8;"
-    end
+    # ActiveRecord::Base.connection.tables.each do |table|
+    #   execute "ALTER TABLE `#{table}` CHARACTER SET = utf8;"
+    # end
 
-    UTF8_TEXT_PAIRS.each do |table, col|
-      execute "ALTER TABLE `#{table}` CHANGE `#{col}` `#{col}` TEXT CHARACTER SET utf8 COLLATE utf_unicode_ci;;"
-    end
+    # UTF8_TEXT_PAIRS.each do |table, col|
+    #   execute "ALTER TABLE `#{table}` CHANGE `#{col}` `#{col}` TEXT CHARACTER SET utf8 COLLATE utf_unicode_ci;;"
+    # end
 
-    UTF8_MEDIUMTEXT_PAIRS.each do |table, col|
-      execute "ALTER TABLE `#{table}` CHANGE `#{col}` `#{col}` MEDIUMTEXT CHARACTER SET utf8 COLLATE utf_unicode_ci;;"
-    end
+    # UTF8_MEDIUMTEXT_PAIRS.each do |table, col|
+    #   execute "ALTER TABLE `#{table}` CHANGE `#{col}` `#{col}` MEDIUMTEXT CHARACTER SET utf8 COLLATE utf_unicode_ci;;"
+    # end
 
-    UTF8_INDEX_PAIRS.each do |table, col|
-      execute "ALTER TABLE `#{table}` CHANGE `#{col}` VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;"
-    end
+    # UTF8_INDEX_PAIRS.each do |table, col|
+    #   execute "ALTER TABLE `#{table}` CHANGE `#{col}` VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;"
+    # end
 
     remove_index :index_works_on_pid
     add_index "works", ["pid"], name: "index_works_on_pid", unique: true, length: {"pid"=>200}, using: :btree
