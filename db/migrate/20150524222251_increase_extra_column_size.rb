92939d6513e6339eb67f9b18c43c8ce0e1a72a0c
Martin Fenner <mfenner@plos.org>
2015-06-14 15:22:16 +0200

    change column size. #324

--- db/migrate/20150524222251_increase_extra_column_size.rb
+++ db/migrate/20150524222251_increase_extra_column_size.rb
@@ -1,7 +1,7 @@
 class IncreaseExtraColumnSize < ActiveRecord::Migration
   def up
-    change_column :retrieval_statuses, :extra, :text, :limit => 4.gigabytes - 1
-    change_column :alerts, :details, :text, :limit => 4.gigabytes - 1
+    change_column :retrieval_statuses, :extra, :text, :limit => 64.kilobytes + 1
+    change_column :alerts, :details, :text, :limit => 64.kilobytes + 1
   end
 
   def down
