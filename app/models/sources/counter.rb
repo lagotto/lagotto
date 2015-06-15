d24f3e02f51123a75418c97a5af2395be5c333fa
Martin Fenner <mfenner@plos.org>
2015-06-16 00:22:44 +0200

    don't check counter source for PLOS Currents articles

--- app/models/sources/counter.rb
+++ app/models/sources/counter.rb
@@ -1,6 +1,6 @@
 class Counter < Source
   def get_query_url(work)
-    return {} unless work.doi =~ /^10.1371/
+    return {} unless work.doi =~ /^10.1371\/journal/
 
     url_private % { :doi => work.doi_escaped }
   end
