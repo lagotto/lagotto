96c875de66b5ff674411afb143d78fb84b5d6f6a
Martin Fenner <mfenner@plos.org>
2015-06-17 02:07:49 +0200

    add rake task to change the pid to a url. #334

--- db/data_migrate/20150616183431_change_pid_to_url.rb
+++ db/data_migrate/20150616183431_change_pid_to_url.rb
@@ -1,11 +1,11 @@
 class ChangePidToUrl < ActiveRecord::Migration
   def up
     # wos and scp don't have corresponding URL (yet), so will ignore
-    Work.where("pid_type = doi").update_all("pid = CONCAT('http://doi.org/', doi)")
-    Work.where("pid_type = pmid").update_all("pid = CONCAT('http://www.ncbi.nlm.nih.gov/pubmed/', pmid)")
-    Work.where("pid_type = pmcid").update_all("pid = CONCAT('http://www.ncbi.nlm.nih.gov/pmc/articles/PMC', pmcid)")
-    Work.where("pid_type = arxiv").update_all("pid = CONCAT('http://arxiv.org/abs/', arxiv)")
-    Work.where("pid_type = ark").update_all("pid = CONCAT('http://n2t.net/', ark)")
+    Work.where("pid_type = 'doi'").update_all("pid = CONCAT('http://doi.org/', doi)")
+    Work.where("pid_type = 'pmid'").update_all("pid = CONCAT('http://www.ncbi.nlm.nih.gov/pubmed/', pmid)")
+    Work.where("pid_type = 'pmcid'").update_all("pid = CONCAT('http://www.ncbi.nlm.nih.gov/pmc/articles/PMC', pmcid)")
+    Work.where("pid_type = 'arxiv'").update_all("pid = CONCAT('http://arxiv.org/abs/', arxiv)")
+    Work.where("pid_type = 'ark'").update_all("pid = CONCAT('http://n2t.net/', ark)")
   end
 
   def down
