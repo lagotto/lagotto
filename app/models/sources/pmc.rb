29dee82c772e7a000d4357c28f8f7551427ef264
Zach Dennis <zach.dennis@gmail.com>
2015-06-26 09:19:13 -0400

    Continued spike into Pmc#to_csv to handle the branch of logic where a format and date ranges can be passed in.

--- app/models/sources/pmc.rb
+++ app/models/sources/pmc.rb
@@ -193,11 +193,31 @@ class Pmc < Source
         end
       end
     else
-      dates = date_range(options).map { |date| "#{date[:year]}-#{date[:month]}" }
+      dates = date_range(options)
+      formatted_dates = dates.map { |date| "#{date[:year]}-#{date[:month]}" }
+      starting_year, starting_month = dates.first[:year], dates.first[:month]
+
+      results = months.joins(:work)
+        .select("months.id, works.pid, CONCAT(months.year, '-', months.month) AS date_key, months.year, months.month, months.html, months.pdf, months.total")
+        .where("(months.year >= :year AND months.month >= :month) OR (months.year > :year)", year: starting_year, month: starting_month)
+        .order("year ASC, month ASC")
+        .all
+
+      results_nested = Hash.new { |h,k| h[k] = {} }
+      results.each do |result|
+        results_nested[result][result.date_key] = result.send(options[:format])
+      end
 
       CSV.generate do |csv|
-        csv << ["pid_type", "pid"] + dates
-        result["rows"].each { |row| csv << ["doi", row["key"]] + dates.map { |date| row["value"][date] || 0 } }
+        csv << ["pid_type", "pid"] + formatted_dates
+
+        results_nested.each_pair do |result, results_by_date_key|
+          arr = ["doi", result.pid]
+          formatted_dates.each do |date_key|
+            arr.push results_by_date_key[date_key] || 0
+          end
+          csv << arr
+        end
       end
     end
   end
