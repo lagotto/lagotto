f3401a7ae6b4ed9331f2d75eb80e53b8a89b9f46
Zach Dennis <zach.dennis@gmail.com>
2015-06-29 16:33:34 -0400

    Pulling up a SourceReport base-class.
    This allows the CounterReport and PmcReport to share the same functionality without having to duplicate all of the structural similarities.

---
+++ app/models/reports/source_report.rb
@@ -0,0 +1,46 @@
+class SourceReport
+  include Reportable::ToCsv
+
+  class Query
+    attr_reader :source_model
+
+    def initialize(source_model)
+      @source_model = source_model
+    end
+
+    def execute
+      source_model.works.includes(:retrieval_statuses)
+        .group("works.id")
+        .select("works.pid, retrieval_statuses.html, retrieval_statuses.pdf, retrieval_statuses.total")
+        .all
+        .order("works.id ASC")
+    end
+  end
+
+  def initialize(source_model)
+    @query = Query.new(source_model)
+  end
+
+  def headers
+    ["pid_type", "pid", "html", "pdf", "total"]
+  end
+
+  def line_items
+    @line_items ||= results.map do |result|
+      Reportable::LineItem.new(
+        pid_type: "doi",
+        pid: result.pid,
+        html: result.html,
+        pdf: result.pdf,
+        total: result.total
+      )
+    end
+  end
+
+  private
+
+  def results
+    @results ||= @query.execute
+  end
+
+end
