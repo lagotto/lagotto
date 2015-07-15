4ca124ffeb57caa9109059b1f4c19a3d3dcd6b8e
Zach Dennis <zach.dennis@gmail.com>
2015-07-15 12:02:54 -0400

    Require SERVERNAME for api snapshot rake tasks.
    It's necessary because it's the default value for determining the host to hit.

--- spec/lib/tasks/api_rake_spec.rb
+++ spec/lib/tasks/api_rake_spec.rb
@@ -1,11 +1,24 @@
 require 'rails_helper'
 
+shared_examples_for "a rake task that requires SERVERNAME env var" do
+  it "should error if SERVERNAME env var is not set" do
+    without_env("SERVERNAME") do
+      expect {
+        without_env("SERVERNAME"){ subject.invoke }
+      }.to raise_error("SERVERNAME env variable must be set!")
+    end
+  end
+end
+
 describe "api:snapshot:events" do
+  include WithEnv
   include ActiveJob::TestHelper
   include_context "rake"
 
   let!(:source) { FactoryGirl.create(:source) }
   let(:output) { "Queuing a snapshot for /api/events\n" }
+
+  include_examples "a rake task that requires SERVERNAME env var"
 
   it "prerequisites should include environment" do
     expect(subject.prerequisites).to include("environment")
@@ -24,11 +37,14 @@ describe "api:snapshot:events" do
 end
 
 describe "api:snapshot:references" do
+  include WithEnv
   include ActiveJob::TestHelper
   include_context "rake"
 
   let!(:source) { FactoryGirl.create(:source) }
   let(:output) { "Queuing a snapshot for /api/references\n" }
+
+  include_examples "a rake task that requires SERVERNAME env var"
 
   it "prerequisites should include environment" do
     expect(subject.prerequisites).to include("environment")
@@ -47,11 +63,14 @@ describe "api:snapshot:references" do
 end
 
 describe "api:snapshot:works" do
+  include WithEnv
   include ActiveJob::TestHelper
   include_context "rake"
 
   let!(:source) { FactoryGirl.create(:source) }
   let(:output) { "Queuing a snapshot for /api/works\n" }
+
+  include_examples "a rake task that requires SERVERNAME env var"
 
   it "prerequisites should include environment" do
     expect(subject.prerequisites).to include("environment")
