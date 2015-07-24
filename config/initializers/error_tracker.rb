05a795a2d5667caf85f02e17452cbe214a50d37a
Martin Fenner <mfenner@plos.org>
2015-07-24 21:53:47 +0200

    optionally allow sentry for error tracking. Closes #404

--- config/initializers/error_tracker.rb
+++ config/initializers/error_tracker.rb
@@ -1,6 +1,14 @@
 if ENV["BUGSNAG_KEY"]
+  require 'bugsnag'
+
   Bugsnag.configure do |config|
     config.api_key = ENV["BUGSNAG_KEY"]
     config.notify_release_stages = ["stage", "production"]
+  end
+elsif ENV["SENTRY_KEY"]
+  require 'raven'
+
+  Raven.configure do |config|
+    config.dsn = ENV["SENTRY_KEY"]
   end
 end
