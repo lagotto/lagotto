982e5f4a07cf2a7b619cd4a1c2730a1615441a98
Martin Fenner <martin.fenner@datacite.org>
2015-09-02 18:33:07 +0200

    shortened obsolete indexes

--- db/migrate/20140424172758_shorten_delayed_job_index_name.rb
+++
@@ -1,6 +0,0 @@
-class ShortenDelayedJobIndexName < ActiveRecord::Migration
-  def change
-    rename_index :delayed_jobs, 'index_delayed_jobs_run_at_locked_at_locked_by_failed_at_priority', 'index_delayed_jobs_run_at_locked_at_failed_at_priority'
-    rename_index :retrieval_histories, 'index_retrieval_histories_on_source_id_and_status_and_updated_at', 'index_retrieval_histories_on_source_id_and_status_and_updated'
-  end
-end
