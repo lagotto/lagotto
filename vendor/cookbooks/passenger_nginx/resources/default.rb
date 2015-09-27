bce35a6fbaa6277289ee3acc27edf78eb050ad4e
Martin Fenner <martin.fenner@datacite.org>
2015-09-27 17:51:41 +0200

    updated Berksfile

--- vendor/cookbooks/passenger_nginx/resources/default.rb
+++
@@ -1,8 +0,0 @@
-actions :config, :cleanup
-default_action :config
-
-attribute :name, :kind_of => String, :name_attribute => true
-attribute :rails_env, :kind_of => String, :default => node['ruby']['rails_env']
-attribute :user, :kind_of => String, :default => node['ruby']['deploy_user']
-attribute :group, :kind_of => String, :default => node['ruby']['deploy_group']
-attribute :default_server, :kind_of => [TrueClass, FalseClass], :default => true
