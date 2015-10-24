08a1304e30c3aee73818c04f4eb731ae3301d1b4
Martin Fenner <martin.fenner@datacite.org>
2015-10-24 20:31:05 +0200

    seed data via yaml or json #431

--- Gemfile
+++ Gemfile
@@ -26,6 +26,7 @@ gem "nokogiri", "~> 1.6.0"
 gem "multi_json", "~> 1.10.1"
 gem "oj", "~> 2.10.4"
 gem 'safe_yaml', '~> 1.0.4'
+gem 'sprig', '~> 0.1.7'
 gem 'hashie', '~> 3.3.2'
 gem 'namae', '~> 0.10.1'
 gem 'rubyzip',  "~> 1.1", :require => 'zip'
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,7 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-# We have created individual seeds files for each model, and have put them into the db/seeds directory
+# We have created individual seeds files for each model, and have put them into the db/seeds/production directory
 
-Dir[File.join(Rails.root, 'db', 'seeds', '*.rb')].each { |seed| load seed }
+include Sprig::Helpers
+
+sprig [WorkType, RelationType, Report, Filter, Publisher, Group, Source, Agent]
