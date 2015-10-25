b41a26760bc6b9dbadea09f592c10e97222b59c3
Martin Fenner <martin.fenner@datacite.org>
2015-10-26 00:14:56 +0100

    change order of column rename

--- db/migrate/20151023210643_change_publisher_column.rb
+++ db/migrate/20151023210643_change_publisher_column.rb
@@ -3,13 +3,13 @@ class ChangePublisherColumn < ActiveRecord::Migration
     remove_foreign_key :publisher_options, name: "publisher_options_publisher_id_fk"
     add_foreign_key "publisher_options", "publishers", name: "publisher_options_publisher_id_fk", on_delete: :cascade
 
+    rename_column :publishers, :service, :registration_agency
     change_column :publishers, :name, :string, limit: 191
-    change_column :publishers, :service, :string, limit: 191
+    change_column :publishers, :registration_agency, :string, limit: 191
     remove_column :publishers, :member_id, :integer
     remove_column :publishers, :member_symbol, :string, limit: 191
     remove_column :publishers, :symbol, :string
     add_column :publishers, :active, :boolean, default: false
-    rename_column :publishers, :service, :registration_agency
 
     add_index :publishers, ["name"], name: "index_publishers_on_name"
     add_index :publishers, ["registration_agency"], name: "index_publishers_on_registration_agency"
