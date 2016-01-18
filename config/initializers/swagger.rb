774b6b472e167b8a81bcef95922ea7847a9032b1
Martin Fenner <martin.fenner@datacite.org>
2016-01-19 00:32:40 +0100

    provide correct swagger server url. #456

--- config/initializers/swagger.rb
+++ config/initializers/swagger.rb
@@ -2,7 +2,7 @@ Swagger::Docs::Config.register_apis(
   "v6" => {
     api_file_path: "public",
     base_api_controller: "Api::BaseController",
-    base_path: "http://#{ENV['SERVERNAME']}",
+    base_path: ENV['SERVER_URL'],
     :attributes => {
       :info => {
         "title" => "API",
