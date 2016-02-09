1f7d02637372882dc2c333ed5ab2b4a6da8042f1
Martin Fenner <martin.fenner@datacite.org>
2016-02-09 13:15:01 +0100

    provide agent.id in active_job exceptions

--- app/jobs/agent_job.rb
+++ app/jobs/agent_job.rb
@@ -14,10 +14,13 @@ class AgentJob < ActiveJob::Base
   variable_retry delays: [1.minute, 5.minutes, 10.minutes, 30.minutes, 60.minutes], retryable_exceptions: RETRYABLE_EXCEPTIONS
 
   rescue_from StandardError do |exception|
+    agent = self.arguments.first
+
     ActiveRecord::Base.connection_pool.with_connection do
       Notification.where(message: exception.message).where(unresolved: true).first_or_create(
                          exception: exception,
-                         class_name: exception.class.to_s)
+                         class_name: exception.class.to_s,
+                         agent_id: agent.id)
     end
   end
 
