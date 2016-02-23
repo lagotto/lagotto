aaac6dd9510466442038756cba18dda1d4216e56
Martin Fenner <martin.fenner@datacite.org>
2016-02-23 14:43:32 +0100

    make it easier to store job errors

--- app/jobs/deposit_job.rb
+++ app/jobs/deposit_job.rb
@@ -6,14 +6,12 @@ class DepositJob < ActiveJob::Base
   end
 
   rescue_from StandardError do |exception|
-    ActiveRecord::Base.connection_pool.with_connection do
-      Notification.where(message: exception.message).where(unresolved: true).first_or_create(
-                         exception: exception,
-                         class_name: exception.class.to_s)
+    Notification.where(message: exception.message).where(unresolved: true).first_or_create(
+                       exception: exception,
+                       class_name: exception.class.to_s)
 
-      deposit = self.arguments.first
-      deposit.error if deposit.present?
-    end
+    # deposit = self.arguments.first
+    # deposit.error if deposit.present?
   end
 
   def perform(deposit)
