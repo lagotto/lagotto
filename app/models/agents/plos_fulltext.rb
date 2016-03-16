fadc0bc5761d7dbadf89e04bc40d78eaa2376d07
Martin Fenner <martin.fenner@datacite.org>
2016-03-16 09:24:21 +0100

    fixed specs for plos_fulltext agent

--- app/models/agents/plos_fulltext.rb
+++ app/models/agents/plos_fulltext.rb
@@ -6,6 +6,13 @@ class PlosFulltext < Agent
     url % { query_string: query_string }
   end
 
+  def get_provenance_url(options = {})
+    query_string = get_query_string(options)
+    return {} unless query_string.present?
+
+    provenance_url % { query_string: query_string }
+  end
+
   def get_query_string(options={})
     # don't query if work is PLOS article
     work = Work.where(id: options.fetch(:work_id, nil)).first
@@ -14,76 +21,41 @@ class PlosFulltext < Agent
     [work.doi, work.canonical_url].compact.map { |i| "everything:%22#{i}%22" }.join("+OR+")
   end
 
-  def parse_data(result, options={})
-    return result if result[:error]
+  def get_relations_with_related_works(result, work)
+    provenance_url = get_provenance_url(work_id: work.id)
 
-    work = Work.where(id: options.fetch(:work_id, nil)).first
-
-    related_works = get_related_works(result, work)
-    total = related_works.length
-    events_url = total > 0 ? get_events_url(work) : nil
-
-    { works: related_works,
-      events: [{
-        source_id: name,
-        work_id: work.pid,
-        total: total,
-        events_url: events_url,
-        extra: get_extra(result),
-        months: get_events_by_month(related_works) }] }
-  end
-
-  def get_related_works(result, work)
     result.fetch("response", {}).fetch("docs", []).map do |item|
       timestamp = get_iso8601_from_time(item.fetch("publication_date", nil))
       doi = item.fetch("id", nil)
+      pid = doi_as_url(doi)
 
-      { "pid" => doi_as_url(doi),
-        "author" => get_authors(item.fetch("author_display", [])),
-        "title" => item.fetch("title", ""),
-        "container-title" => item.fetch("cross_published_journal_name", []).first,
-        "issued" => get_date_parts(timestamp),
-        "timestamp" => timestamp,
-        "DOI" => doi,
-        "type" => "article-journal",
-        "tracked" => tracked,
-        "registration_agency" => "crossref",
-        "related_works" => [{ "pid" => work.pid,
-                              "source_id" => name,
-                              "relation_type_id" => "cites" }] }
-    end
-  end
-
-  def get_extra(result)
-    result.fetch("response", {}).fetch("docs", []).map do |item|
-      event_time = get_iso8601_from_time(item.fetch("publication_date", nil))
-      doi = item.fetch("id")
-
-      { event: item,
-        event_time: event_time,
-        event_url: "http://doi.org/#{doi}",
-
-        # the rest is CSL (citation style language)
-        event_csl: {
-          "author" => get_authors(item.fetch("author_display", [])),
-          "title" => item.fetch("title", ""),
-          "container-title" => item.fetch("cross_published_journal_name", []).first,
-          "issued" => get_date_parts(event_time),
-          "DOI" => doi,
-          "type" => "article-journal" }
-      }
+      { relation: { "subj_id" => pid,
+                    "obj_id" => work.pid,
+                    "relation_type_id" => "cites",
+                    "provenance_url" => provenance_url,
+                    "source_id" => source_id },
+        subj: { "pid" => pid,
+                "author" => get_authors(item.fetch("author_display", [])),
+                "title" => item.fetch("title", ""),
+                "container-title" => item.fetch("cross_published_journal_name", []).first,
+                "issued" => get_date_parts(timestamp),
+                "timestamp" => timestamp,
+                "DOI" => doi,
+                "type" => "article-journal",
+                "tracked" => tracked,
+                "registration_agency" => "crossref" }}
     end
   end
 
   def config_fields
-    [:url, :events_url]
+    [:url, :provenance_url]
   end
 
   def url
     "http://api.plos.org/search?q=%{query_string}&fq=doc_type:full&fl=id,publication_date,title,cross_published_journal_name,author_display&wt=json&rows=1000"
   end
 
-  def events_url
+  def provenance_url
     "http://www.plosone.org/search/advanced?unformattedQuery=%{query_string}"
   end
 
--- spec/models/agents/plos_fulltext_spec.rb
+++ spec/models/agents/plos_fulltext_spec.rb
@@ -3,7 +3,17 @@ require 'rails_helper'
 describe PlosFulltext, type: :model, vcr: true do
   subject { FactoryGirl.create(:plos_fulltext) }
 
-  let(:work) { FactoryGirl.create(:work, doi: nil, canonical_url: "https://github.com/rougier/ten-rules") }
+  let(:work) { FactoryGirl.create(:work, doi: nil, canonical_url: "https://github.com/rougier/ten-rules", registration_agency: "github") }
+
+  context "urls" do
+    it "should get_query_url" do
+      expect(subject.get_query_url(work_id: work.id)).to eq("http://api.plos.org/search?q=everything:%22https://github.com/rougier/ten-rules%22&fq=doc_type:full&fl=id,publication_date,title,cross_published_journal_name,author_display&wt=json&rows=1000")
+    end
+
+    it "should get_provenance_url" do
+      expect(subject.get_provenance_url(work_id: work.id)).to eq("http://www.plosone.org/search/advanced?unformattedQuery=everything:%22https://github.com/rougier/ten-rules%22")
+    end
+  end
 
   context "lookup canonical URL" do
     it "should look up canonical URL if there is no work url" do
@@ -27,7 +37,7 @@ describe PlosFulltext, type: :model, vcr: true do
     end
 
     it "should report if there are no events returned by the PLOS Search API" do
-      work = FactoryGirl.create(:work, doi: nil, canonical_url: "https://github.com/pymor/pymor")
+      work = FactoryGirl.create(:work, doi: nil, canonical_url: "https://github.com/pymor/pymor", registration_agency: "github")
       response = subject.get_data(work_id: work.id)
       expect(response["response"]["numFound"]).to eq(0)
     end
@@ -56,45 +66,40 @@ describe PlosFulltext, type: :model, vcr: true do
      it "should report that there are no events if the doi has the wrong prefix" do
       work = FactoryGirl.create(:work, doi: "10.1371/journal.pmed.0020124")
       result = {}
-      expect(subject.parse_data(result, work_id: work.id)).to eq(works: [], events: [{ source_id: "plos_fulltext", work_id: work.pid, total: 0, events_url: nil, extra: [], months: [] }])
+      expect(subject.parse_data(result, work_id: work.id)).to eq([])
     end
 
     it "should report if there are no events returned by the PLOS Search API" do
       body = File.read(fixture_path + 'plos_fulltext_nil.json')
       result = JSON.parse(body)
-      expect(subject.parse_data(result, work_id: work.id)).to eq(works: [], events: [{ source_id: "plos_fulltext", work_id: work.pid, total: 0, events_url: nil, extra: [], months: [] }])
+      expect(subject.parse_data(result, work_id: work.id)).to eq([])
     end
 
     it "should report if there are events returned by the PLOS Search API" do
-      work = FactoryGirl.create(:work, doi: nil, canonical_url: "https://github.com/rougier/ten-rules", published_on: "2009-03-15")
+      work = FactoryGirl.create(:work, doi: nil, pid: "https://github.com/rougier/ten-rules", canonical_url: "https://github.com/rougier/ten-rules", published_on: "2009-03-15", registration_agency: "github")
       body = File.read(fixture_path + 'plos_fulltext.json')
       result = JSON.parse(body)
       response = subject.parse_data(result, work_id: work.id)
 
-      event = response[:events].first
-      expect(event[:source_id]).to eq("plos_fulltext")
-      expect(event[:work_id]).to eq(work.pid)
-      expect(event[:total]).to eq(1)
-      expect(event[:months].length).to eq(1)
-      expect(event[:months].first).to eq(year: 2014, month: 9, total: 1)
+      expect(response.length).to eq(1)
+      expect(response.first[:relation]).to eq("subj_id"=>"http://doi.org/10.1371/journal.pcbi.1003833",
+                                              "obj_id"=>work.pid,
+                                              "relation_type_id"=>"cites",
+                                              "provenance_url"=> "http://www.plosone.org/search/advanced?unformattedQuery=everything:%22https://github.com/rougier/ten-rules%22",
+                                              "source_id"=>"plos_fulltext")
 
-      expect(response[:works].length).to eq(1)
-      related_work = response[:works].last
-      expect(related_work['author']).to eq([{"family"=>"Rougier", "given"=>"Nicolas P."}, {"family"=>"Droettboom", "given"=>"Michael"}, {"family"=>"Bourne", "given"=>"Philip E."}])
-      expect(related_work['title']).to eq("Ten Simple Rules for Better Figures")
-      expect(related_work['container-title']).to eq("PLOS Computational Biology")
-      expect(related_work['issued']).to eq("date-parts"=>[[2014, 9, 11]])
-      expect(related_work['type']).to eq("article-journal")
-      expect(related_work['timestamp']).to eq("2014-09-11T00:00:00Z")
-      expect(related_work['related_works']).to eq([{"pid"=> work.pid, "source_id"=>"plos_fulltext", "relation_type_id"=>"cites"}])
-
-      extra = event[:extra].last
-      expect(extra[:event_time]).to eq("2014-09-11T00:00:00Z")
-      expect(extra[:event_csl]['author']).to eq([{"family"=>"Rougier", "given"=>"Nicolas P."}, {"family"=>"Droettboom", "given"=>"Michael"}, {"family"=>"Bourne", "given"=>"Philip E."}])
-      expect(extra[:event_csl]['title']).to eq("Ten Simple Rules for Better Figures")
-      expect(extra[:event_csl]['container-title']).to eq("PLOS Computational Biology")
-      expect(extra[:event_csl]['issued']).to eq("date-parts"=>[[2014, 9, 11]])
-      expect(extra[:event_csl]['type']).to eq("article-journal")
+      expect(response.first[:subj]).to eq("pid"=>"http://doi.org/10.1371/journal.pcbi.1003833",
+                                          "author"=>[{"family"=>"Rougier", "given"=>"Nicolas P."},
+                                                     {"family"=>"Droettboom", "given"=>"Michael"},
+                                                     {"family"=>"Bourne", "given"=>"Philip E."}],
+                                          "title"=>"Ten Simple Rules for Better Figures",
+                                          "container-title"=>"PLOS Computational Biology",
+                                          "issued"=>{"date-parts"=>[[2014, 9, 11]]},
+                                          "timestamp"=>"2014-09-11T00:00:00Z",
+                                          "DOI"=>"10.1371/journal.pcbi.1003833",
+                                          "type"=>"article-journal",
+                                          "tracked"=>false,
+                                          "registration_agency"=>"crossref")
     end
 
     it "should catch timeout errors with the PLOS Search API" do
