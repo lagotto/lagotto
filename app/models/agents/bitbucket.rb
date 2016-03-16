0446aa0fec73bc1f4bb31cf86af5b3c9f9ebeb8f
Martin Fenner <martin.fenner@datacite.org>
2016-03-16 08:49:11 +0100

    work on bitbucket agent specs

--- app/models/agents/bitbucket.rb
+++ app/models/agents/bitbucket.rb
@@ -1,65 +1,96 @@
 class Bitbucket < Agent
-  # include common methods for repos
-  include Repoable
+  def get_query_url(options={})
+    url % { owner: options[:owner], repo: options[:repo] }
+  end
+
+  def get_provenance_url(options={})
+    provenance_url % { owner: options[:owner], repo: options[:repo] }
+  end
+
+  def get_total(options={})
+    Work.tracked.where(registration_agency: 'bitbucket').count
+  end
+
+  def queue_jobs(options={})
+    return 0 unless active?
+
+    works = Work.tracked.where(registration_agency: 'bitbucket').pluck(:id)
+    total = works.size
+
+    works.each_slice(job_batch_size) do |ids|
+      AgentJob.set(queue: queue, wait_until: schedule_at).perform_later(self, options.merge(ids: ids))
+    end
+
+    # return number of works queued
+    total
+  end
+
+  def get_data(options={})
+    work = Work.where(id: options[:work_id]).first
+    return {} unless work.present?
+
+    query_url = get_query_url(work_id: work.id)
+    get_result(query_url, options)
+  end
+
+  def get_owner_and_repo(work)
+    # code from https://github.com/octokit/octokit.rb/blob/master/lib/octokit/repository.rb
+    full_name = URI.parse(work.canonical_url).path[1..-1]
+    owner, repo = full_name.split('/')
+    { owner: owner, repo: repo }
+  end
 
   def parse_data(result, options={})
     return result if result[:error]
     work = Work.where(id: options[:work_id]).first
     return { error: "Resource not found.", status: 404 } unless work.present?
 
-    # TODO forks = readers?
-    # readers = result.fetch("forks_count", 0)
-    # likes = result.fetch("followers_count", 0)
-    # total = readers + likes
-    # extra = result.slice("followers_count", "forks_count", "description", "utc_created_on")
-    # events_url = total > 0 ? get_events_url(work_id: work.id) : nil
+    relations = []
+    provenance_url = get_provenance_url(get_owner_and_repo(work)) if work.canonical_url.present?
+    followers_count = result.fetch("followers_count", 0)
+    if followers_count > 0
+      relations << { relation: { "subj_id" => "https://bitbucket.org",
+                                 "obj_id" => work.pid,
+                                 "relation_type_id" => "bookmarks",
+                                 "total" => followers_count,
+                                 "provenance_url" => provenance_url,
+                                 "source_id" => source_id },
+                     subj: { "pid" => "https://bitbucket.org",
+                             "URL" => "https://bitbucket.org",
+                             "title" => "Bitbucket",
+                             "issued" => { "date-parts" => [[2008, 2, 8]] }} }
+    end
 
-    # { events: [{
-    #     source_id: name,
-    #     work_id: work.pid,
-    #     readers: readers,
-    #     likes: likes,
-    #     total: total,
-    #     events_url: events_url,
-    #     extra: extra }] }
+    forks_count = result.fetch("forks_count", 0)
+    if forks_count > 0
+      relations << { relation: { "subj_id" => "https://bitbucket.org",
+                                 "obj_id" => work.pid,
+                                 "relation_type_id" => "is_derived_from",
+                                 "total" => forks_count,
+                                 "provenance_url" => provenance_url,
+                                 "source_id" => source_id },
+                     subj: { "pid" => "https://bitbucket.org",
+                             "URL" => "https://bitbucket.org",
+                             "title" => "Bitbucket",
+                             "issued" => { "date-parts" => [[2008, 2, 8]] }} }
+    end
 
-        [{"subj_id" => "http://bitbucket.org",
-          "obj_id" => work.pid,
-          "relation_type_id" => "reads",
-          "total" => result.fetch("followers_count", 0),
-          "source_id" => name},
-        { "subj_id" => "http://bitbucket.org",
-          "obj_id" => work.pid,
-          "relation_type_id" => "forks",
-          "total" => result.fetch("forks_count", 0),
-          "source_id" => name}]
+    relations
   end
 
   def config_fields
-    [:url, :events_url]
+    [:url, :provenance_url]
   end
 
   def url
     "https://api.bitbucket.org/1.0/repositories/%{owner}/%{repo}"
   end
 
-  def events_url
+  def provenance_url
     "https://bitbucket.org/%{owner}/%{repo}"
   end
 
   def rate_limiting
     config.rate_limiting || 30000
-  end
-
-  def repo_key
-    "bitbucket.org"
-  end
-
-  def likes_key
-    "followers_count"
-  end
-
-  def events_key
-    ["followers_count", "forks_count", "description", "utc_created_on"]
   end
 end
--- spec/models/agents/bitbucket_spec.rb
+++ spec/models/agents/bitbucket_spec.rb
@@ -49,53 +49,20 @@ describe Bitbucket, type: :model, vcr: true do
     it "should report if the canonical_url is missing" do
       work = FactoryGirl.create(:work, :canonical_url => nil)
       result = {}
-      # expect(subject.parse_data(result, work_id: work.id)).to eq(events: [{ source_id: "bitbucket", work_id: work.pid, readers: 0, likes: 0, total: 0, events_url: nil, extra: {} }])
-      expect(subject.parse_data(result, work_id: work.id)).to eq([{ "subj_id" => "http://bitbucket.org",
-                                                                    "obj_id" =>  work.pid,
-                                                                    "relation_type_id" => "reads",
-                                                                    "total" => 0,
-                                                                    "source_id" => "bitbucket"},
-                                                                  { "subj_id" => "http://bitbucket.org",
-                                                                    "obj_id" => work.pid,
-                                                                    "relation_type_id" => "forks",
-                                                                    "total" => 0,
-                                                                    "source_id" => "bitbucket"}])
+      expect(subject.parse_data(result, work_id: work.id)).to eq([])
     end
 
     it "should report that there are no events if the canonical_url is not a Bitbucket URL" do
       work = FactoryGirl.create(:work, :canonical_url => "https://code.google.com/p/gwtupload/")
       result = {}
-      # expect(subject.parse_data(result, work_id: work.id)).to eq(events: [{ source_id: "bitbucket", work_id: work.pid, readers: 0, likes: 0, total: 0, events_url: nil, extra: {} }])
-      expect(subject.parse_data(result, work_id: work.id)).to match_array [{ "subj_id" => "http://bitbucket.org",
-                                                                             "obj_id" => work.pid, 
-                                                                             "relation_type_id" => "reads", 
-                                                                             "total" => 0,
-                                                                             "source_id" => "bitbucket"}, 
-                                                                           { "subj_id" => "http://bitbucket.org",
-                                                                             "obj_id" => work.pid, 
-                                                                             "relation_type_id" => "forks", 
-                                                                             "total" => 0, 
-                                                                             "source_id" => "bitbucket"}]
+      expect(subject.parse_data(result, work_id: work.id)).to eq([])
     end
 
     it "should report if there are no events and event_count returned by the Bitbucket API" do
       body = File.read(fixture_path + 'bitbucket_nil.json')
       result = JSON.parse(body)
-      # extra = { "followers_count"=>0, "forks_count"=>0, "description"=>"Exemplos da Aula 1 do curso de Desenvolvimento Web com Ruby on Rails do ruby+web\r\n\r\nhttp://rubymaisweb.ning.com", "utc_created_on"=>"2012-01-13 14:47:01+00:00" }
       response = subject.parse_data(result, work_id: work.id)
-      # expect(response).to eq(events: [{ source_id: "bitbucket", work_id: work.pid, readers: 0, likes: 0, total: 0, events_url: nil, extra: extra }])
-      expect(response).to eq([{ "subj_id" => "http://bitbucket.org",
-                                "obj_id" => work.pid,
-                                "relation_type_id" => "reads",
-                                "total" => 0,
-                                "source_id" => "bitbucket"},
-                              { "subj_id" => "http://bitbucket.org",
-                                "obj_id" => work.pid,
-                                "relation_type_id" => "forks",
-                                "total" => 0,
-                                "source_id" => "bitbucket"},
-
-                                ])
+      expect(response).to eq([])
     end
 
     it "should report if there are events and event_count returned by the Bitbucket API" do
@@ -103,23 +70,23 @@ describe Bitbucket, type: :model, vcr: true do
       result = JSON.parse(body)
       response = subject.parse_data(result, work_id: work.id)
 
-      expect(response).to eq([{ "subj_id" => "http://bitbucket.org",
-                                "obj_id" => work.pid,
-                                "relation_type_id" => "reads",
-                                "total" => 162,
-                                "source_id" => "bitbucket" },
-                              { "subj_id" => "http://bitbucket.org",
-                                "obj_id" => work.pid,
-                                "relation_type_id" => "forks",
-                                "total" => 272,
-                                "source_id" => "bitbucket"}])
-
-      # TODO JW - bit if a mismatch (and duplication) here
-      # expect(event[:total]).to eq(434)
-      # expect(event[:readers]).to eq(272)
-      # expect(event[:likes]).to eq(162)
-      # expect(event[:events_url]).to eq("https://bitbucket.org/galaxy/galaxy-central")
-      # expect(event[:extra]["followers_count"]).to eq(162)
+      expect(response.length).to eq(2)
+      expect(response.first[:relation]).to eq("subj_id"=>"https://bitbucket.org",
+                                              "obj_id"=>work.pid,
+                                              "relation_type_id"=>"bookmarks",
+                                              "total"=>162,
+                                              "provenance_url" => "https://bitbucket.org/galaxy/galaxy-central",
+                                              "source_id"=>"bitbucket")
+      expect(response.first[:subj]).to eq("pid"=>"https://bitbucket.org",
+                                          "URL"=>"https://bitbucket.org",
+                                          "title"=>"Bitbucket",
+                                          "issued"=>{"date-parts"=>[[2008, 2, 8]]})
+      expect(response.second[:relation]).to eq("subj_id"=>"https://bitbucket.org",
+                                              "obj_id"=>work.pid,
+                                              "relation_type_id"=>"is_derived_from",
+                                              "total"=>272,
+                                              "provenance_url" => "https://bitbucket.org/galaxy/galaxy-central",
+                                              "source_id"=>"bitbucket")
     end
 
     it "should catch timeout errors with the Bitbucket API" do
