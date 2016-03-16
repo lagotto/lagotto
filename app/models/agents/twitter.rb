fcd77b349d441ef61356573927191c7568fd739f
Martin Fenner <martin.fenner@datacite.org>
2016-03-16 07:53:40 +0100

    fixed twitter specs

--- app/models/agents/twitter.rb
+++ app/models/agents/twitter.rb
@@ -6,15 +6,7 @@ class Twitter < Agent
     url_private % { :doi => work.doi_escaped }
   end
 
-  def get_events_url(work)
-    nil
-  end
-
-  def response_options
-    { :metrics => :comments }
-  end
-
-  def get_related_works(result, work)
+  def get_relations_with_related_works(result, work)
     Array(result['rows']).map do |item|
       data = item['value']
       if data.key?("from_user")
@@ -30,46 +22,20 @@ class Twitter < Agent
       timestamp = get_iso8601_from_time(data['created_at'])
       url = "http://twitter.com/#{user}/status/#{data['id_str']}"
 
-      { "pid" => url,
-        "author" => get_authors([user_name]),
-        "title" => data.fetch('text') { '' },
-        "container-title" => 'Twitter',
-        "issued" => get_date_parts(timestamp),
-        "timestamp" => timestamp,
-        "URL" => url,
-        "type" => 'personal_communication',
-        "tracked" => tracked,
-        "registration_agency" => "twitter",
-        "related_works" => [{ "pid" => work.pid,
-                              "source_id" => name,
-                              "relation_type_id" => "discusses" }] }
-    end
-  end
-
-  def get_extra(result)
-    Array(result['rows']).map do |item|
-      data = item['value']
-      if data.key?("from_user")
-        user = data["from_user"]
-        user_name = data["from_user_name"]
-        user_profile_image = data["profile_image_url"]
-      else
-        user = data["user"]["screen_name"]
-        user_name = data["user"]["name"]
-        user_profile_image = data["user"]["profile_image_url"]
-      end
-
-      event_time = get_iso8601_from_time(data['created_at'])
-      url = "http://twitter.com/#{user}/status/#{data['id_str']}"
-
-      { event: { id: data.fetch("id_str", nil),
-                 text: data.fetch("text", nil),
-                 created_at: event_time,
-                 user: user,
-                 user_name: user_name,
-                 user_profile_image: user_profile_image },
-        event_time: event_time,
-        event_url: url }
+      { relation: { "subj_id" => url,
+                    "obj_id" => work.pid,
+                    "relation_type_id" => "discusses",
+                    "source_id" => source_id },
+        subj: { "pid" => url,
+                "author" => get_authors([user_name]),
+                "title" => data.fetch('text') { '' },
+                "container-title" => 'Twitter',
+                "issued" => get_date_parts(timestamp),
+                "timestamp" => timestamp,
+                "URL" => url,
+                "type" => 'personal_communication',
+                "tracked" => tracked,
+                "registration_agency" => "twitter" }}
     end
   end
 
--- spec/models/agents/twitter_spec.rb
+++ spec/models/agents/twitter_spec.rb
@@ -46,7 +46,7 @@ describe Twitter, type: :model, vcr: true do
       body = File.read(fixture_path + 'twitter_nil.json', encoding: 'UTF-8')
       result = JSON.parse(body)
       response = subject.parse_data(result, work_id: work.id)
-      expect(response).to eq(works: [], events: [{ source_id: "twitter", work_id: work.pid, comments: 0, total: 0, extra: [], months: [] }])
+      expect(response).to eq([])
     end
 
     it "should report if there are events returned by the Twitter API" do
@@ -54,30 +54,22 @@ describe Twitter, type: :model, vcr: true do
       result = JSON.parse(body)
       response = subject.parse_data(result, work_id: work.id)
 
-      event = response[:events].first
-      expect(event[:total]).to eq(2)
-      expect(event[:months].length).to eq(1)
-      expect(event[:months].first).to eq(year: 2012, month: 5, total: 2, comments: 2)
+      expect(response.length).to eq(2)
+      expect(response.first[:relation]).to eq("subj_id"=>"http://twitter.com/regrum/status/204270013081849857",
+                                               "obj_id"=>work.pid,
+                                               "relation_type_id"=>"discusses",
+                                               "source_id"=>"twitter")
 
-      expect(response[:works].length).to eq(2)
-      related_work = response[:works].first
-      expect(related_work['author']).to eq([{"given"=>"regrum"}])
-      expect(related_work['title']).to eq("Don't be blinded by science http://t.co/YOWRhsXb")
-      expect(related_work['container-title']).to eq("Twitter")
-      expect(related_work['issued']).to eq("date-parts"=>[[2012, 5, 20]])
-      expect(related_work['type']).to eq("personal_communication")
-      expect(related_work['URL']).to eq("http://twitter.com/regrum/status/204270013081849857")
-      expect(related_work['timestamp']).to eq("2012-05-20T17:59:00Z")
-      expect(related_work['related_works']).to eq([{"pid"=> work.pid, "source_id"=>"twitter", "relation_type_id"=>"discusses"}])
-
-      extra = event[:extra].first
-      extra = extra[:event]
-      expect(extra[:id]).to eq("204270013081849857")
-      expect(extra[:text]).to eq("Don't be blinded by science http://t.co/YOWRhsXb")
-      expect(extra[:created_at]).to eq("2012-05-20T17:59:00Z")
-      expect(extra[:user]).to eq("regrum")
-      expect(extra[:user_name]).to eq("regrum")
-      expect(extra[:user_profile_image]).to eq("http://a0.twimg.com/profile_images/61215276/regmanic2_normal.JPG")
+      expect(response.first[:subj]).to eq("pid"=>"http://twitter.com/regrum/status/204270013081849857",
+                                          "author"=>[{"given"=>"regrum"}],
+                                          "title"=>"Don't be blinded by science http://t.co/YOWRhsXb",
+                                          "container-title"=>"Twitter",
+                                          "issued"=>{"date-parts"=>[[2012, 5, 20]]},
+                                          "timestamp"=>"2012-05-20T17:59:00Z",
+                                          "URL"=>"http://twitter.com/regrum/status/204270013081849857",
+                                          "type"=>"personal_communication",
+                                          "tracked"=>false,
+                                          "registration_agency"=>"twitter")
     end
 
     it "should catch timeout errors with the Twitter API" do
