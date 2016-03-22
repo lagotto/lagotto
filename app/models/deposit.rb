3d4f97fe768aa3351f87c11d29d3e9d5be69a8db
Martin Fenner <martin.fenner@datacite.org>
2016-03-22 15:18:58 +0100

    use default checked_at time for publisher

--- app/models/deposit.rb
+++ app/models/deposit.rb
@@ -212,7 +212,7 @@ class Deposit < ActiveRecord::Base
     p = Publisher.where(name: subj_id).first_or_create!(title: subj["title"])
     p.update_attributes!(title: subj["title"],
                          registration_agency: subj["registration_agency"],
-                         checked_at: subj["issued"],
+                         checked_at: subj.fetch("issued", Time.zone.now.utc.iso8601),
                          active: subj["active"])
     p
   rescue ActiveRecord::RecordInvalid, ActiveRecord::RecordNotUnique => exception
