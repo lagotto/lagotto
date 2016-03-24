0a8d1a8b97d729d5286b0dc2a0e6f8b4e2376384
Martin Fenner <martin.fenner@datacite.org>
2016-03-24 21:36:38 +0100

    don't raise timeout errors

--- app/jobs/status_cache_job.rb
+++ app/jobs/status_cache_job.rb
@@ -1,6 +1,11 @@
 class StatusCacheJob < ActiveJob::Base
   queue_as :critical
 
+  # don't raise error for ActiveRecord::ConnectionTimeoutError
+  rescue_from *RETRYABLE_EXCEPTIONS do |exception|
+
+  end
+
   rescue_from StandardError do |exception|
     ActiveRecord::Base.connection_pool.with_connection do
       Notification.where(message: exception.message).where(unresolved: true).first_or_create(
