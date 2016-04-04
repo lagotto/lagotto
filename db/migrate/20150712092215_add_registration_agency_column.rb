c579529a72af9ff34e762d9a9077de7c5f60790d
Martin Fenner <martin.fenner@datacite.org>
2016-04-04 21:12:06 +0200

    removed duplicate column names

--- db/migrate/20150712092215_add_registration_agency_column.rb
+++ db/migrate/20150712092215_add_registration_agency_column.rb
@@ -2,6 +2,8 @@ class AddRegistrationAgencyColumn < ActiveRecord::Migration
   def up
     add_column :works, :registration_agency, :string
     add_index "works", ["registration_agency"], name: "index_works_on_registration_agency", length: 191
+
+    Work.where("doi IS NOT NULL and registration_agency IS NULL").update_all(registration_agency: 'crossref')
   end
 
   def down
