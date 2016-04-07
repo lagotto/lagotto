b4e9a0d25e5a2bdd04f53960eb704fe41ba0dc43
Martin Fenner <martin.fenner@datacite.org>
2016-04-08 00:07:01 +0200

    renamed scope. #517

--- app/models/publisher.rb
+++ app/models/publisher.rb
@@ -54,13 +54,13 @@ class Publisher < ActiveRecord::Base
     if ActionController::Base.perform_caching
       Rails.cache.read("publisher/#{name}/#{source_id}/work_count/#{timestamp}").to_i
     else
-      works.has_events.by_source(source_id).size
+      works.has_results.by_source(source_id).size
     end
   end
 
   def work_count_by_source=(source_id, time)
     Rails.cache.write("publisher/#{name}/#{source_id}/work_count/#{time}",
-                      works.has_events.by_source(source_id).size)
+                      works.has_results.by_source(source_id).size)
   end
 
   def cache_key
