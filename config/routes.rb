a7c8bc6414444f90766c5a386e4ab702bbfe3c3e
Martin Fenner <martin.fenner@datacite.org>
2016-04-08 00:06:41 +0200

    changed route name. #517

--- config/routes.rb
+++ config/routes.rb
@@ -75,7 +75,7 @@ Lagotto::Application.routes.draw do
       resources :relation_types, only: [:index, :show]
       resources :results
       resources :sources, concerns: [:workable] do
-        resources :months
+        resources :results
       end
       resources :status, only: [:index]
       resources :work_types, only: [:index, :show]
