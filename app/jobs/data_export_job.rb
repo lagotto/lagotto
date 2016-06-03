112310283d56915603d3e364042067313a2fa62b
Martin Fenner <martin.fenner@datacite.org>
2016-06-03 08:27:00 +0200

    silenty retry all jobs on db connection timeout error. #562

--- app/jobs/data_export_job.rb
+++ app/jobs/data_export_job.rb
@@ -1,6 +1,10 @@
 class DataExportJob < ActiveJob::Base
   queue_as :high
 
+  rescue_from ActiveJob::DeserializationError, ActiveRecord::ConnectionTimeoutError do
+    retry_job wait: 5.minutes, queue: :default
+  end
+
   def perform(options={})
     ActiveRecord::Base.connection_pool.with_connection do
       data_export = DataExport.find_by_id!(options[:id])
--- app/jobs/deposit_reprocess_job.rb
+++ app/jobs/deposit_reprocess_job.rb
@@ -1,6 +1,10 @@
 class DepositReprocessJob < ActiveJob::Base
   queue_as :high
 
+  rescue_from ActiveJob::DeserializationError, ActiveRecord::ConnectionTimeoutError do
+    retry_job wait: 5.minutes, queue: :default
+  end
+
   def perform(ids)
     ActiveRecord::Base.connection_pool.with_connection do
       Deposit.where(id: ids).find_each { |deposit| deposit.reset }
