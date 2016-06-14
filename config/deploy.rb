e564ed8ccff75bd7f3e7b68cdece8f8abf710e80
Joe Wass <jwass@crossref.org>
2016-06-14 17:07:10 +0100

    Fix frontend paths moved to deploy. Linked to #9b4973f1ce98fb4dc049a194edf17b576006dfb7

--- config/deploy.rb
+++ config/deploy.rb
@@ -32,7 +32,7 @@ set :pty, false
 set :branch, ENV["REVISION"] || ENV["BRANCH_NAME"] || "master"
 
 # install/update npm modules and bower components
-set :npm_target_path, -> { release_path.join('frontend') }
+set :npm_target_path, -> { release_path.join('vendor') }
 
 # restart passenger method
 set :passenger_restart_with_touch, true
@@ -62,7 +62,7 @@ set :log_level, log_level
 set :linked_files, %w{ .env }
 
 # Default value for linked_dirs is []
-set :linked_dirs, %w{ log tmp/pids tmp/sockets tmp/files tmp/reports vendor/bundle frontend/node_modules frontend/bower_components }
+set :linked_dirs, %w{ log tmp/pids tmp/sockets tmp/files tmp/reports vendor/bundle vendor/node_modules vendor/bower_components }
 
 # Default value for keep_releases is 5
 set :keep_releases, 5
