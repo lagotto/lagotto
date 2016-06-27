4b02be33d8fd255f18f6d68c7578f1d32e860f89
Martin Fenner <martin.fenner@datacite.org>
2016-06-28 00:44:36 +0200

    include bower_components in assets path

--- config/application.rb
+++ config/application.rb
@@ -45,8 +45,8 @@ module Lagotto
     # config.autoload_paths += %W(#{config.root}/extras)
     config.autoload_paths += Dir["#{config.root}/app/models/**/**", "#{config.root}/app/controllers/**/"]
 
-    # add assets from Ember app
-    config.assets.paths << "#{Rails.root}/frontend/bower_components"
+    # add npm assets
+    config.assets.paths << "#{Rails.root}/vendor/bower_components"
 
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.
