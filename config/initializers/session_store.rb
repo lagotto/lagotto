9fe3f34090a885225e42bd56441d65d610d44384
Martin Fenner <martin.fenner@datacite.org>
2016-07-19 11:43:16 +0200

    set tld_length for session cookie

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: ENV['SESSION_KEY'], domain: ENV['SESSION_DOMAIN']
+Rails.application.config.session_store :cookie_store, key: ENV['SESSION_KEY'], domain: ENV['SESSION_DOMAIN'], tld_length: 2
