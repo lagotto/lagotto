415fb212e95ece69e30bc772c5395613ec465d1f
Martin Fenner <martin.fenner@datacite.org>
2016-07-21 07:46:15 +0200

    use session store in multiple subdomains

--- config/application.rb
+++ config/application.rb
@@ -25,7 +25,6 @@ end
 # default values for some ENV variables
 ENV['APPLICATION'] ||= "lagotto"
 ENV['SESSION_KEY'] ||= "_#{ENV['APPLICATION']}_session"
-ENV['SESSION_DOMAIN'] ||= ""
 ENV['SITENAMELONG'] ||= "Lagotto"
 ENV['LOG_LEVEL'] ||= "info"
 ENV['GITHUB_URL'] ||= "https://github.com/lagotto/lagotto"
