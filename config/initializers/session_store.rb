74cc035a4b4eaf773eed285203eb9fcaa74f2c96
Martin Fenner <martin.fenner@datacite.org>
2016-09-05 17:30:58 +0200

    use session cookie specific to site

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,2 @@
 # Be sure to restart your server when you modify this file.
-
-Rails.application.config.session_store :cookie_store, key: ENV['SESSION_KEY'], domain: :all
+Rails.application.config.session_store :cookie_store, key: ENV['SESSION_KEY']
