d7467cf22b65b78580e4852fa93473f33ca54b51
Martin Fenner <martin.fenner@datacite.org>
2016-09-09 05:51:28 -0600

    Check for redis_url env variable #641

--- config/initializers/mini_profiler.rb
+++ config/initializers/mini_profiler.rb
@@ -5,6 +5,8 @@ Rack::MiniProfilerRails.initialize!(Rails.application)
 Rails.application.middleware.delete(Rack::MiniProfiler)
 Rails.application.middleware.insert_after(Rack::Deflater, Rack::MiniProfiler)
 
-uri = URI.parse(ENV["REDIS_URL"])
-Rack::MiniProfiler.config.storage_options = { :host => uri.host, :port => uri.port }
-Rack::MiniProfiler.config.storage = Rack::MiniProfiler::RedisStore
+if ENV["REDIS_URL"].present?
+  uri = URI.parse(ENV["REDIS_URL"])
+  Rack::MiniProfiler.config.storage_options = { :host => uri.host, :port => uri.port }
+  Rack::MiniProfiler.config.storage = Rack::MiniProfiler::RedisStore
+end
