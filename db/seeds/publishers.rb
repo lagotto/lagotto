9733c4055836a9bd7d0b79c8dc1b77873508d45d
Matthew Bowen <mbowen@plos.org>
2017-01-12 14:37:54 -0800

    PLT-709: seed data (sd)

--- db/seeds/publishers.rb
+++ db/seeds/publishers.rb
@@ -2,5 +2,9 @@
 plos = Publisher.where(name: '340').first_or_create(
     :title => 'Public Library of Science (PLoS)',
     :member_id => '340',
-    :prefixes => "--- \n- '10.1371'",
-    :other_names => '--- \n- xPublic Library of Science\n- Public Library of Science (PLoS)')
+    :other_names => '--- \n- xPublic Library of Science\n- Public Library of Science (PLoS)',
+    :prefixes => <<-eos
+--- 
+- '10.1371'
+eos
+)
