2174ff4e642bc343f979134e486994050a26a3bf
Sweet Potato <tpolischuk@plos.org>
2017-01-12 13:24:54 -0800

    PLT-708 Adding cap rake task for importing test DOIs

--- config/deploy.rb
+++ config/deploy.rb
@@ -93,7 +93,7 @@ namespace :deploy do
   after :publishing, :restart
   after :publishing, "data:migrate"
   after :publishing, "swagger:docs"
-  after :publishing, "db:works:import:plos"
+  after :publishnig, "sample_works:sample_works"
 
   after :finishing, "deploy:cleanup"
 end
