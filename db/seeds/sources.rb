a52acc881bb6972b8937b15571b0bd376a07a36f
Matthew Bowen <mbowen@plos.org>
2017-01-13 14:18:38 -0800

    PLT-709: Citeulike seed data.

--- db/seeds/sources.rb
+++ db/seeds/sources.rb
@@ -1,18 +1,16 @@
 #
 # GROUPS
 #
+saved       = Group.where(name: 'saved').first_or_create(title: 'Saved')
 cited       = Group.where(name: 'cited').first_or_create(title: 'Cited')
 discussed   = Group.where(name: 'discussed').first_or_create(title: 'Discussed')
+viewed      = Group.where(name: 'viewed').first_or_create(title: 'Viewed')
 other       = Group.where(name: 'other').first_or_create(title: 'Other')
 recommended = Group.where(name: 'recommended').first_or_create(title: 'Recommended')
-saved       = Group.where(name: 'saved').first_or_create(title: 'Saved')
-viewed      = Group.where(name: 'viewed').first_or_create(title: 'Viewed')
 
 #
-# SOURCES
+# SOURCES (start with a clean slate)
 #
-
-# start with a clean-slate
 Source.delete_all
 
 bloglines_cfg = OpenStruct.new
@@ -32,6 +30,39 @@ bloglines = Bloglines.where(name: 'bloglines').first_or_create(
   :queueable   => 1,
   :eventable   => 1)
 
+
+citeulike_cfg = OpenStruct.new
+citeulike_cfg['url'] = 'http://www.citeulike.org/api/posts/for/doi/%{doi}'
+citeulike_cfg['job_batch_size']                 = 200
+citeulike_cfg['batch_time_interval']            = 3600
+citeulike_cfg['rate_limiting']                  = 2000
+citeulike_cfg['wait_time']                      = 300
+citeulike_cfg['staleness_week']                 = 86400
+citeulike_cfg['staleness_month']                = 86400
+citeulike_cfg['staleness_year']                 = 648000
+citeulike_cfg['staleness_all']                  = 2592000
+citeulike_cfg['timeout']                        = 30
+citeulike_cfg['max_failed_queries']             = 200
+citeulike_cfg['max_failed_query_time_interval'] = 86400
+citeulike_cfg['disable_delay']                  = 10
+citeulike_cfg['workers']                        = 50
+citeulike_cfg['events_url']                     = 'http://www.citeulike.org/doi/%{doi}'
+citeulike_cfg['priority']                       = 6
+citeulike_cfg['queue']                          = 'low'
+citeulike_cfg['tracked']                        = "'0'"
+
+citeulike = Citeulike.where(name: 'citeulike').first_or_create(
+  :id          => 2,
+  :type        => Citeulike,
+  :name        => citeulike,
+  :title       => CiteULike,
+  :config      => citeulike_cfg,
+  :group_id    => saved.id,
+  :private     => 0,
+  :description => 'CiteULike is a free social bookmarking service for scholarly content.',
+  :queueable   => 1,
+  :eventable   => 1)
+
 #
 # PUBLISHER_OPTIONS 
 #
@@ -39,9 +70,9 @@ crossref_cfg = OpenStruct.new
 crossref_cfg['username'] = 'plos'
 crossref_cfg['password'] = 'plos1'
 
-crossref_po = PublisherOption.where(source_id: bloglines.id).where(publisher_id: 340).first_or_create(
+crossref_po = PublisherOption.where(source_id: crossref.id).where(publisher_id: 340).first_or_create(
   :publisher_id => 340,
-  :source_id => bloglines.id,
+  :source_id => crossref.id,
   :config => crossref_cfg)
 
 #pmc_cfg = OpenStruct.new
