ef65f0eea21620b8f13ececb863f268e0d4d44fa
Matthew Bowen <mbowen@plos.org>
2018-05-10 16:25:49 -0700

    ALM-886: update pubmed source to only store related content identifiers (with count) and not lookup and insert as separate works.

--- spec/models/pub_med_spec.rb
+++ spec/models/pub_med_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-describe PubMed, type: :model, vcr: true do
+describe PubMed, type: :model, vcr: true, focus: true do
   subject { FactoryGirl.create(:pub_med) }
 
   let(:work) { FactoryGirl.create(:work, :doi => "10.1371/journal.pone.0000001", :pmid => "17183631", :pmcid => "1762328") }
@@ -41,7 +41,7 @@ describe PubMed, type: :model, vcr: true do
       work = FactoryGirl.create(:work, :pmid => "")
       result = {}
       result.extend Hashie::Extensions::DeepFetch
-      expect(subject.parse_data(result, work)).to eq(works: [], events: { source: "pub_med", work: work.pid, total: 0, extra: [], days: [], months: [] })
+      expect(subject.parse_data(result, work)).to eq(events: { source: "pub_med", work: work.pid, total: 0, extra: []})
     end
 
     it "should report if there are no events and event_count returned by the PubMed API" do
@@ -50,7 +50,7 @@ describe PubMed, type: :model, vcr: true do
       result = Hash.from_xml(body)
       result.extend Hashie::Extensions::DeepFetch
       response = subject.parse_data(result, work)
-      expect(response).to eq(works: [], events: { source: "pub_med", work: work.pid, total: 0, extra: [], days: [], months: [] })
+      expect(response).to eq(events: { source: "pub_med", work: work.pid, total: 0, extra: [] })
     end
 
     it "should report if there are events and event_count returned by the PubMed API" do
@@ -58,23 +58,8 @@ describe PubMed, type: :model, vcr: true do
       result = Hash.from_xml(body)
       result.extend Hashie::Extensions::DeepFetch
       response = subject.parse_data(result, work)
-      expect(response[:works].length).to eq(13)
       expect(response[:events][:total]).to eq(13)
-      expect(response[:events][:events_url]).to eq("http://www.ncbi.nlm.nih.gov/sites/entrez?db=pubmed&cmd=link&LinkName=pubmed_pmc_refs&from_uid=17183631")
-
-      event = response[:works].first
-      expect(event["DOI"]).to eq("10.3389/fendo.2012.00005")
-      expect(event["PMID"]).to eq("22389645")
-      expect(event["PMCID"]).to eq("3292175")
-      expect(event['author']).to eq([{"affiliation"=>[], "family"=>"Morrison", "given"=>"Shaun F."}, {"affiliation"=>[], "family"=>"Madden", "given"=>"Christopher J."}, {"affiliation"=>[], "family"=>"Tupone", "given"=>"Domenico"}])
-      expect(event['title']).to eq("Central Control of Brown Adipose Tissue Thermogenesis")
-      expect(event['container-title']).to eq("Front. Endocrin.")
-      expect(event['issued']).to eq("date-parts"=>[[2012]])
-      expect(event['volume']).to eq("3")
-      expect(event['issue']).to be_nil
-      expect(event['page']).to be_nil
-      expect(event['type']).to eq("article-journal")
-      expect(event['related_works']).to eq([{"related_work"=> work.pid, "source"=>"pub_med", "relation_type"=>"cites"}])
+      expect(subject.get_events_url work).to eq("http://www.ncbi.nlm.nih.gov/sites/entrez?db=pubmed&cmd=link&LinkName=pubmed_pmc_refs&from_uid=17183631")
 
       extra = response[:events][:extra].first
       expect(extra[:event_url]).to eq("http://www.pubmedcentral.nih.gov/articlerender.fcgi?artid=" + extra[:event])
@@ -85,23 +70,8 @@ describe PubMed, type: :model, vcr: true do
       result = Hash.from_xml(body)
       result.extend Hashie::Extensions::DeepFetch
       response = subject.parse_data(result, work)
-      expect(response[:works].length).to eq(1)
       expect(response[:events][:total]).to eq(1)
-      expect(response[:events][:events_url]).to eq("http://www.ncbi.nlm.nih.gov/sites/entrez?db=pubmed&cmd=link&LinkName=pubmed_pmc_refs&from_uid=17183631")
-
-      event = response[:works].first
-      expect(event["DOI"]).to eq("10.3389/fendo.2012.00005")
-      expect(event["PMID"]).to eq("22389645")
-      expect(event["PMCID"]).to eq("3292175")
-      expect(event['author']).to eq([{"affiliation"=>[], "family"=>"Morrison", "given"=>"Shaun F."}, {"affiliation"=>[], "family"=>"Madden", "given"=>"Christopher J."}, {"affiliation"=>[], "family"=>"Tupone", "given"=>"Domenico"}])
-      expect(event['title']).to eq("Central Control of Brown Adipose Tissue Thermogenesis")
-      expect(event['container-title']).to eq("Front. Endocrin.")
-      expect(event['issued']).to eq("date-parts"=>[[2012]])
-      expect(event['volume']).to eq("3")
-      expect(event['issue']).to be_nil
-      expect(event['page']).to be_nil
-      expect(event['type']).to eq("article-journal")
-      expect(event['related_works']).to eq([{"related_work"=> work.pid, "source"=>"pub_med", "relation_type"=>"cites"}])
+      expect(subject.get_events_url work).to eq("http://www.ncbi.nlm.nih.gov/sites/entrez?db=pubmed&cmd=link&LinkName=pubmed_pmc_refs&from_uid=17183631")
     end
 
     it "should catch timeout errors with the PubMed API" do
