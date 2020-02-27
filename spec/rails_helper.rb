1a35f8854dd869b084e5deb1eb1c63c9be5224e7
Shawn Gatchell <shawng03@gmail.com>
2020-02-27 15:04:56 -0800

    ALM-1008 shoulda-matchers needed extra config

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -23,6 +23,13 @@ CodeClimate::TestReporter.start
 require File.expand_path("../../config/environment", __FILE__)
 require "rspec/rails"
 require "shoulda-matchers"
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
+end
+
 require "email_spec"
 require "factory_girl_rails"
 require "capybara/rspec"
